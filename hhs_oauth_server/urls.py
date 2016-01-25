2853c71e9f0c0926521e739d92b8f4db4edf4410
Federico Frenguelli <synasius@gmail.com>
2016-01-25 16:32:13 +0100

    moved user_self view to api/profile/ endpoint

--- hhs_oauth_server/urls.py
+++ hhs_oauth_server/urls.py
@@ -17,12 +17,15 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from apps.accounts.urls import api_urls
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     url(r'^$',                  include('apps.home.urls')),
     url(r'^accounts/',          include('apps.accounts.urls')),
     url(r'^api/',               include('apps.api.urls')),
+    url(r'^api/',               include(api_urls)),
     url(r'^social-auth/',       include('social.apps.django_app.urls', namespace='social')),
 
 ]
