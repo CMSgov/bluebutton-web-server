605e3a54fdf0b634508a44c7a641634f9f0591d0
Federico Frenguelli <synasius@gmail.com>
2016-03-09 10:42:08 +0100

    removed custom RegisterApplication view. override the oauth2_provider.ApplicationRegistration view

--- apps/dot_ext/urls.py
+++ apps/dot_ext/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import include, url
 from . import views
-from .views.custom_app_register import RegisterApplication
 
 
 oauth2_provider_urls = ([
-    url(r'^applications/register/$', RegisterApplication, name="register"),
+    url(r'^applications/register/$', views.ApplicationRegistration.as_view(), name="register"),
     url(r'^applications/(?P<pk>\d+)/update/$', views.ApplicationUpdate.as_view(), name="update"),
     url(r'', include('oauth2_provider.urls')),
 ], 'oauth2_provider', 'oauth2_provider')
--- apps/dot_ext/views/application.py
+++ apps/dot_ext/views/application.py
@@ -6,7 +6,18 @@ from oauth2_provider import views as oauth2_views
 from ..forms import CustomRegisterApplicationForm
 
 
-class ApplicationRegistration(oauth2_views.ApplicationRegistration):
+class CustomFormMixin(object):
+    def get_form_kwargs(self):
+        """
+        Add `user` to kwargs because it is required by the constructor of
+        CustomRegisterApplicationForm class.
+        """
+        kwargs = super(CustomFormMixin, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+
+class ApplicationRegistration(CustomFormMixin, oauth2_views.ApplicationRegistration):
     """
     View used to register a new Application for the request.user
     """
@@ -17,18 +28,9 @@ class ApplicationRegistration(oauth2_views.ApplicationRegistration):
         return CustomRegisterApplicationForm
 
 
-class ApplicationUpdate(oauth2_views.ApplicationUpdate):
+class ApplicationUpdate(CustomFormMixin, oauth2_views.ApplicationUpdate):
     """
     View used to update an application owned by the request.user
     """
     fields = None
     form_class = CustomRegisterApplicationForm
-
-    def get_form_kwargs(self):
-        """
-        Add `user` to kwargs because it is required by the constructor of
-        CustomRegisterApplicationForm class.
-        """
-        kwargs = super(ApplicationUpdate, self).get_form_kwargs()
-        kwargs['user'] = self.request.user
-        return kwargs
