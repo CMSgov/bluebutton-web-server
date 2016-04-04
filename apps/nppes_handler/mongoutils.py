092ebc9ab67e10a7fe94b03e0e99e1d93d891246
Alan Viars <aviars@videntity.com>
2016-04-04 17:56:03 -0400

    Added oAuth protected NPPES updater

---
+++ apps/nppes_handler/mongoutils.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ai ts=4 sts=4 et sw=4
+
+from django.conf import settings
+import os, json, sys
+import datetime
+from bson.code import Code
+from bson.objectid import ObjectId
+from pymongo import MongoClient, DESCENDING
+from collections import OrderedDict
+
+
+
+def to_json(results_dict):
+    return json.dumps(results_dict, indent = 4, default=json_util.default)
+  
+  
+def update_mongo_pjson(document, database_name="nppes", collection_name="pjson"):
+    """Update a Provider JSON resource.  The resource must exist in this implementation.
+    """
+    l=[]
+    response_dict=OrderedDict()
+    invalid = False
+    existing_document = None
+    try:
+
+        mc          = MongoClient(host='127.0.0.1', port=27017, document_class=OrderedDict)
+        db          = mc[database_name]
+        collection  = db[collection_name]
+
+        myobject=collection.find_one({'number':document['number']})
+        response_dict['number'] = document['number']
+        if not myobject:
+            response_dict['code']=404
+            response_dict['errors']=["The record cannot be updated because it is not found in the database.",]
+            
+        else:
+            document['_id'] = myobject['_id']
+            myobjectid=collection.save(document)
+            response_dict['code']=200
+    except:
+        print "Error reading from Mongo"
+        print str(sys.exc_info())
+        response_dict['code'] = 500
+        response_dict['errors']=[ str(sys.exc_info()), ]
+
+    
+    return response_dict
+
+
--- apps/nppes_handler/views.py
+++ apps/nppes_handler/views.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-import json
+import json, sys
 import logging
 from collections import OrderedDict
 
@@ -9,33 +9,54 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from apps.dot_ext.decorators import capability_protected_resource
+from .mongoutils import update_mongo_pjson
+from pdt.pjson.validate_pjson import validate_pjson
 
 
 @capability_protected_resource()
 @csrf_exempt
 def nppes_update(request):
     
+    response = OrderedDict()
+    response['code']= 000
+    response['status']= ""
+    response['message']=""
+    errors = []
+    
     if request.method == 'POST':
-        errors =[]
         try:
             j =json.loads(request.body, object_pairs_hook=OrderedDict)
-
-            if type(j) !=  type(OrderedDict()):
-                errors.append("The string did not contain a JSON object.")
-            else:
-                j['write'] = True
+            response = validate_pjson(request.body, "update")
         except:
             errors.append("The string did not contain valid JSON.")
+        
+        if response['errors']:
+            for e in response['errors']:
+                errors.append(e)
+        
         if errors:
             response = OrderedDict()
             response['code']= 400
             response['status']= "Error"
-            response['message']="Write Failed."
+            response['message']="The NPPES update failed."
             response['errors']= errors
-            return HttpResponse(json.dumps(response, indent =4),
-                                           content_type="application/json")
-        #A valid JSON object was provided.
-        return HttpResponse(json.dumps(j, indent =4),
+            
+        else:
+            #Looks good
+            
+            nppes_write_response = update_mongo_pjson(j)
+
+            
+            response['code']= nppes_write_response['code']
+            if response['code'] == 200:
+                response['status']= "OK"
+                response['message']="The NPPES update for %s succeeded."  % (nppes_write_response['number'])
+            else:
+                response['status']= "FAIL"
+                response['message']="The NPPES update for %s failed."  % (nppes_write_response['number'])               
+                response['errors'] = nppes_write_response['errors']
+                
+        return HttpResponse(json.dumps(response, indent =4),
                                            content_type="application/json")
     #this is a GET
     response = OrderedDict()
