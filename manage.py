13164b1004d2d52861935bb28158c4c2cfe4be0f
Alan Viars <aviars@videntity.com>
2016-05-13 16:51:16 -0400

    Major re-arrangement of deck chairs

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hhs_oauth_server.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "_start.settings")
 
     from django.core.management import execute_from_command_line
 
