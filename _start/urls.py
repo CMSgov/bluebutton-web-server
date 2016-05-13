d8ae510dfb2b024e2e41153f1603b91b3735d035
Alan Viars <aviars@videntity.com>
2016-05-13 16:51:16 -0400

    Major re-arrangement of deck chairs

--- _start/urls.py
+++ _start/urls.py
@@ -10,10 +10,8 @@ urlpatterns = [
     url(r'^accounts/',      include('apps.accounts.urls')),
     url(r'^api/',           include('apps.api.urls')),
     url(r'^api/',           include(api_urls)),
-    url(r'^fhir/v3/',       include('apps.fhir.urls')),
-    url(r'^nppes/',         include('apps.nppes_handler.urls')),
+    #url(r'^fhir/v3/',       include('apps.fhir.urls')),
     url(r'^capabilities/',  include('apps.capabilities.urls')),
     url(r'^o/',             include('apps.dot_ext.urls')),
-    url(r'^signups/',       include('apps.signups.urls')),
     url(r'^',               include('apps.home.urls')),
 ]
