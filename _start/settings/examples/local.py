f4570da2f6717644e37868ffbe42e37f70168acb
Mark Scrimshire <ekivemark>
2016-05-16 00:30:39 -0400

    Added SETTINGS_MODE to last item in DJANGO_SETTINGS_MODULE Added SETTINGS_MODE to EXPORT SETTINGS UPDATED MANAGERS AND ADMINS in all SETTIGS files

--- _start/settings/examples/local.py
+++ _start/settings/examples/local.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ai ts=4 sts=4 et sw=4
+
+"""
+hhs_oauth_server
+FILE: local.py
+Created: 5/16/16 12:10 AM
+
+
+"""
+__author__ = 'Mark Scrimshire:@ekivemark'
+
 from .base import *
 from platform import python_version
 
@@ -5,23 +18,22 @@ DEBUG = True
 
 SECRET_KEY = "BBOAUTH2-LOCAL-_CHANGE_THIS_FAKE_KEY_TO_YOUR_OWN_SECRET_KEY"
 
-if DEBUG:
-    print("Managers:", MANAGERS)
-    
 # define app managers 
 ADMINS = (
     ('Mark Scrimshire', 'mark@ekivemark.com'),
 )
+MANAGERS = ADMINS
 
+ALLOWED_HOSTS = ['*']
 
 if DEBUG:
     print("==========================================================")
     # APPLICATION_TITLE is set in .base
     print(APPLICATION_TITLE)
+    # SETTINGS_MODE should be set in base to DJANGO_SETTINGS_MODULE
+    print("Mode:", SETTINGS_MODE)
     print("running on", python_version())
-    # checking if MANAGERS gets updated or do we needed to 
-    # update MANAGERS too
-    # in which case we should add note to base.py to make sure
-    # these are set in the custom settings file
+    # We should add note to base.py to make sure
+    # ADMINS and MANAGERS are set in the custom settings file
     print("Application Managers:", MANAGERS)
     print("==========================================================")
--- _start/settings/examples/production.py
+++ _start/settings/examples/production.py
@@ -3,3 +3,25 @@ from .base import *
 DEBUG = True
 
 SECRET_KEY = "BBOAUTH2-LOCAL-_CHANGE_THIS_FAKE_KEY_TO_YOUR_OWN_SECRET_KEY"
+
+# define app managers
+ADMINS = (
+    ('Mark Scrimshire', 'mark@ekivemark.com'),
+)
+MANAGERS = ADMINS
+
+ALLOWED_HOSTS = ['*']
+
+if DEBUG:
+    print("==========================================================")
+    # APPLICATION_TITLE is set in .base
+    print(APPLICATION_TITLE)
+    # SETTINGS_MODE should be set in base to DJANGO_SETTINGS_MODULE
+    print("Mode:", SETTINGS_MODE)
+    print("running on", python_version())
+    # checking if MANAGERS gets updated or do we needed to
+    # update MANAGERS too
+    # in which case we should add note to base.py to make sure
+    # these are set in the custom settings file
+    print("Application Managers:", MANAGERS)
+    print("==========================================================")
