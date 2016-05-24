3f3793b00222114f01a055625301b158862e8f3a
Alan Viars <aviars@videntity.com>
2016-05-24 11:35:12 -0400

    Adding password reset by email.

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,7 +4,7 @@
 from django.conf.urls import include, url
 from .views.core import (simple_login, create, account_settings, reissue_api_keys,
                          request_invite, mylogout, forgot_password,
-                         reset_password,  display_api_keys,
+                         password_reset_email_verify,  display_api_keys,
                          send_activation_key_via_email, activation_verify)
 
 urlpatterns = [
@@ -18,23 +18,20 @@ urlpatterns = [
     url(r'settings$', account_settings, name='account_settings'),
     
     #Request an invite to signup ---------------------------
-    url(r'request-invite', request_invite,
-        name="request_invite"),
+    url(r'request-invite', request_invite, name="request_invite"),
+    
     #Forgot password? ---------------------------------------
-    url(r'forgot-password$', forgot_password,
-        name='forgot_password'),
+    url(r'forgot-password$', forgot_password, name='forgot_password'),
 
     #Change password using reset token ------------------------
-    url(r'reset-password/(?P<reset_password_key>[^/]+)/', reset_password,
-        name='reset_password'),
+    url(r'password-reset-email-verify/(?P<reset_password_key>[^/]+)/', password_reset_email_verify,
+        name='password_reset_email_verify'),
 
     #Verify the account
     url(r'activation-verify/(?P<activation_key>[^/]+)/', activation_verify,
         name='activation_verify'),
     
-    url(r'display-api-keys$', display_api_keys,
-        name='display_api_keys'),
+    url(r'display-api-keys$', display_api_keys, name='display_api_keys'),
 
-    url(r'reissue-api-keys$', reissue_api_keys,
-        name='reissue_api_keys'),
+    url(r'reissue-api-keys$', reissue_api_keys, name='reissue_api_keys'),
 ]
