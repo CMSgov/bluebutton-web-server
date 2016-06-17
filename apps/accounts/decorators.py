fed856b5afcc8a46d51d2228e151195ef91a7b0a
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-06-17 16:10:58 +0200

    flake8 accounts

--- apps/accounts/decorators.py
+++ apps/accounts/decorators.py
@@ -1,49 +1,38 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim: ai ts=4 sts=4 et sw=4
-
-
 """
-    Decorator to check for credentials before responding on API requests.
-    REsponse with JSON instead of standard login redirect.
+Decorator to check for credentials before responding on API requests.
+REsponse with JSON instead of standard login redirect.
 """
 
-import json, string, random
-import urlparse
+import json
 
-from django.conf import settings
-from django.contrib.auth import login, authenticate, REDIRECT_FIELD_NAME
-from django.core.urlresolvers import reverse
-from django.http import HttpResponse, HttpResponseRedirect
-from django.utils.decorators import available_attrs
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponse
 
-from datetime import date
-from functools import update_wrapper, wraps
+from functools import update_wrapper
 
 from .httpauth import HttpBasicAuthentication
 
 
 def json_login_required(func):
     """
-        Put this decorator before your view to check if the user is logged in
-        and return a JSON 401 error if he/she is not.
+    Put this decorator before your view to check if the user is logged in
+    and return a JSON 401 error if he/she is not.
     """
 
     def wrapper(request, *args, **kwargs):
-        user= None
-        #get the Basic username and password from the request.
+        user = None
+        # get the Basic username and password from the request.
         auth_string = request.META.get('HTTP_AUTHORIZATION', None)
 
         if auth_string:
-            (authmeth, auth) = auth_string.split(" ", 1)
+            (authmeth, auth) = auth_string.split(' ', 1)
             auth = auth.strip().decode('base64')
             (username, password) = auth.split(':', 1)
 
             user = authenticate(username=username, password=password)
 
         if not user or not user.is_active:
-            return HttpResponse(unauthorized_json_response(),
-                    mimetype="application/json")
+            return HttpResponse(unauthorized_json_response(), mimetype='application/json')
         login(request, user)
         return func(request, *args, **kwargs)
 
@@ -51,24 +40,27 @@ def json_login_required(func):
 
 
 def authorize(request):
-    a=HttpBasicAuthentication()
+    a = HttpBasicAuthentication()
     if a.is_authenticated(request):
-        login(request,request.user)
-        auth=True
+        login(request, request.user)
+        auth = True
     else:
         if request.user.is_authenticated():
-            auth=True
+            auth = True
         else:
-            auth=False
+            auth = False
     return auth
 
 
 def unauthorized_json_response(additional_info=None):
-    body={"code": 401,
-          "message": "Unauthorized - Your account credentials were invalid.",
-          "errors": [ "Unauthorized - Your account credentials were invalid.", ]
-          }
+    body = {
+        'code': 401,
+        'message': 'Unauthorized - Your account credentials were invalid.',
+        'errors': ['Unauthorized - Your account credentials were invalid.']
+    }
+
     if additional_info:
-        body['message']="%s %s" % (body['message'], additional_info)
-    body=json.dumps(body, indent=4, )
+        body['message'] = '%s %s' % (body['message'], additional_info)
+    body = json.dumps(body, indent=4)
+
     return body
--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,16 +1,12 @@
-#!/usr/bin/env python
 from random import randint
 
 from django import forms
-from django.contrib.auth.models import User
-from django.conf import settings
-from django.contrib.auth.models import Group
-from django.core.mail import mail_admins
-from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth.password_validation import validate_password
-from .models import Invitation, RequestInvite, UserProfile, create_activation_key
-from .emails import send_activation_key_via_email
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.password_validation import validate_password
+from django.utils.translation import ugettext_lazy as _
+
+from .models import Invitation, RequestInvite, UserProfile, create_activation_key
 
 
 class RequestDeveloperInviteForm(forms.ModelForm):
@@ -18,20 +14,22 @@ class RequestDeveloperInviteForm(forms.ModelForm):
         model = RequestInvite
         fields = ('first_name', 'last_name', 'organization', 'email')
 
-    user_type = "DEV"
-    human_x = randint(1,9)
-    human_y = randint(1,9)
+    user_type = 'DEV'
+    human_x = randint(1, 9)
+    human_y = randint(1, 9)
     human_z = human_x + human_y
-    human_q = ("What is %s + %s?" % (human_x,human_y))
-    human = forms.CharField(max_length=30, label=_(human_q),
-            help_text = "We are asking this to make sure you are human. Hint: "
-                        "the answer is %s." % human_z)
+    human_q = ('What is %s + %s?' % (human_x, human_y))
+    human = forms.CharField(
+        max_length=30,
+        label=_(human_q),
+        help_text='We are asking this to make sure you are human. Hint: the answer is %s.' % human_z,
+    )
     required_css_class = 'required'
 
     def clean_human(self):
-        human = self.cleaned_data.get("human", "")
+        human = self.cleaned_data.get('human', '')
         if str(human) != str(self.human_z):
-            raise forms.ValidationError(_("You are either not human or just just really bad at math."))
+            raise forms.ValidationError(_('You are either not human or just just really bad at math.'))
         return human
 
 
@@ -40,51 +38,55 @@ class RequestUserInviteForm(forms.ModelForm):
         model = RequestInvite
         fields = ('first_name', 'last_name', 'email')
 
-    user_type = "BEN"
-    human_x = randint(1,9)
-    human_y = randint(1,9)
+    user_type = 'BEN'
+    human_x = randint(1, 9)
+    human_y = randint(1, 9)
     human_z = human_x + human_y
-    human_q = ("What is %s + %s?" % (human_x,human_y))
-    human = forms.CharField(max_length=30, label=_(human_q),
-            help_text = "We are asking this to make sure you are human. Hint: "
-                        "the answer is %s." % human_z)
+    human_q = ('What is %s + %s?' % (human_x, human_y))
+    human = forms.CharField(
+        max_length=30,
+        label=_(human_q),
+        help_text='We are asking this to make sure you are human. Hint: the answer is %s.' % human_z
+    )
     required_css_class = 'required'
 
     def clean_human(self):
-        human = self.cleaned_data.get("human", "")
+        human = self.cleaned_data.get('human', '')
         if str(human) != str(self.human_z):
-            raise forms.ValidationError(_("You are either not human or just just really bad at math."))
+            raise forms.ValidationError(_('You are either not human or just just really bad at math.'))
         return human
 
 
 class PasswordResetRequestForm(forms.Form):
-    email= forms.CharField(max_length=75, label=_("Email"))
+    email = forms.CharField(max_length=75, label=_('Email'))
     required_css_class = 'required'
-    
+
+
 class PasswordResetForm(forms.Form):
     password1 = forms.CharField(widget=forms.PasswordInput, max_length=30,
-                                label=_("Password*"))
+                                label=_('Password*'))
     password2 = forms.CharField(widget=forms.PasswordInput, max_length=30,
-                                label=_("Password (again)*"))
+                                label=_('Password (again)*'))
 
     required_css_class = 'required'
+
     def clean_password2(self):
-        password1 = self.cleaned_data.get("password1", "")
-        password2 = self.cleaned_data["password2"]
+        password1 = self.cleaned_data.get('password1', '')
+        password2 = self.cleaned_data['password2']
         if password1 != password2:
-            raise forms.ValidationError(_("The two password fields didn't match."))
-        
+            raise forms.ValidationError(_('The two password fields didn\'t match.'))
+
         try:
             validate_password(password1)
-        except ValidationError as err:            
+        except ValidationError as err:
             raise forms.ValidationError(err.error_list[0])
         return password2
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=30, label=_("User"))
+    username = forms.CharField(max_length=30, label=_('User'))
     password = forms.CharField(widget=forms.PasswordInput, max_length=30,
-                               label=_("Password"))
+                               label=_('Password'))
     required_css_class = 'required'
 
 
@@ -102,28 +104,26 @@ class SignupUserForm(forms.Form,):
     #                                      required=False
     #                                      )
     password1 = forms.CharField(widget=forms.PasswordInput, max_length=30,
-                                label=_("Password"))
+                                label=_('Password'))
     password2 = forms.CharField(widget=forms.PasswordInput, max_length=30,
-                                label=_("Password (again)"))
-    
+                                label=_('Password (again)'))
     required_css_class = 'required'
 
-
     def clean_password2(self):
-        password1 = self.cleaned_data.get("password1", "")
-        password2 = self.cleaned_data["password2"]
+        password1 = self.cleaned_data.get('password1', '')
+        password2 = self.cleaned_data['password2']
         if password1 != password2:
-            raise forms.ValidationError(_("The two password fields didn't match."))
-        
+            raise forms.ValidationError(_('The two password fields didn\'t match.'))
+
         try:
             validate_password(password1)
-        except ValidationError as err:            
+        except ValidationError as err:
             raise forms.ValidationError(err.error_list[0])
-            
+
         return password2
-    
+
     def clean_email(self):
-        email = self.cleaned_data.get('email', "")
+        email = self.cleaned_data.get('email', '')
         if email:
             username = self.cleaned_data.get('username')
             if email and User.objects.filter(email=email).exclude(username=username).count():
@@ -134,42 +134,41 @@ class SignupUserForm(forms.Form,):
 
     def clean_username(self):
         username = self.cleaned_data.get('username')
-        if User.objects.filter(username=username).count()>0:
+        if User.objects.filter(username=username).count() > 0:
             raise forms.ValidationError(_('This username is already taken.'))
         return username
-    
+
     def clean_invitation_code(self):
         invitation_code = self.cleaned_data['invitation_code']
-        if Invitation.objects.filter(valid=True, code = invitation_code).count() != 1:
+        if Invitation.objects.filter(valid=True, code=invitation_code).count() != 1:
             raise forms.ValidationError(_('The invitation code is not valid.'))
         return invitation_code
 
-
     def save(self):
-        
         invitation_code = self.cleaned_data['invitation_code']
-        #make the invitation a invalid/spent.
+        # make the invitation a invalid/spent.
         invite = Invitation.objects.get(code=str(invitation_code), valid=True)
-        invite.valid=False
+        invite.valid = False
         invite.save()
-            
+
         new_user = User.objects.create_user(
-                        username=self.cleaned_data['username'],
-                        first_name=self.cleaned_data['first_name'],
-                        last_name=self.cleaned_data['last_name'],
-                        password=self.cleaned_data['password1'],
-                        email=self.cleaned_data['email'],
-                        is_active=False)
+            username=self.cleaned_data['username'],
+            first_name=self.cleaned_data['first_name'],
+            last_name=self.cleaned_data['last_name'],
+            password=self.cleaned_data['password1'],
+            email=self.cleaned_data['email'],
+            is_active=False,
+        )
         
         up = UserProfile.objects.create(user=new_user,
-                                        user_type="BEN")
+                                        user_type='BEN')
 
         group = Group.objects.get(name='BlueButton')
         new_user.groups.add(group)
-        
-        #Send a verification email
-        ak = create_activation_key(new_user)
-        
+
+        # Send a verification email
+        create_activation_key(new_user)
+
         return new_user
 
 
@@ -256,32 +255,26 @@ class SignupDeveloperForm(forms.Form, ):
         new_user.groups.add(group)
 
         # Send a verification email
-        ak = create_activation_key(new_user)
+        create_activation_key(new_user)
 
         return new_user
 
 
 class AccountSettingsForm(forms.Form):
+    username = forms.CharField(max_length=30, label=_('User Name'))
+    email = forms.CharField(max_length=30, label=_('Email'))
+    first_name = forms.CharField(max_length=100, label=_('First Name'))
+    last_name = forms.CharField(max_length=100, label=_('Last Name'))
+    organization_name = forms.CharField(max_length=100, label=_('Organization Name'),
+                                        required=False)
+    required_css_class = 'required'
 
-    username                = forms.CharField(max_length=30, label=_("User Name"))
-    email                   = forms.CharField(max_length=30, label=_("Email"))
-    first_name              = forms.CharField(max_length=100, label=_("First Name"))
-    last_name               = forms.CharField(max_length=100, label=_("Last Name"))
-    organization_name       = forms.CharField(max_length=100, label=_("Organization Name"),
-                                required= False )
-
-    required_css_class      = 'required'
-    
-
-    
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if email:
-            username = self.cleaned_data.get('username')
             if email and User.objects.filter(email=email).exclude(email=email).count():
-                raise forms.ValidationError(_('This email address is already registered.'))   
+                raise forms.ValidationError(_('This email address is already registered.'))
         return email
-
 
     def clean_username(self):
         username = self.cleaned_data.get('username')
