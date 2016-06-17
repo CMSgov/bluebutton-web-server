ab389993c21f8c5ef65ae57e015b792ed8330593
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-06-17 16:10:58 +0200

    flake8 accounts

--- apps/accounts/tests/test_login.py
+++ apps/accounts/tests/test_login.py
@@ -1,11 +1,15 @@
 from django.test import TestCase
-from django.contrib.auth.models import User, Group  
+from django.contrib.auth.models import User, Group
 from django.test.client import Client
 from django.core.urlresolvers import reverse
 from apps.accounts.models import UserProfile
 
+
 class LoginTestCase(TestCase):
-    """Test login and logout"""
+    """
+    Test login and logout
+    """
+
     def _create_user(self, username, password, **extra_fields):
         """
         Helper method that creates a user instance
@@ -13,49 +17,50 @@ class LoginTestCase(TestCase):
         """
         user = User.objects.create_user(username, password=password, **extra_fields)
         return user
-    
-    
+
     def setUp(self):
-        
         self._create_user('fred', 'bedrocks', first_name='Fred',
-                    last_name='Flinstone', email='fred@example.com')
+                          last_name='Flinstone', email='fred@example.com')
         self.client = Client()
         self.url = reverse('login')
-        Group.objects.create(name="BlueButton")
-        
-        
+        Group.objects.create(name='BlueButton')
+
     def test_valid_login(self):
-        """Valid User can login"""
-        form_data = {"username":"fred", "password":"bedrocks"}
+        """
+        Valid User can login
+        """
+        form_data = {'username': 'fred', 'password': 'bedrocks'}
         response = self.client.post(self.url, form_data, follow=True)
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Logout")
-
+        self.assertContains(response, 'Logout')
 
     def test_invalid_login(self):
-        """Invalid user cannot login"""
-        form_data = {"username":"fred", "password":"dino"}
+        """
+        Invalid user cannot login
+        """
+        form_data = {'username': 'fred', 'password': 'dino'}
         response = self.client.post(self.url, form_data, follow=True)
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Login")
-        
-        
+        self.assertContains(response, 'Login')
+
     def test_logout(self):
-        """User can logout"""
-        self.client.login(username="fred", password="bedrocks")
+        """
+        User can logout
+        """
+        self.client.login(username='fred', password='bedrocks')
         response = self.client.get(reverse('mylogout'), follow=True)
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Login")
-        
-        
+        self.assertContains(response, 'Login')
+
     def test_external_auth_results_in_usertype_benny(self):
-        """When user is authenticated by external source then presumed a benny"""
-        form_data = {"username":"ben", "password":"bluebutton"}
+        """
+        When user is authenticated by external source then presumed a benny
+        """
+        form_data = {'username': 'ben', 'password': 'bluebutton'}
         response = self.client.post(self.url, form_data, follow=True)
-        up = UserProfile.objects.get(user__username="ben")
+        up = UserProfile.objects.get(user__username='ben')
         # User is a beneficiary ()
-        self.assertEqual(up.user_type, "BEN")
-        #User is not yet active.  Pending activation.
-        self.assertContains(response, "Please check your email")
+        self.assertEqual(up.user_type, 'BEN')
+        # User is not yet active. Pending activation.
+        self.assertContains(response, 'Please check your email')
         self.assertEqual(up.user.is_active, False)
-        
--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from django.conf.urls import url
 
-from django.conf.urls import include, url
 from .views.core import (simple_login, create_user,
                          create_developer, account_settings,
                          reissue_api_keys, request_developer_invite,
@@ -10,16 +8,15 @@ from .views.core import (simple_login, create_user,
                          send_activation_key_via_email, activation_verify)
 
 urlpatterns = [
-   
     # login and Logout ------------------------------------
-    url(r'login', simple_login,  name="login"),
+    url(r'login', simple_login, name='login'),
     url(r'logout$', mylogout, name='mylogout'),
     
     # create and update account info -----------------------
     url(r'create-user', create_user,  name="accounts_create_user"),
     url(r'create-developer', create_developer, name="accounts_create_developer"),
     url(r'settings$', account_settings, name='account_settings'),
-    
+
     # Request a Developer invite to signup ---------------------------
     url(r'request-developer-invite', request_developer_invite, name="request_developer_invite"),
     # Request an invite to signup ---------------------------
@@ -36,10 +33,8 @@ urlpatterns = [
     # Verify the account
     url(r'activation-verify/(?P<activation_key>[^/]+)/', activation_verify,
         name='activation_verify'),
-    
+
     url(r'display-api-keys$', display_api_keys, name='display_api_keys'),
 
     url(r'reissue-api-keys$', reissue_api_keys, name='reissue_api_keys'),
-
-
 ]
