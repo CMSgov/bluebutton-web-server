130d79e1fa7ea9654801070c78b0a9723592dcb1
Alan Viars <aviars@videntity.com>
2016-07-12 14:35:30 -0400

    Skipping a test in TravisCI. Flake8

--- apps/accounts/tests/test_login.py
+++ apps/accounts/tests/test_login.py
@@ -4,6 +4,7 @@ from django.test.client import Client
 from django.core.urlresolvers import reverse
 from apps.accounts.models import UserProfile
 from getenv import env
+from unittest import skipUnless
 
 
 class LoginTestCase(TestCase):
@@ -55,7 +56,7 @@ class LoginTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Login')
 
-    def test_external_auth_results_in_usertype_benny(self):
+    def test_settings_auth_results_in_usertype_benny(self):
         """
         When user is authenticated by external source then presumed a benny
         password =
@@ -65,7 +66,6 @@ class LoginTestCase(TestCase):
         set with
         export DJANGO_SLS_PASSWORD='pbkdf2_sha256$24000$V6XjGqYYNGY7$13tFC13aa
         TohxBgP2W3glTBz6PSbQN4l6HmUtxQrUys='
-
         """
         form_data = {'username': 'ben',
                      'password': 'bluebutton'}
@@ -79,15 +79,19 @@ class LoginTestCase(TestCase):
         self.assertContains(response, 'Please check your email')
         self.assertEqual(up.user.is_active, False)
 
-        def test_mymedicare_valid_login_succeeds(self):
-            """
-            Test Mymedicare valid login succeeds.
-            """
+    @skipUnless(env('MM_USER', ''), "Requires real MyMedicare credentials. Test locally")
+    def test_mymedicare_valid_login_succeeds(self):
+        """
+        Test MyMedicare valid login succeeds.
+        When user is authenticated by MyMedicare, then user is presumed a benny
+        """
         form_data = {'username': env('MM_USER', 'CHANGE_ME'),
                      'password': env('MM_PASS', 'CHANGE_ME')}
         response = self.client.post(self.url,
                                     form_data,
                                     follow=True)
         up = UserProfile.objects.get(user__username=env('MM_USER', 'CHANGE_ME'))
-        # User is a beneficiary ()
+        # This user is a beneficiary.
         self.assertEqual(up.user_type, 'BEN')
+        # The user is logged in
+        self.assertContains(response, "Logout")
