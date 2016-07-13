3b6f354b00294ae5481a7bf2e87366d104776826
Alan Viars <aviars@videntity.com>
2016-07-13 15:20:48 -0400

    Add user profile to context in authenticated_home

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.utils.translation import ugettext_lazy as _
+from ..accounts.models import UserProfile
 
 
 def authenticated_home(request):
     if request.user.is_authenticated():
         name = _('Authenticated Home')
+        try:
+            profile = UserProfile.objects.get(user=request.user)
+        except UserProfile.DoesNotExist:
+            profile = None
+
         # this is a GET
-        context = {'name': name}
+        context = {'name': name, 'profile': profile}
         template = 'authenticated-home.html'
     else:
         name = ('home')
