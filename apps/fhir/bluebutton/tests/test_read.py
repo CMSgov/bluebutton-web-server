416bdea45fedb3a58d53f74d4d9a301ff3ba5973
Mark Scrimshire <ekivemark>
2016-07-25 12:24:35 -0400

    flake8 pass Document Mock in test_read.py Cleanup test_mock.py

--- apps/fhir/bluebutton/tests/test_read.py
+++ apps/fhir/bluebutton/tests/test_read.py
@@ -1,26 +1,15 @@
 try:
-    # python 3
+    # python 3 - Mock is now a standard module in unittest
     from unittest.mock import Mock, patch
 except ImportError:
-    # python 2
+    # python 2 - Mock needs to be pip installed
     from mock import Mock, patch   # NOQA
 
 import apps.fhir.bluebutton.utils
 from django.test import TestCase, RequestFactory
 
+# Get the pre-defined Conformance statement
 from .data_conformance import CONFORMANCE
-
-# from django.contrib.auth.models import User, Group
-# from django.test.client import Client
-# from django.core.urlresolvers import reverse
-# import responses
-# import requests
-#
-# import unittest
-#
-# from .data_conformance import CONFORMANCE
-#
-# from ..views.home import fhir_conformance
 
 
 class ConformanceReadRequestTest(TestCase):
@@ -35,19 +24,29 @@ class ConformanceReadRequestTest(TestCase):
 
     @patch('apps.fhir.bluebutton.utils.requests')
     def test_fhir_bluebutton_read_conformance_testcase(self, mock_requests):
-        """ Checking Conformance """
+        """ Checking Conformance
+
+            The @patch replaces the call to requests ith mock_requests
+
+        """
 
         call_to = '/bluebutton/fhir/v1/metadata'
         request = self.factory.get(call_to)
 
+        # Now we can setup the responses we want to the call
         mock_requests.get.return_value.status_code = 200
         mock_requests.get.return_value.text = CONFORMANCE
         mock_requests.get.return_value.json = {"field": "My text is here!!!!"}
 
+        # Make the call to request_call which uses requests.get
+        # patch will intercept the call to requests.get and
+        # return the pre-defined values
         result = apps.fhir.bluebutton.utils.request_call(request,
                                                          call_to,
                                                          fail_redirect="/")
 
-        print("\nText:%s" % result.text)
+        # Activate the print statement if you want to see what was returned
+        # print("\nText:%s" % result.text)
 
+        # Test for a match
         self.assertEqual(result.text, CONFORMANCE)
