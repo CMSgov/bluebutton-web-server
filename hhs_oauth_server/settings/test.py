22bb599f40144294225a3508de7441c5d7708ef8
Mark Scrimshire <ekivemark>
2016-07-26 15:53:00 -0400

    Fixing duplicate entry for apps.fhir.testac. application labels aren't unique error for 'testac'

--- hhs_oauth_server/settings/test.py
+++ hhs_oauth_server/settings/test.py
@@ -1,9 +1,11 @@
 from .dev import *
 
 # Add testac to Test environments only
-INSTALLED_APPS = INSTALLED_APPS + [
-    'apps.fhir.testac',
-]
+if  'apps.fhir.testac' not in INSTALLED_APPS:
+    INSTALLED_APPS = INSTALLED_APPS + [
+        'apps.fhir.testac',
+    ]
+
 
 # disabling loggers
 del LOGGING['loggers']
