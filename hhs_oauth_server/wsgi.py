a0140ad66368d481d62f17d7fb14d38cd3559e2d
Mark Scrimshire <mscrimshire+github@gmail.com>
2016-08-17 12:05:15 -0400

    wsgi.py updated to enable local env vars to be set - Enables machine specific configuration - details provided in gist: https://gist.github.com/ekivemark/27595ca635f780b5360f1ca78571a572 - will check for custom-envvars.py in parent folder above python application - If found will check for environment variable and set if not found. - Use custom-envvars.py to set environment specific and sensitive settings - custom-envvars is NOT in the repo. Content is documented in Gist.

--- hhs_oauth_server/wsgi.py
+++ hhs_oauth_server/wsgi.py
@@ -1,4 +1,26 @@
+import os
+from getenv import env
 from django.core.wsgi import get_wsgi_application
 
+
+DJANGO_CUSTOM_SETTINGS_DIR = env("DJANGO_CUSTOM_SETTINGS_DIR", '..')
+EXEC_FILE = os.path.join(DJANGO_CUSTOM_SETTINGS_DIR, 'custom-envvars.py')
+
+# check if custom-envvars.py exists
+# If it does then run it
+if os.path.isfile(EXEC_FILE):
+
+    # Python2
+    try:
+        execfile(EXEC_FILE)
+    except NameError:
+        # Python3 - execfile call format changed
+        exec(open(EXEC_FILE).read())
+
+else:
+    print("no custom variables set:[%s] - Not Found" % EXEC_FILE)
+# If custom-envvars or web server didn't pre-set DJANGO_SETTINGS_MODULE
+# then we set it to the default
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hhs_oauth_server.settings.base")
 
 application = get_wsgi_application()
