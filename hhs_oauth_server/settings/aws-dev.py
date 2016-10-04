458cea47e22e0421bfb7f1a81845cff5c0f2d49d
Mark Scrimshire <mscrimshire+github@gmail.com>
2016-10-03 22:22:25 -0400

    trying to replace_normalize_name in storages.backends.s3boto3.S3BotoStorage

--- hhs_oauth_server/settings/aws-dev.py
+++ hhs_oauth_server/settings/aws-dev.py
@@ -52,12 +52,12 @@ AWS_S3_CUSTOM_DOMAIN = 's3.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME
 
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'hhs_oauth_server.s3_storage.StaticStorage'
-STATIC_URL = "https:///%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 # STATIC_URL = '/static/'
 
 MEDIAFILES_LOCATION = 'media'
 DEAFULT_FILE_STORAGE = 'hhs_oauth_server.s3_storage.MediaStorage'
-MEDIA_URL = "https:///%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 # MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(ASSETS_ROOT, 'collectedstatic')
