98e02ace6203df92ae7392af0a251f9fc6ff0d6b
Mark Scrimshire <mscrimshire+github@gmail.com>
2016-10-09 12:37:46 -0400

    added test for fhir_consent.status()

--- apps/fhir/fhir_consent/tests.py
+++ apps/fhir/fhir_consent/tests.py
@@ -1,9 +1,13 @@
-from django.test import TestCase, RequestFactory
+# from django.test import TestCase
 from apps.test import BaseApiTest
-from django.contrib.auth.models import Group, User
+from django.contrib.auth.models import User
 
 from apps.accounts.models import UserProfile
 from .models import fhir_Consent
+
+import logging
+logger = logging.getLogger('hhs_server.%s' % __name__)
+
 
 class FHIR_ConsentResourceActionTest(BaseApiTest):
     """ Create a Consent Record """
@@ -21,20 +25,19 @@ class FHIR_ConsentResourceActionTest(BaseApiTest):
                                    create_applications=True)
 
         app = self._create_application('ThePHR', user=u)
-        print("\n APP:%s" %app)
+        logger.debug("\n APP:%s" % app)
 
         create_consent = fhir_Consent()
         create_consent.user = u
         create_consent.application = app
         create_consent.save()
 
-        print("\n created Consent:%s" % create_consent)
-        print("\nKey:%s" % create_consent.key)
+        logger.debug("\n created Consent:%s" % create_consent)
+        logger.debug("\nKey:%s" % create_consent.key)
         expected = True
         result = create_consent.granted()
 
         self.assertEqual(result, expected)
-
 
     def test_createConsentCheckRevoked(self):
         """ Check for revoked """
@@ -56,16 +59,51 @@ class FHIR_ConsentResourceActionTest(BaseApiTest):
         create_consent.application = app
         create_consent.save()
 
-
-        print("\n created Consent:%s" % create_consent)
-        print("\nKey:%s" % create_consent.key)
+        logger.debug("\n created Consent:%s" % create_consent)
+        logger.debug("\nKey:%s" % create_consent.key)
 
         revocation = create_consent.revoke_consent(True)
-        print("\nRevoked:%s" % create_consent.revoked.strftime("%Y-%M-%DT%H:%M:%S"))
-
+        logger.debug("\n Revocation:%s" % revocation)
+        logger.debug("\nRevoked:%s" % create_consent.revoked.strftime("%Y-%M-%DT%H:%M:%S"))
 
         expected = False
         result = create_consent.granted()
 
         self.assertEqual(result, expected)
 
+    def test_createConsentCheckStatus(self):
+        """ Check for Status """
+
+        u = User.objects.create_user(username="billybob",
+                                     first_name="Billybob",
+                                     last_name="Button",
+                                     email='billybob@example.com',
+                                     password="foobar", )
+        UserProfile.objects.create(user=u,
+                                   user_type="DEV",
+                                   create_applications=True)
+
+        app = self._create_application('ThePHR', user=u)
+        print("\n APP:%s" % app)
+
+        create_consent = fhir_Consent()
+        create_consent.user = u
+        create_consent.application = app
+        create_consent.save()
+
+        logger.debug("\n created Consent:%s" % create_consent)
+        logger.debug("\nKey:%s" % create_consent.key)
+
+        granted = create_consent.status()
+        self.assertEqual(granted, "VALID")
+
+        revocation = create_consent.revoke_consent(True)
+        logger.debug("\n Revocation:%s" % revocation)
+        logger.debug("\nRevoked:%s" % create_consent.revoked.strftime("%Y-%M-%DT%H:%M:%S"))
+
+
+        expected = "REVOKED"
+        result = create_consent.status()
+        logger.debug("\nStatus:%s" % result)
+
+        self.assertEqual(result, expected)
