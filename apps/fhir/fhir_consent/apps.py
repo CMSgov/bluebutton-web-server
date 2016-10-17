ab99094e637508b14ecaa32a473bedb8eb193c3c
Mark Scrimshire <mscrimshire+github@gmail.com>
2016-10-17 02:32:26 -0400

    fix signals.py call in apps.py got fhir_connect to work with PY2.7/3.x

--- apps/fhir/fhir_consent/apps.py
+++ apps/fhir/fhir_consent/apps.py
@@ -1,14 +1,16 @@
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
-
+# from apps.fhir.fhir_consent import signals
 # Setting is required in __init__.py
 # default_app_config = 'apps.fhir.fhir_consent.apps.fhir_consentConfig'
 
 
 class fhir_consentConfig(AppConfig):
+    # name = 'apps.fhir.fhir_consent'
     name = 'apps.fhir.fhir_consent'
     verbose_name = _("fhir_consent")
 
     def ready(self):
-        import apps.fhir.fhir_consent.signals  # NOQA
-        # import fhir_consent.signals
+        # import apps.fhir.fhir_consent.signals  # NOQA
+        from apps.fhir.fhir_consent import signals  # NOQA
+        # import signals
