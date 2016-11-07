a5330523641fd49f0ce825588dd6ec33cc79f765
Alan Viars <aviars@videntity.com>
2016-11-07 16:03:16 -0500

    Code cleanup

--- apps/accounts/tests/test_invitations.py
+++ apps/accounts/tests/test_invitations.py
@@ -9,33 +9,6 @@ Created: 8/24/16 12:17 AM
 
 File created by: ''
 """
-import logging
-from django.test import TestCase
+# import logging
+# from django.test import TestCase
 # from django.test.client import Client
-from ..models import InvitesAvailable
-from ..views.core import issue_invite
-
-logger = logging.getLogger('hhs_server.%s' % __name__)
-
-
-class InvitationTest(TestCase):
-    """ Test invitations """
-
-    def test_good_developer(self):
-        iab = InvitesAvailable.objects.create(user_type="BEN",
-                                              issued=0,
-                                              available=3)
-
-        iad = InvitesAvailable.objects.create(user_type="DEV",
-                                              issued=0,
-                                              available=3)
-
-        logger.debug("Ben: %s" % iab)
-        logger.debug("Dev: %s" % iad)
-        result = issue_invite("Developer1@dev.com", user_type="DEV")
-
-        expect = "Developer1@dev.com"
-
-        logger.debug("Result:%s" % result)
-
-        self.assertEqual(result.email, expect)
