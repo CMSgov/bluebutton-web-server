45cf233b612574aac1abfe7170dbe7bb52b3bbae
Alan Viars <aviars@videntity.com>
2017-03-16 13:18:54 -0400

    Adding .wellknown openid configuration URL

--- apps/wellknown/urls.py
+++ apps/wellknown/urls.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
 from django.conf.urls import url
 from .views import openid_configuration
 
 
 urlpatterns = [
     # openid-configuration -----------------------------------
-    url(r'^openid-configuration$', openid_configuration, name='openid-configuration'),
+    url(r'^openid-configuration$',
+        openid_configuration,
+        name='openid-configuration'),
 ]
