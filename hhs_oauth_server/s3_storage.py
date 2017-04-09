ad7a771bd5f2a5b463bb24a288acc486f8c7c330
Mark Scrimshire <mscrimshire+github@gmail.com>
2017-04-08 23:33:59 -0400

    Testing admin_static in admn/base.html
    Adding debug output to hhs_oauth_server.s3_storage.py

--- hhs_oauth_server/s3_storage.py
+++ hhs_oauth_server/s3_storage.py
@@ -14,6 +14,9 @@ https://www.caktusgroup.com/blog/2014/11/10/
 Using-Amazon-S3-to-store-your-Django-sites-static-and-media-files/
 
 """
+import logging
+
+logger = logging.getLogger('hhs_server.%s' % __name__)
 
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
@@ -25,13 +28,16 @@ class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
 
     def _clean_name(self, name):
+        logger.debug("S3 Storage: Cleaning name:%s" % name)
         return name
 
     def _normalize_name(self, name):
         if not name.endswith('/'):
+            logger.debug("S3 Storage: Name has no /:%s" % name)
             name += "/"
 
         name += self.location
+        logger.debug("S3 Storage: Name with location added: %s" % name)
         return name
 
 
