e3f02272389f57684397ca9a1ba0bc7e2fd96f07
Alan Viars <aviars@videntity.com>
2017-05-17 11:03:31 -0400

    Setup database for Identity and Authenticator Assurance levels for NIST SP 800-63-3.

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -27,13 +27,37 @@ USER_CHOICES = (
     ('DEV', 'Developer'),
 )
 
+
+# Enrollment and Identity Proofing. NIST SP 800-63-A
+# Level of Assurance - Legacy/Deprecated  See NIST SP 800-63-2
 LOA_CHOICES = (
-    ('0', 'LOA-0'),
+    ('', 'Undefined'),
     ('1', 'LOA-1'),
     ('2', 'LOA-2'),
     ('3', 'LOA-3'),
     ('4', 'LOA-4'),
 )
+
+# Enrollment and Identity Proofing. NIST SP 800-63-3 A
+# Identity assurance level
+IAL_CHOICES = (
+    ('', 'Undefined'),
+    ('1', 'IAL1'),
+    ('2', 'IAL2'),
+    ('3', 'IAL3'),
+)
+
+
+# Enrollment and Identity Proofing. NIST SP 800-63-33 B
+# Authenticator Assurance Level
+AAL_CHOICES = (
+    ('', 'Undefined'),
+    ('1', 'AAL1'),
+    ('2', 'AAL2'),
+    ('3', 'AAL3'),
+)
+
+
 
 
 QUESTION_1_CHOICES = (
@@ -67,9 +91,27 @@ class UserProfile(models.Model):
     organization_name = models.CharField(max_length=255,
                                          blank=True,
                                          default='')
-    loa = models.CharField(default='0',
+    loa = models.CharField(default='',
                            choices=LOA_CHOICES,
-                           max_length=5)
+                           max_length=1,
+                           blank=True,
+                           verbose_name="Level of Assurance",
+                           help_text = "Legacy and Deprecated. Using IAL AAL is recommended.")
+    
+    ial = models.CharField(default='',
+                           choices=IAL_CHOICES,
+                           max_length=1,
+                           blank=True,
+                           verbose_name = "Identity Assurance Level",
+                           help_text="See NIST SP 800 63 A for definitions.")
+
+    aal = models.CharField(default='1',
+                           choices=AAL_CHOICES,
+                           max_length=1,
+                           blank=True,
+                           verbose_name = "Authenticator Assurance Level",
+                           help_text="See NIST SP 800 63 B for definitions.")
+    
     user_type = models.CharField(default='DEV',
                                  choices=USER_CHOICES,
                                  max_length=5)
@@ -146,6 +188,9 @@ class UserProfile(models.Model):
         return name
 
     def save(self, **kwargs):
+        if self.mfa_login_mode:
+            self.aal = '2'
+        
         if not self.access_key_id or self.access_key_reset:
             self.access_key_id = random_key_id()
             self.access_key_secret = random_secret()
