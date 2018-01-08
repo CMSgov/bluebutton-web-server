a71e614cca810927080843acc05cb26c8d0752cc
Sam Gensburg <sam.gensburg@cms.hhs.gov>
2018-01-08 01:10:52 -0500

    Remove additional pathways to metadata

--- apps/fhir/bluebutton/urls.py
+++ apps/fhir/bluebutton/urls.py
@@ -5,22 +5,12 @@ from apps.fhir.bluebutton.views.history import history
 from apps.fhir.bluebutton.views.read import read
 from apps.fhir.bluebutton.views.vread import vread
 from apps.fhir.bluebutton.views.search import search
-from apps.fhir.bluebutton.views.home import fhir_conformance
 
 admin.autodiscover()
 
 urlpatterns = [
     # URLs with no authentication
     # Interactions on Resources
-
-    # Move Conformance and Capability above general calls
-    # Conformance statement
-    url(r'^metadata[^/]',
-        fhir_conformance, name='bb_fhir_conformance'),
-
-    # Capability statement
-    url(r'^meta[^/]',
-        fhir_conformance, name='bb_fhir_conformance'),
 
     # Vread GET --------------------------------
     url(r'(?P<resource_type>[^/]+)/(?P<id>[^/]+)/_history/(?P<vid>[^/]+)',
