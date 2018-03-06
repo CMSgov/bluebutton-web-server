85e547141c3f9e7dde9c4a02d67f51eabc49f700
Mark Scrimshire <mscrimshire+github@gmail.com>
2018-03-06 06:57:40 -0800

    Enable search and filter on User and Application in AccessToken admin. CBBP-973

--- apps/dot_ext/admin.py
+++ apps/dot_ext/admin.py
@@ -1,4 +1,7 @@
-# from django.contrib import admin
+from django.contrib import admin
+from oauth2_provider.models import AccessToken
+from oauth2_provider.admin import RawIDAdmin
+
 # from oauth2_provider.models import get_application_model
 #
 #
@@ -19,3 +22,25 @@
 #
 #
 # admin.site.register(Application, ApplicationAdmin)
+
+
+class AccessTokenAdmin(admin.ModelAdmin):
+
+    list_display = ('get_user_username', 'get_app_name',)
+    search_fields = ('user__username', 'application__name',)
+    list_filter = ("user__username", "application__name")
+
+    def get_app_name(self, obj):
+        return obj.application.name
+
+    get_app_name.admin_order_field = "name"
+    get_app_name.short_description = "Application Name"
+
+    def get_user_username(self, obj):
+        return obj.user.username
+
+    get_user_username.admin_order_field = "username"
+    get_user_username.short_description = "User Name"
+
+admin.site.unregister(AccessToken)
+admin.site.register(AccessToken, AccessTokenAdmin)
