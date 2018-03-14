2a110fda1a121568bddb22e3caae9dba070e55f3
Alan Viars <aviars@videntity.com>
2018-03-14 09:24:19 -0400

    fix token admin.

--- apps/dot_ext/admin.py
+++ apps/dot_ext/admin.py
@@ -19,7 +19,7 @@ class MyApplication(Application):
 
 class MyApplicationAdmin(admin.ModelAdmin):
 
-    list_display = ("name", "user", "authorization_grant_type", "scopes",
+    list_display = ("user", "authorization_grant_type", "scopes",
                     "skip_authorization", "created", "updated")
     list_filter = ("client_type", "authorization_grant_type",
                    "skip_authorization")
@@ -35,21 +35,10 @@ admin.site.register(MyApplication, MyApplicationAdmin)
 
 class MyAccessTokenAdmin(admin.ModelAdmin):
 
-    list_display = ('get_user_username', 'get_app_name', 'expires', 'scope')
+    list_display = ('user', 'application', 'expires', 'scope')
     search_fields = ('user__username', 'application__name',)
     list_filter = ("user__username", "application__name")
-
-    def get_app_name(self, obj):
-        return obj.application.name
-
-    get_app_name.admin_order_field = "name"
-    get_app_name.short_description = "Application Name"
-
-    def get_user_username(self, obj):
-        return obj.user.username
-
-    get_user_username.admin_order_field = "username"
-    get_user_username.short_description = "User Name"
+    raw_id_fields = ("user", 'application')
 
 
 admin.site.register(MyAccessToken, MyAccessTokenAdmin)
