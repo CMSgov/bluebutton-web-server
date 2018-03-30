a7418bfa7d96c1bba757dbf744490479a5cf126c
Mark Scrimshire <mscrimshire+github@gmail.com>
2018-03-30 13:27:41 -0400

    CBBP-1018: Add keep-alive value via set_default_header using value from base.py.

--- hhs_oauth_server/settings/base.py
+++ hhs_oauth_server/settings/base.py
@@ -423,6 +423,7 @@ FHIR_CLIENT_CERTSTORE = env('DJANGO_FHIR_CERTSTORE',
 # Timeout for request call
 REQUEST_CALL_TIMEOUT = (30, 120)
 # Headers Keep-Alive value
+# this can be over-ridden in aws-{env}.py file to set values per environment
 REQUEST_EOB_KEEP_ALIVE = "timeout=120, max=10"
 
 SIGNUP_TIMEOUT_DAYS = env('SIGNUP_TIMEOUT_DAYS', 7)
