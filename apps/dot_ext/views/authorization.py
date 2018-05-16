7907dd31068c23d57bbf98a4ca8b1644d500ca8d
David Gage <whytheplatypus@gmail.com>
2018-05-16 10:50:21 -0400

    Create new app approval flow without sessions
    Successful redirect to approval instance
    
    Create application Approval model
    
    Redirect on expired or unauthed approval

--- apps/dot_ext/views/authorization.py
+++ apps/dot_ext/views/authorization.py
@@ -4,7 +4,7 @@ from oauth2_provider.models import get_application_model
 from oauth2_provider.exceptions import OAuthToolkitError
 from oauth2_provider.http import HttpResponseUriRedirect
 from ..forms import AllowForm, SimpleAllowForm
-from ..models import ExpiresIn
+from ..models import ExpiresIn, Approval
 
 logger = logging.getLogger('hhs_server.%s' % __name__)
 
@@ -17,6 +17,39 @@ class AuthorizationView(DotAuthorizationView):
     form_class = SimpleAllowForm
     login_url = "/mymedicare/login"
     template_name = "design_system/authorize.html"
+
+
+class ApprovalView(DotAuthorizationView):
+    """
+    Override the base authorization view from dot to
+    use the custom AllowForm.
+    """
+    form_class = SimpleAllowForm
+    login_url = "/mymedicare/login"
+    template_name = "design_system/authorize.html"
+
+    def dispatch(self, request, uuid, *args, **kwargs):
+        # trows DoesNotExist
+        try:
+            approval = Approval.objects.get(uuid=uuid)
+            if approval.expired:
+                raise Approval.DoesNotExist
+            if approval.application\
+                    and approval.application.client_id != request.GET.get('client_id', None)\
+                    and approval.application.client_id != request.POST.get('client_id', None):
+                raise Approval.DoesNotExist
+            request.user = approval.user
+        except Approval.DoesNotExist:
+            pass
+
+        result = super(ApprovalView, self).dispatch(request, *args, **kwargs)
+
+        application = self.oauth2_data.get('application', None)
+        if application is not None:
+            approval.application = self.oauth2_data.get('application', None)
+            approval.save()
+
+        return result
 
 
 class ScopeAuthorizationView(DotAuthorizationView):
