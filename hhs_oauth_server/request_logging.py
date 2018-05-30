d594f978c7ed4837d60c9c75637bd37004e1996a
David Tisza <36048547+dtisza1@users.noreply.github.com>
2018-05-30 15:35:04 -0400

    BLUEBUTTON-207 Audit log request/response pairs. (#602)
    * BLUEBUTTON-207 Audit log request/response pairs.
    
    - Update request_logging.py middleware to log request/response pair info.
    
    - Added application name/id.
    
    - Added developer name/id.
    
    - Added access_token_hash.
    
    - Added location for 3xx results.
    
    - Audit log info is contained in the top level 'message' field as JSON.
    
    Travis CI fixups
    
    Flake8 formatting fixups.
    
    * Updated logger setup.
    
    - Added 'audit' logger and 'audit_trail' handler.
    
    - Updated request_logging.py to use 'audit' logger.
    
    - Removed 'tag' from logged items.
    
    * BLUEBUTTON-207 Audit log request/response pairs.
    
    * Added a RequestResponseLog class for generating request/response pair JSON string.
    
    * Changed code to use a DICT and json.dumps.
    
    * Misc code structure cleanup.
    
    * BLUEBUTTON-207 Audit log request/response pairs.
    
    * Fixup location and size.
    
    * Removing separate logger. Using original perf_mon instead.
    
    * Move of log_msg defaults for better readability.
    
    * Changed to get access token from request.auth .
    
    * Changed access token hash algo to hashlib.sha256
    
    * Access token set changes.
    
    * Remove resource_owner check from get_access_token_from_request.
    
    * Update to get access token from request.auth or get_access_token_from_request().
    
    * Updated to replace hasattr lines with getattr one liner.
    
    * Cleanup for flake8 imports not used.
    
    * Removed str() from objects that were JSON serializable.
    
    * Add separate audit logger
    
    * Fix flake8 trailing space

--- hhs_oauth_server/request_logging.py
+++ hhs_oauth_server/request_logging.py
@@ -1,8 +1,80 @@
 import logging
 import datetime
 import uuid
+import hashlib
+import json
+from apps.fhir.bluebutton.utils import (get_ip_from_request,
+                                        get_user_from_request,
+                                        get_access_token_from_request)
+from oauth2_provider.models import AccessToken
 
-logger = logging.getLogger('performance.%s' % __name__)
+
+audit = logging.getLogger('audit.%s' % __name__)
+
+
+class RequestResponseLog(object):
+    """Audit Log message to JSON string
+
+    The JSON log format contians the following fields:
+        - start_time = Unix Epoch format time of the request processed.
+        - end_time = Unix Epoch format time of the response processed.
+        - request_uuid = The UUID identifying the request.
+        - path = The request.path.
+        - response_code = The response status code.
+        - size = Size in bytes of the response.content
+        - location = Location (redirect) for 300,301,302,307 response codes.
+        - user = Login user (or None) or OAuth2 API.
+        - ip_addr = IP address of the request, account for the possibility of being behind a proxy.
+        - access_token_hash = A hash of the access token.
+        - app_name = Application name.
+        - app_id = Application id.
+        - dev_name = Developer user name.
+        - dev_id = Developer user id.
+    """
+
+    request = None
+    response = None
+
+    def __init__(self, req, resp):
+        self.request = req
+        self.response = resp
+
+    def __str__(self):
+        # Create log message dict
+        log_msg = {}
+        log_msg['start_time'] = self.request._logging_start_dt.timestamp()
+        log_msg['end_time'] = datetime.datetime.utcnow().timestamp()
+        log_msg['request_uuid'] = str(self.request._logging_uuid)
+        log_msg['path'] = self.request.path
+        log_msg['response_code'] = getattr(self.response, 'status_code', 0)
+        log_msg['size'] = ""
+        log_msg['location'] = ""
+        log_msg['app_name'] = ""
+        log_msg['app_id'] = ""
+        log_msg['dev_id'] = ""
+        log_msg['dev_name'] = ""
+        log_msg['access_token_hash'] = ""
+
+        if log_msg['response_code'] in (300, 301, 302, 307):
+            log_msg['location'] = self.response.get('Location', '?')
+        elif self.response.content:
+            log_msg['size'] = len(self.response.content)
+
+        log_msg['user'] = str(get_user_from_request(self.request))
+        log_msg['ip_addr'] = get_ip_from_request(self.request)
+
+        access_token = getattr(self.request, 'auth', get_access_token_from_request(self.request))
+
+        if AccessToken.objects.filter(token=access_token).exists():
+            at = AccessToken.objects.get(token=access_token)
+            log_msg['app_name'] = at.application.name
+            log_msg['app_id'] = at.application.id
+            log_msg['dev_id'] = at.application.user.id
+            log_msg['dev_name'] = str(at.application.user)
+            log_msg['access_token_hash'] = hashlib.sha256(str(access_token).encode('utf-8')).hexdigest()
+
+        return(json.dumps(log_msg))
+
 
 ##############################################################################
 #
@@ -26,53 +98,15 @@ class RequestTimeLoggingMiddleware(object):
     """
 
     @staticmethod
-    def log_message(request, tag, message=''):
-        """Log timing message to stderr.
-
-        Logs message about `request' with a `tag' (a string, 10
-        characters or less if possible), timing info and optional
-        `message'.
-
-        Log format is "timestamp tag uuid count path +delta message"
-        - timestamp is microsecond timestamp of message
-        - tag is the `tag' parameter
-        - uuid is the UUID identifying request
-        - count is number of logged message for this request
-        - path is request.path
-        - delta is timedelta between first logged message
-          for this request and current message
-        - message is the `message' parameter.
-        """
-
-        dt = datetime.datetime.utcnow()
-        if not hasattr(request, '_logging_uuid'):
-            request._logging_uuid = uuid.uuid1()
-            request._logging_start_dt = dt
-            request._logging_pass = 0
-
+    def log_message(request, response):
+        audit.info(RequestResponseLog(request, response))
         request._logging_pass += 1
 
-        # Replace this with logging output
-
-        logger.info('%s %-10s %s %2d %s +%s %s' % (
-                    dt.isoformat(),
-                    tag,
-                    request._logging_uuid,
-                    request._logging_pass,
-                    request.path,
-                    dt - request._logging_start_dt,
-                    message)
-                    )
-
     def process_request(self, request):
-        self.log_message(request, 'request ')
+        request._logging_uuid = uuid.uuid1()
+        request._logging_start_dt = datetime.datetime.utcnow()
+        request._logging_pass = 1
 
     def process_response(self, request, response):
-        s = getattr(response, 'status_code', 0)
-        r = str(s)
-        if s in (300, 301, 302, 307):
-            r += ' => %s' % response.get('Location', '?')
-        elif response.content:
-            r += ' (%db)' % len(response.content)
-        self.log_message(request, 'response', r)
+        self.log_message(request, response)
         return response
