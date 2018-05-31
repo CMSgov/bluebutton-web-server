983187905c30297db476dba0438eea5b7b04865d
David Gage <whytheplatypus@gmail.com>
2018-05-31 11:43:44 -0400

    BLUEBUTTON-170 Remove unused scope-authorize path (#607)

--- apps/dot_ext/forms.py
+++ apps/dot_ext/forms.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 from oauth2_provider.forms import AllowForm as DotAllowForm
 from oauth2_provider.models import get_application_model
-from oauth2_provider.scopes import get_scopes_backend
 from oauth2_provider.settings import oauth2_settings
 from oauth2_provider.validators import urlsplit
 
@@ -116,38 +115,6 @@ class CustomRegisterApplicationForm(forms.ModelForm):
 
 class SimpleAllowForm(DotAllowForm):
     pass
-
-
-class AllowForm(DotAllowForm):
-    scope = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple)
-    expires_in = forms.TypedChoiceField(choices=settings.DOT_EXPIRES_IN, coerce=int,
-                                        empty_value=None,
-                                        label="Access to this application expires in")
-
-    def __init__(self, *args, **kwargs):
-        application = kwargs.pop('application', None)
-
-        if application is None:
-            super(AllowForm, self).__init__(*args, **kwargs)
-        else:
-            # we use the application instance to get the list of available scopes
-            # because it is needed to create the choices list for the `scope`
-            # field.
-            available_scopes = get_scopes_backend().get_available_scopes(application)
-
-            # set the available_scopes as the initial value so that
-            # all checkboxes are checked
-            kwargs['initial']['scope'] = available_scopes
-
-            # init the form to create self.fields
-            super(AllowForm, self).__init__(*args, **kwargs)
-
-            # get the list of all the scopes available in the system
-            # to get the description of each available scope.
-            all_scopes = get_scopes_backend().get_all_scopes()
-            choices = [(scope, all_scopes[scope])
-                       for scope in available_scopes]
-            self.fields['scope'].choices = choices
 
 
 def get_allowed_schemes():
