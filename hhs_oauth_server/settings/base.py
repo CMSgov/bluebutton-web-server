9d39abe320fe0c9fbc0b78ceed759ce78b0aeb85
David Gage <whytheplatypus@gmail.com>
2018-05-31 11:24:18 -0400

    BLUEBUTTON-166 Remove unused social-auth behavior (#604)
    * Remove unused social-auth behavior
    
    * Clean vendored requirements

--- hhs_oauth_server/settings/base.py
+++ hhs_oauth_server/settings/base.py
@@ -73,7 +73,6 @@ INSTALLED_APPS = [
     # 3rd Party ---------------------
     'corsheaders',
     'bootstrapform',
-    'social_django',
     # DOT must be installed after apps.dot_ext in order to override templates
     'oauth2_provider',
     'axes',
@@ -113,7 +112,6 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'social_django.middleware.SocialAuthExceptionMiddleware',
     'apps.dot_ext.throttling.ThrottleMiddleware',
 ]
 
@@ -377,7 +375,6 @@ SETTINGS_EXPORT = [
     'EXPLAINATION_LINE',
     'EXTERNAL_AUTH_NAME',
     'ALLOW_END_USER_EXTERNAL_AUTH',
-    'SOCIAL_AUTH_BACKEND_NAME',
     'OPTIONAL_INSTALLED_APPS',
     'INSTALLED_APPS',
     'REQUIRE_INVITE_TO_REGISTER',
@@ -418,39 +415,8 @@ REQUIRE_AUTHORIZE_APP_FLAG = False
 # Move Admin to a variable url location
 ADMIN_PREPEND_URL = env('DJANGO_ADMIN_PREPEND_URL', '')
 
-# Python Social Auth Settings.
-SOCIAL_AUTH_PIPELINE = (
-    'social_core.pipeline.social_auth.social_details',
-    'social_core.pipeline.social_auth.social_uid',
-    'social_core.pipeline.social_auth.auth_allowed',
-    'social_core.pipeline.social_auth.social_user',
-    'social_core.pipeline.user.get_username',
-    'social_core.pipeline.mail.mail_validation',
-    'social_core.pipeline.user.create_user',
-    'apps.accounts.auth_backends.pipeline.create_user_profile',
-    'apps.fhir.bluebutton.auth_backends.pso_pipeline.set_sample_patient_id',
-    'social_core.pipeline.social_auth.associate_user',
-    'social_core.pipeline.debug.debug',
-    'social_core.pipeline.social_auth.load_extra_data',
-    'social_core.pipeline.user.user_details',
-    'social_core.pipeline.debug.debug'
-)
-
-# Get your own Google Client OAuth2 client and secret.
-# https://console.developers.google.com/apis/credentials
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = ''
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = ''
-# The name of an external oauth2 provider.
 ALLOW_END_USER_EXTERNAL_AUTH = "B"
 EXTERNAL_AUTH_NAME = 'MyMedicare.gov'
-SOCIAL_AUTH_BACKEND_NAME = "google_oauth2"
-
-# python-social-auth settings
-SOCIAL_AUTH_URL_NAMESPACE = 'social'
-
-SOCIAL_AUTH_FIELDS_STORED_IN_SESSION = ['next']
-SOCIAL_AUTH_ALWAYS_ASSOCIATE = True
-
 
 MEDICARE_LOGON = True
 MEDICARE_LOGIN_URI = env('DJANGO_MEDICARE_LOGIN_URI ',
