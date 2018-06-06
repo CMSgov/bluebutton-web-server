aae46d3e34485bcabb6c8d0e8659bba2d4717cae
Dave Tisza <dtisza@fearless.tech>
2018-06-06 16:24:21 -0400

    BLUEBUTTON-209 Add endpoint class views for applications and tokens

--- apps/metrics/views.py
+++ apps/metrics/views.py
@@ -3,6 +3,8 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from ..accounts.models import UserProfile
+from ..dot_ext.models import Application
+from oauth2_provider.models import AccessToken
 
 
 class BeneMetricsView(APIView):
@@ -22,5 +24,47 @@ class BeneMetricsView(APIView):
     def get(self, request, format=None):
         content = {
             'beneficiary_count': UserProfile.objects.filter(user_type='BEN').count()
+        }
+        return Response(content)
+
+
+class AppMetricsView(APIView):
+    """
+    View to provide application metrics.
+
+    * Only admin users are able to access this view.
+    * Default returns count info
+    """
+    permission_classes = (
+        IsAuthenticated,
+        IsAdminUser,
+    )
+
+    renderer_classes = (JSONRenderer, )
+
+    def get(self, request, format=None):
+        content = {
+            'application_count': Application.objects.count()
+        }
+        return Response(content)
+
+
+class TokenMetricsView(APIView):
+    """
+    View to provide access token metrics.
+
+    * Only admin users are able to access this view.
+    * Default returns count info
+    """
+    permission_classes = (
+        IsAuthenticated,
+        IsAdminUser,
+    )
+
+    renderer_classes = (JSONRenderer, )
+
+    def get(self, request, format=None):
+        content = {
+            'access_token_count': AccessToken.objects.count()
         }
         return Response(content)
