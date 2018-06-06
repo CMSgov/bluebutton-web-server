d89417cbaf2aa124c0f93cc5ba1e794c4d23ef85
Dave Tisza <dtisza@fearless.tech>
2018-06-06 15:48:15 -0400

    BLUEBUTTON-209 Redo code to use a class type view for bene end point

--- apps/metrics/urls.py
+++ apps/metrics/urls.py
@@ -1,17 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
-from .views import (total_user_count, active_user_count, staff_user_count,
-                    super_user_count, total_user_profile_count, dev_user_profile_count,
-                    ben_user_profile_count)
+from .views import BeneMetricsView
 
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^total-user-count$', total_user_count, name='total-user-count'),
-    url(r'^active-user-count$', active_user_count, name='active-user-count'),
-    url(r'^staff-user-count$', staff_user_count, name='staff-user-count'),
-    url(r'^super-user-count$', super_user_count, name='super-user-count'),
-    url(r'^total-user-profile-count$', total_user_profile_count, name='total-user-profile-count'),
-    url(r'^dev-user-profile-count$', dev_user_profile_count, name='dev-user-profile-count'),
-    url(r'^ben-user-profile-count$', ben_user_profile_count, name='ben-user-profile-count'),
+    url(r'^beneficiaries$', BeneMetricsView.as_view(), name='beneficiaries'),
 ]
--- apps/metrics/views.py
+++ apps/metrics/views.py
@@ -1,62 +1,26 @@
-from django.views.decorators.cache import never_cache
-from django.http import HttpResponse
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework.decorators import renderer_classes
-from django.contrib.auth.models import User
+from rest_framework.views import APIView
 from ..accounts.models import UserProfile
-# from ..fhir.bluebutton.models import Crosswalk
 
 
-# Metric auth and response
-@never_cache
-@api_view(['GET'])
-@renderer_classes((JSONRenderer,))
-def metric_response(request, content):
-    if request.user.is_authenticated() and request.user.is_staff:
+class BeneMetricsView(APIView):
+    """
+    View to provide beneficiary metrics.
+
+    * Only admin users are able to access this view.
+    * Default returns count info
+    """
+    permission_classes = (
+        IsAuthenticated,
+        IsAdminUser,
+    )
+
+    renderer_classes = (JSONRenderer, )
+
+    def get(self, request, format=None):
+        content = {
+            'beneficiary_count': UserProfile.objects.filter(user_type='BEN').count()
+        }
         return Response(content)
-    else:
-        return HttpResponse('Unauthorized', status=401)
-
-
-# User records count
-def total_user_count(request, format=None):
-        return metric_response(request,
-                               {'total_user_count', User.objects.count()})
-
-
-# User active records count
-def active_user_count(request, format=None):
-        return metric_response(request,
-                               {'active_user_count': User.objects.filter(is_active=True).count()})
-
-
-# User staff records count
-def staff_user_count(request, format=None):
-        return metric_response(request,
-                               {'staff_user_count': User.objects.filter(is_staff=True).count()})
-
-
-# User super-user/admin records count
-def super_user_count(request, format=None):
-        return metric_response(request,
-                               {'admin_user_count': User.objects.filter(is_superuser=True).count()})
-
-
-# UserProfile records count
-def total_user_profile_count(request, format=None):
-        return metric_response(request,
-                               {'total_user_profile_count': UserProfile.objects.count()})
-
-
-# DEV UserProfile records count
-def dev_user_profile_count(request, format=None):
-        return metric_response(request,
-                               {'dev_user_profile_count': UserProfile.objects.filter(user_type='DEV').count()})
-
-
-# BEN UserProfile records count
-def ben_user_profile_count(request, format=None):
-        return metric_response(request,
-                               {'ben_user_profile_count': UserProfile.objects.filter(user_type='BEN').count()})
