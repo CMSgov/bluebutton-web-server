82680f964f81e2ffd30b9d1a30a221380868561c
David Tisza <36048547+dtisza1@users.noreply.github.com>
2018-06-15 12:06:31 -0400

    BLUEBUTTON-208 Add data and unique bene counts to application view (#612)
    * BLUEBUTTON-208 Add data and unique bene counts to application view
    
    * BLUEBUTTON-208 Improve upon serialization/queryset and add pagination
    
    * Improve serializer class for AppMetricsView
    
    * Improve queryset with aggregation annotate clause for distinct Count()
    
    * Add BrowsableAPIRenderer renderer class in addition to JSONRenderer
    
    * BLUEBUTTON-208 Change field namings to their normal keys
    
    * BLUEBUTTON-208 Update to improve to api design standards
    
    * Change beneficiaries count to beneficiaries.count serialization
    
    * Add applications APIView for get detail
    
    * BLUEBUTTON-208 Fix issue with applications detail JSON inside array

--- apps/metrics/views.py
+++ apps/metrics/views.py
@@ -1,10 +1,40 @@
+from django.contrib.auth.models import User
+from oauth2_provider.models import AccessToken
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework.renderers import JSONRenderer
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from ..accounts.models import UserProfile
 from ..dot_ext.models import Application
-from oauth2_provider.models import AccessToken
+
+
+class UserSerializer(ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+
+class AppMetricsSerializer(ModelSerializer):
+
+    beneficiaries = SerializerMethodField()
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Application
+        fields = ('id', 'name', 'active', 'user', 'beneficiaries')
+
+    def get_beneficiaries(self, obj):
+        return({'count': AccessToken.objects.filter(application=obj.id).distinct('user').count()})
+
+
+class AppMetricsPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
 
 
 class BeneMetricsView(APIView):
@@ -28,25 +58,52 @@ class BeneMetricsView(APIView):
         return Response(content)
 
 
-class AppMetricsView(APIView):
+class AppMetricsView(ListAPIView):
     """
     View to provide application metrics.
 
-    * Only admin users are able to access this view.
-    * Default returns count info
+
+    * Only admin users are able to access this view
+    * Default returns count info and application list data with unique bene counts
     """
-    permission_classes = (
+
+    permission_classes = [
         IsAuthenticated,
         IsAdminUser,
-    )
+    ]
 
-    renderer_classes = (JSONRenderer, )
+    renderer_classes = (JSONRenderer, BrowsableAPIRenderer)
+    serializer_class = AppMetricsSerializer
+    pagination_class = AppMetricsPagination
 
-    def get(self, request, format=None):
-        content = {
-            'count': Application.objects.count()
-        }
-        return Response(content)
+    def get_queryset(self):
+
+        queryset = Application.objects.all().order_by('name')
+
+        return queryset
+
+
+class AppMetricsDetailView(APIView):
+    """
+    View to provide application metrics detail.
+
+
+    * Only admin users are able to access this view
+    * Default returns count info and application list data with unique bene count
+    """
+
+    permission_classes = [
+        IsAuthenticated,
+        IsAdminUser,
+    ]
+
+    renderer_classes = (JSONRenderer, BrowsableAPIRenderer)
+
+    def get(self, request, pk, format=None):
+
+        queryset = Application.objects.get(pk=pk)
+
+        return Response(AppMetricsSerializer(queryset).data)
 
 
 class TokenMetricsView(APIView):
