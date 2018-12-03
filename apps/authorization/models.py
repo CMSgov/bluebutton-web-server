78811f93c89deb36dc6380051fd864d8fd179984
David Gage <whytheplatypus@gmail.com>
2018-12-03 10:34:26 -0500

    Record a Grant of access for all approvals
    When a beneficiary gives their approval for an application to access
    their data for the first time, create a record of this grant of access.

---
+++ apps/authorization/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.conf import settings
+from oauth2_provider.settings import oauth2_settings
+
+
+class DataAccessGrant(models.Model):
+    beneficiary = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    application = models.ForeignKey(
+        oauth2_settings.APPLICATION_MODEL,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
---
+++ apps/authorization/test_data_access_grant.py
@@ -0,0 +1,61 @@
+from oauth2_provider.compat import parse_qs, urlparse
+from oauth2_provider.models import get_application_model
+from django.urls import reverse
+from django.test import Client
+
+from apps.test import BaseApiTest
+
+from .models import DataAccessGrant
+
+Application = get_application_model()
+
+
+class TestDataAccessGrant(BaseApiTest):
+    def test_creation_on_approval(self):
+        redirect_uri = 'http://localhost'
+        # create a user
+        user = self._create_user('anna', '123456')
+        capability_a = self._create_capability('Capability A', [])
+        capability_b = self._create_capability('Capability B', [])
+        # create an application and add capabilities
+        application = self._create_application(
+            'an app',
+            grant_type=Application.GRANT_AUTHORIZATION_CODE,
+            redirect_uris=redirect_uri)
+        application.scope.add(capability_a, capability_b)
+
+        # user logs in
+        self.client.login(username='anna', password='123456')
+
+        payload = {
+            'client_id': application.client_id,
+            'response_type': 'code',
+            'redirect_uri': redirect_uri,
+        }
+        response = self.client.get('/v1/o/authorize', data=payload)
+        # post the authorization form with only one scope selected
+        payload = {
+            'client_id': application.client_id,
+            'response_type': 'code',
+            'redirect_uri': redirect_uri,
+            'scope': ['capability-a'],
+            'expires_in': 86400,
+            'allow': True,
+        }
+        response = self.client.post(response['Location'], data=payload)
+
+        self.assertEqual(response.status_code, 302)
+        # now extract the authorization code and use it to request an access_token
+        query_dict = parse_qs(urlparse(response['Location']).query)
+        authorization_code = query_dict.pop('code')
+        token_request_data = {
+            'grant_type': 'authorization_code',
+            'code': authorization_code,
+            'redirect_uri': redirect_uri,
+            'client_id': application.client_id,
+        }
+        response = self.client.post(reverse('oauth2_provider:token'), data=token_request_data)
+        self.assertEqual(response.status_code, 200)
+
+        # errors if DNE or more than one is found
+        DataAccessGrant.objects.get(beneficiary=user.id, application=application.id)
