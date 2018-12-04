5710f8f7597d654b0a959b17f656105a4c618bfe
David Gage <whytheplatypus@gmail.com>
2018-12-04 17:01:46 -0500

    BLUEBUTTON-571 Data Access Grant (#670)
    * Record a Grant of access for all approvals
    
    * When a beneficiary gives their approval for an application to access
    their data for the first time, create a record of this grant of access.
    
    * Only one Grant of Access is recorded for a beneficiary / application
    pair
    
    * Initial relationship between tokens and grant
    
    * Provide grant metrics through an API
    
    * Provide admin view for grant model
    
    * DataAccessGrant metrics uses benes not users
    
    * Create DataAccessGrants from tokens during migrate
    
    * Allow update_grant migration to fail
    
    * If no migrations have been run before the update_grant migration will
    and should fail.

---
+++ apps/authorization/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import DataAccessGrant
+
+
+class DataAccessGrantAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(DataAccessGrant, DataAccessGrantAdmin)
---
+++ apps/authorization/signals.py
@@ -0,0 +1,12 @@
+from oauth2_provider.signals import app_authorized
+from .models import DataAccessGrant
+
+
+def app_authorized_record_grant(sender, request, token, **kwargs):
+    DataAccessGrant.objects.get_or_create(
+        beneficiary=token.user,
+        application=token.application,
+    )
+
+
+app_authorized.connect(app_authorized_record_grant)
