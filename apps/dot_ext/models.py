6cf5b2ded446b84fcb07762e1ed57a9874850d99
David Gage <whytheplatypus@gmail.com>
2019-01-17 09:14:08 -0600

    Document depreciation of client_uri (#694)

--- apps/dot_ext/models.py
+++ apps/dot_ext/models.py
@@ -30,9 +30,12 @@ class Application(AbstractApplication):
     updated = models.DateTimeField(auto_now=True)
     op_tos_uri = models.CharField(default=settings.TOS_URI, blank=True, max_length=512)
     op_policy_uri = models.CharField(default="", blank=True, max_length=512)
-    client_uri = models.URLField(default="", blank=True, null=True, max_length=512, verbose_name="Website URI",
+
+    # client_uri is depreciated but will continued to be referenced until it can be removed safely
+    client_uri = models.URLField(default="", blank=True, null=True, max_length=512, verbose_name="Client URI",
                                  help_text="This is typically a home/download website for the application. "
                                            "For example, https://www.example.org or http://www.example.org .")
+
     website_uri = models.URLField(default="", blank=True, null=True, max_length=512, verbose_name="Website URI",
                                   help_text="This is typically a home/download website for the application. "
                                             "For example, https://www.example.org or http://www.example.org .")
