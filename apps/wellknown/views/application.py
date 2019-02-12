b9062f4269bd53138d87434d12c3c87fe254d03b
Dave Tisza <dtisza@fearless.tech>
2019-02-12 16:26:56 -0500

    Add django setting for APP_LIST_EXCLUDE for list

--- apps/wellknown/views/application.py
+++ apps/wellknown/views/application.py
@@ -1,6 +1,7 @@
 from ...dot_ext.models import Application
-from rest_framework.generics import ListAPIView
 from ..serializers import ApplicationListSerializer
+from django.conf import settings
+from rest_framework.generics import ListAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny
 from rest_framework.renderers import JSONRenderer
@@ -16,8 +17,8 @@ class ApplicationListView(ListAPIView):
     """
     View to provide an applications list.
 
-    An application that has active=False or with a label name = "internal-use"
-    will be excluded from this view.
+    An application that has active=False or with a label slug in the APP_LIST_EXCLUDE
+    list will be excluded from this view.
     """
     permission_classes = (AllowAny,)
     renderer_classes = (JSONRenderer,)
@@ -25,5 +26,6 @@ class ApplicationListView(ListAPIView):
     pagination_class = ApplicationListPagination
 
     def get_queryset(self):
-        queryset = Application.objects.exclude(active=False).exclude(applicationlabel__name='internal-use').order_by('name')
+        queryset = Application.objects.exclude(active=False).exclude(
+            applicationlabel__slug__in=settings.APP_LIST_EXCLUDE).order_by('name')
         return queryset
