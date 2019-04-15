120392a22131a7d775a6092ffc0a607acc03ab50
David Gage <whytheplatypus@gmail.com>
2019-04-15 13:32:22 -0400

    Email dev after their first usage of the API

--- apps/dot_ext/signals.py
+++ apps/dot_ext/signals.py
@@ -1,10 +1,13 @@
 import logging
-from django.db.models.signals import post_save
-from oauth2_provider.models import get_application_model
+from django.db.models.signals import post_save, pre_save
+from oauth2_provider.models import get_application_model, get_access_token_model
 from libs.mail import Mailer
+from .models import ArchivedToken
 
 
 Application = get_application_model()
+Token = get_access_token_model()
+
 logger = logging.getLogger('hhs_server.%s' % __name__)
 
 
@@ -29,4 +32,35 @@ def check_first_application(sender, instance=None, created=False, **kwargs):
                          (instance.user.username, instance.user.email))
 
 
+def outreach_first_api_call(sender, instance=None, **kwargs):
+    if not isinstance(instance, Token):
+        return
+
+    if instance.application.first_active is not None:
+        return
+
+    if Token.objects.filter(application=instance.application).exists():
+        return
+
+    if ArchivedToken.objects.filter(application=instance.application).exists():
+        return
+
+    if Application.objects.filter(user=instance.application.user).count() != 1:
+        return
+
+    try:
+        mailer = Mailer(subject='Congrats on Making Your First API Call',
+                        template_text='email/email-success-first-api-call-template.txt',
+                        template_html='email/email-success-first-api-call-template.html',
+                        to=[instance.application.user.email, ],
+                        context={"FIRST_NAME": instance.application.user.first_name})
+        mailer.send()
+        logger.info("Congrats on Making Your First API Call sent to %s (%s)" %
+                    (instance.application.user.username, instance.application.user.email))
+    except:  # noqa
+        logger.error("Making Your First API Call Application failed send to %s (%s)" %
+                     (instance.application.user.username, instance.application.user.email))
+
+
 post_save.connect(check_first_application, sender='dot_ext.Application')
+pre_save.connect(outreach_first_api_call, sender=Token)
