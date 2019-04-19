b117edeeb37f1b001453854f69b970305485f7ec
Dave Tisza <dtisza@fearless.tech>
2019-04-19 10:15:53 -0400

    Move switch param array to constant

--- apps/core/management/commands/create_test_feature_switches.py
+++ apps/core/management/commands/create_test_feature_switches.py
@@ -2,21 +2,24 @@ from django.core.management.base import BaseCommand
 from waffle.models import Switch
 
 
+WAFFLE_FEATURE_SWITCHES = (('outreach_email', True),
+                           ('wellknown_applications', True),
+                           ('login', True),
+                           ('signup', True))
+
+
 class Command(BaseCommand):
     help = 'Create Feature Switches for Local Testing'
 
     def handle(self, *args, **options):
         # Create feature switches for testing in local development
-        for switch in [['outreach_email', True],
-                       ['wellknown_applications', True],
-                       ['login', True],
-                       ['signup', True]]:
+        for switch in WAFFLE_FEATURE_SWITCHES:
             try:
                 Switch.objects.get(name=switch[0])
-                self._log('Feature switch already exists: %s' % (switch))
+                self._log('Feature switch already exists: %s' % (str(switch)))
             except Switch.DoesNotExist:
                 Switch.objects.create(name=switch[0], active=switch[1])
-                self._log('Feature switch created: %s' % (switch))
+                self._log('Feature switch created: %s' % (str(switch)))
 
     def _log(self, message):
         self.stdout.write(message)
