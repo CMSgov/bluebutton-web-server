1b286c15cc6b2a8ef7becb876aa49fdf1448f66e
Dave Tisza <dtisza@fearless.tech>
2019-04-29 14:02:44 -0400

    Update unit tests

--- apps/accounts/tests/test_password_reset.py
+++ apps/accounts/tests/test_password_reset.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.test.client import Client
 from django.contrib.auth.models import User
 from django.urls import reverse
-from ..models import UserProfile, ValidPasswordResetKey
+from ..models import UserProfile
 from waffle.testutils import override_switch
 
 
@@ -21,13 +21,7 @@ class PasswordResetTestCase(TestCase):
                                      password="foobar",)
         UserProfile.objects.create(user=u,
                                    user_type="DEV",
-                                   create_applications=True,
-                                   password_reset_question_1='1',
-                                   password_reset_answer_1='blue',
-                                   password_reset_question_2='2',
-                                   password_reset_answer_2='Frank',
-                                   password_reset_question_3='3',
-                                   password_reset_answer_3='Bentley')
+                                   create_applications=True)
         self.client = Client()
 
     @override_switch('login', active=True)
@@ -36,36 +30,11 @@ class PasswordResetTestCase(TestCase):
         form_data = {'email': 'fred@example.com'}
         response = self.client.post(url, form_data, follow=True)
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Answer')
 
     @override_switch('login', active=True)
     def test_password_reset_invalid_user(self):
         url = reverse('forgot_password')
         form_data = {'email': 'derf@example.com'}
         response = self.client.post(url, form_data, follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(
-            response, 'A user with the email supplied does not exist.')
-
-    @override_switch('login', active=True)
-    def test_password_reset_invalid_answer_3(self):
-        url = reverse('secret_question_challenge_3', args=('fred',))
-        form_data = {'answer': 'Yolo'}
-        response = self.client.post(url, form_data, follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Wrong answer.')
-
-    @override_switch('login', active=True)
-    def test_password_reset_valid_answer_3(self):
-        url = reverse('secret_question_challenge_3', args=('fred',))
-        form_data = {'answer': 'bentley'}
-        response = self.client.post(url, form_data, follow=True)
-
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(
-            response,
-            'Please check your email for a special link to reset your password.')
-        # Check that the password reset key was created.
-        self.assertGreater(
-            ValidPasswordResetKey.objects.filter(
-                user__username='fred').count(), 0)
+        self.assertRedirects(response, "/v1/accounts/password-reset-done", status_code=302, target_status_code=200,
+                             msg_prefix='', fetch_redirect_response=True)
