75705e3c802edc6098c52590c5eb0c7f00aeef19
Dave Tisza <dtisza@fearless.tech>
2019-05-03 15:41:33 -0400

    Create labels filter on application list endpoint

--- apps/wellknown/views/application.py
+++ apps/wellknown/views/application.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet, CharFilter
+from rest_framework import serializers
 from rest_framework.generics import ListAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny
@@ -13,6 +15,30 @@ class ApplicationListPagination(PageNumberPagination):
     max_page_size = 10000
 
 
+class LabelSlugFilter(CharFilter):
+    def filter(self, qs, value):
+        result_qs = Application.objects.none()
+        if value:
+            slugs = [slug.strip() for slug in value.split(',')]
+            for slug in slugs:
+                # Check if label slug is valid
+                if not ApplicationLabel.objects.exclude(slug__in=settings.APP_LIST_EXCLUDE).filter(slug=slug).exists():
+                    raise serializers.ValidationError(detail='Invalid slug name for label parameter:  %s' % (slug), code=400)
+                # Concatinate filter query results
+                result_qs = result_qs | super().filter(qs, slug)
+        else:
+            result_qs = super().filter(qs, value)
+        return result_qs.distinct()
+
+
+class ApplicationFilter(FilterSet):
+    label = LabelSlugFilter(field_name="applicationlabel__slug")
+
+    class Meta:
+        model = Application
+        fields = ['label']
+
+
 class ApplicationListView(ListAPIView):
     """
     View to provide an applications list.
@@ -24,6 +50,8 @@ class ApplicationListView(ListAPIView):
     renderer_classes = (JSONRenderer,)
     serializer_class = ApplicationListSerializer
     pagination_class = ApplicationListPagination
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = ApplicationFilter
 
     def get_queryset(self):
         queryset = Application.objects.exclude(active=False).exclude(
