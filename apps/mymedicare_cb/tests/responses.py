3237c39abe4008fb4135e020befccd718fb093f3
whytheplatypus <whytheplatypus@gmail.com>
2019-06-27 08:23:16 -0400

    Update tests with new behavior from data server

--- apps/mymedicare_cb/tests/responses.py
+++ apps/mymedicare_cb/tests/responses.py
@@ -1,46 +1,70 @@
 patient_response = {
-    "resourceType": "Bundle",
-    "id": "d0c10556-a3df-4af6-bdb3-d60908b1f16b",
-    "meta": {
-        "lastUpdated": "2018-04-05T15:44:17.721-04:00"
-    },
-    "type": "searchset",
-    "total": 1,
-    "entry": [{
-        "fullUrl": "https://sandbox.bluebutton.cms.gov/v1/fhir/Patient/20140000008325",
-        "resource": {
-            "resourceType": "Patient",
-            "id": "20140000008325",
-            "extension": [{
-                "url": "https://bluebutton.cms.gov/resources/variables/race",
-                "valueCoding": {
-                    "system": "https://bluebutton.cms.gov/resources/variables/race",
-                    "code": "1",
-                    "display": "White"
-                }
-            }],
-            "identifier": [{
-                "system": "https://bluebutton.cms.gov/resources/variables/bene_id",
-                "value": "20140000008325"
-            }, {
-                "system": "https://bluebutton.cms.gov/resources/identifier/hicn-hash",
-                "value": "96228a57f37efea543f4f370f96f1dbf01c3e3129041dba3ea4367545507c6e7"
-            }],
-            "name": [{
-                "use": "usual",
-                "family": "Doe",
-                "given": [
-                    "Jane",
-                    "X"
-                ]
-            }],
-            "gender": "unknown",
-            "birthDate": "1999-06-01",
-            "address": [{
-                "district": "999",
-                "state": "30",
-                "postalCode": "99999"
-            }]
-        }
-    }]
+   "total" : 1,
+   "resourceType" : "Bundle",
+   "type" : "searchset",
+   "link" : [
+      {
+         "url" : "http://localhost:8000/v1/fhir/Patient?_count=5&startIndex=0&_id=20140000008325",
+         "relation" : "first"
+      },
+      {
+         "url" : "http://localhost:8000/v1/fhir/Patient?_count=5&startIndex=0&_id=20140000008325",
+         "relation" : "last"
+      },
+      {
+         "url" : "http://localhost:8000/v1/fhir/Patient/?_count=5&_format=application%2Fjson%2Bfhir&_id=20140000008325&startIndex=0",
+         "relation" : "self"
+      }
+   ],
+   "id" : "4250e2c5-8956-40a1-9809-1be08ba542f7",
+   "entry" : [
+      {
+         "resource" : {
+            "name" : [
+               {
+                  "given" : [
+                     "Jane",
+                     "X"
+                  ],
+                  "family" : "Doe",
+                  "use" : "usual"
+               }
+            ],
+            "extension" : [
+               {
+                  "valueCoding" : {
+                     "system" : "https://bluebutton.cms.gov/resources/variables/race",
+                     "display" : "White",
+                     "code" : "1"
+                  },
+                  "url" : "https://bluebutton.cms.gov/resources/variables/race"
+               }
+            ],
+            "identifier" : [
+               {
+                  "value" : "20140000008325",
+                  "system" : "https://bluebutton.cms.gov/resources/variables/bene_id"
+               },
+               {
+                  "value" : "2025fbc612a884853f0c245e686780bf748e5652360ecd7430575491f4e018c5",
+                  "system" : "https://bluebutton.cms.gov/resources/identifier/hicn-hash"
+               }
+            ],
+            "resourceType" : "Patient",
+            "id" : "20140000008325",
+            "address" : [
+               {
+                  "state" : "15",
+                  "district" : "999",
+                  "postalCode" : "99999"
+               }
+            ],
+            "gender" : "female",
+            "birthDate" : "2014-06-01"
+         }
+      }
+   ],
+   "meta" : {
+      "lastUpdated" : "2019-06-27T08:17:11.811-04:00"
+   }
 }
