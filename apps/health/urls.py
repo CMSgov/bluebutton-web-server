662dd182f4b4a6da3bef90e84d6a0495ee8838c3
David Gage <whytheplatypus@gmail.com>
2019-08-26 09:53:01 -0400

    BLUEBUTTON-1203 Independant health checks (#746)
    * Separate internal an external health checks
    
    * Use local vendored dependencies for development

--- apps/health/urls.py
+++ apps/health/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
-from .views import Check
+from .views import (
+    CheckInternal,
+    CheckExternal,
+)
 
 urlpatterns = [
-    url(r'', Check.as_view()),
+    url(r'/external', CheckExternal.as_view()),
+    url(r'', CheckInternal.as_view()),
 ]
