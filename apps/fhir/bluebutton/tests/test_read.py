993e6359cd9c9fc6b26d16fab0e5b7491e4e69c6
Dave Tisza <dtisza@fearless.tech>
2020-03-17 17:20:41 -0400

    Add tests for _lastUpdated and type params

--- apps/fhir/bluebutton/tests/test_read.py
+++ apps/fhir/bluebutton/tests/test_read.py
@@ -441,6 +441,98 @@ class BackendConnectionTest(BaseApiTest):
 
             self.assertEqual(response.status_code, 502)
 
+    def test_search_parameters_request(self):
+        # create the user
+        first_access_token = self.create_token('John', 'Smith')
+
+        @all_requests
+        def catchall(url, req):
+            self.assertIn("https://fhir.backend.bluebutton.hhsdevcloud.us/v1/fhir/ExplanationOfBenefit/", req.url)
+            self.assertIn("_format=application%2Fjson%2Bfhir", req.url)
+
+            return {
+                'status_code': 200,
+                'content': {
+                    'resourceType': 'ExplanationOfBenefit',
+                    'patient': {
+                        'reference': 'stuff/-20140000008325',
+                    },
+                },
+            }
+
+        # Test _lastUpdated with valid parameter starting with "lt"
+        with HTTMock(catchall):
+            response = self.client.get(
+                reverse(
+                    'bb_oauth_fhir_search',
+                    kwargs={'resource_type': 'ExplanationOfBenefit'}),
+                {'_lastUpdated': 'lt2019-11-22T14:00:00-05:00'},
+                Authorization="Bearer %s" % (first_access_token))
+            self.assertEqual(response.status_code, 200)
+
+        # Test _lastUpdated with invalid parameter starting with "zz"
+        with HTTMock(catchall):
+            response = self.client.get(
+                reverse(
+                    'bb_oauth_fhir_search',
+                    kwargs={'resource_type': 'ExplanationOfBenefit'}),
+                {'_lastUpdated': 'zz2020-11-22T14:00:00-05:00'},
+                Authorization="Bearer %s" % (first_access_token))
+
+            content = json.loads(response.content.decode("utf-8"))
+            self.assertEqual(content['detail'], 'the _lastUpdated operator is not valid')
+            self.assertEqual(response.status_code, 400)
+
+        # Test type= with single valid value: "pde"
+        with HTTMock(catchall):
+            response = self.client.get(
+                reverse(
+                    'bb_oauth_fhir_search',
+                    kwargs={'resource_type': 'ExplanationOfBenefit'}),
+                {'type': 'pde'},
+                Authorization="Bearer %s" % (first_access_token))
+            self.assertEqual(response.status_code, 200)
+
+        # Test type= with multiple (all valid values)
+        with HTTMock(catchall):
+            response = self.client.get(
+                reverse(
+                    'bb_oauth_fhir_search',
+                    kwargs={'resource_type': 'ExplanationOfBenefit'}),
+                {'type': 'carrier,'
+                         'pde,'
+                         'dme,'
+                         'hha,'
+                         'hospice,'
+                         'inpatient,'
+                         'outpatient,'
+                         'snf,'
+                         'https://bluebutton.cms.gov/resources/codesystem/eob-type|carrier,'
+                         'https://bluebutton.cms.gov/resources/codesystem/eob-type|pde,'
+                         'https://bluebutton.cms.gov/resources/codesystem/eob-type|dme,'
+                         'https://bluebutton.cms.gov/resources/codesystem/eob-type|hha,'
+                         'https://bluebutton.cms.gov/resources/codesystem/eob-type|hospice,'
+                         'https://bluebutton.cms.gov/resources/codesystem/eob-type|inpatient,'
+                         'https://bluebutton.cms.gov/resources/codesystem/eob-type|outpatient,'
+                         'https://bluebutton.cms.gov/resources/codesystem/eob-type|snf'},
+                Authorization="Bearer %s" % (first_access_token))
+            self.assertEqual(response.status_code, 200)
+
+        # Test type= with an invalid type
+        with HTTMock(catchall):
+            response = self.client.get(
+                reverse(
+                    'bb_oauth_fhir_search',
+                    kwargs={'resource_type': 'ExplanationOfBenefit'}),
+                {'type': 'carrier,'
+                         'INVALID-TYPE,'
+                         'dme,'},
+                Authorization="Bearer %s" % (first_access_token))
+
+            content = json.loads(response.content.decode("utf-8"))
+            self.assertEqual(content['detail'], 'the type parameter value is not valid')
+            self.assertEqual(response.status_code, 400)
+
     def test_read_request_failed_no_fhir_id(self):
         # create the user
         first_access_token = self.create_token('John', 'Smith')
