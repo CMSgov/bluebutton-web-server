07d7649db45b8c8dc77de9ab810e554c65e16a83
James Fuqian <jfuqian@ellumen.com>
2020-05-26 21:24:19 +0100

    fix indentation.

--- apps/testclient/management/commands/create_user_identification_label_selection.py
+++ apps/testclient/management/commands/create_user_identification_label_selection.py
@@ -14,8 +14,8 @@ DEVELOPER_SIGNUP_ROLE_CHOICES = [
 def create_user_identification_label():
     UserIdentificationLabel.objects.all().delete()
     for c in DEVELOPER_SIGNUP_ROLE_CHOICES:
-       choice = UserIdentificationLabel.objects.create(name=c[1], slug=c[2], weight=c[0])
-       print("group created %s " % (choice))
+        choice = UserIdentificationLabel.objects.create(name=c[1], slug=c[2], weight=c[0])
+        print("group created %s " % (choice))
 
 
 class Command(BaseCommand):
