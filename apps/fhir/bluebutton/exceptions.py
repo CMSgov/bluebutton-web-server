a6a3baf10133a470b9e10aa3b20aab47c2b94f43
James Fuqian <jfuqian@ellumen.com>
2020-06-16 23:22:37 +0100

    narrow fix only to BFD 500 errors with Coverage, EOB request with 'Unsupported ID pattern' error

--- apps/fhir/bluebutton/exceptions.py
+++ apps/fhir/bluebutton/exceptions.py
@@ -7,21 +7,29 @@ from .models import Fhir_Response
 def process_error_response(response: Fhir_Response) -> APIException:
     """
     TODO: This should be more specific (original comment before BB2-128)
-    BB2-128: map BFD coarse grained 500 error to 502 (backward compatible),
-    attach FHIR response as details such that BB2 API consumer could act on
-    the response depend on nature of the response.
+    BB2-128: check FHIR response: if error is "IllegalArgumentException: Unsupported ID pattern..."
+    then append the error to the general error message: 'An error occurred contacting the upstream server'
     """
     err: APIException = None
     r: Response = response.backend_response
     if response.status_code >= 300:
-        bfd_err = {'FHIR response status_code': response.status_code}
-        bfd_err.update({'fhir response content': r.content})
         if response.status_code == 404:
-            bb2_err = {'Error': response.status_code, 'message': 'The requested resource does not exist'}
-            err = NotFound(detail={**bb2_err, **bfd_err})
+            err = NotFound('The requested resource does not exist')
         else:
-            bb2_err = {'Error': 502, 'message': 'An error occurred contacting the upstream server'}
-            err = UpstreamServerException(detail={**bb2_err, **bfd_err})
+            detail = 'An error occurred contacting the upstream server'
+            if response.status_code == 500:
+                json = None
+                try:
+                    json = r.json()
+                except Exception as e:
+                    pass
+                if json is not None:
+                    issues = json.get('issue')
+                    issue = issues[0] if issues else None
+                    diagnostics = issue.get('diagnostics') if issue else None
+                    if "Unsupported ID pattern" in diagnostics:
+                        detail = detail + ": " + diagnostics
+            err = UpstreamServerException(detail)
     return err
 
 
