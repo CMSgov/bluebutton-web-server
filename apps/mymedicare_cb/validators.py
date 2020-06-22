8fcb17e49afa67b97f27486e3967a44365cd105d
Dave Tisza <dtisza@fearless.tech>
2020-06-22 14:39:07 -0400

    Refactor validation function to be cleaner
    BB2-54

--- apps/mymedicare_cb/validators.py
+++ apps/mymedicare_cb/validators.py
@@ -18,41 +18,29 @@ def is_mbi_format_valid(mbi):
     CHAR_TYPE_N = "0123456789"
     # Type is alpha minus: S,L,O,I,B,Z
     CHAR_TYPE_A = "ACDEFGHJKMNPQRTUVWXY"
+    CHAR_TYPE_AN = CHAR_TYPE_A + CHAR_TYPE_N
 
-    # Position mappings. Note is minus one from PDF document position.
-    CHAR_POSITION_C = [0]
-    CHAR_POSITION_N = [3, 6, 9, 10]
-    CHAR_POSITION_A = [1, 4, 7, 8]
-    CHAR_POSITION_AN = [2, 5]
-
+    # Position mapping list[0 thru 10]:
+    VALID_VALUES_BY_POS = [ CHAR_TYPE_C,
+                            CHAR_TYPE_A + "S",
+                            CHAR_TYPE_AN,
+                            CHAR_TYPE_N,
+                            CHAR_TYPE_A,
+                            CHAR_TYPE_AN,
+                            CHAR_TYPE_N,
+                            CHAR_TYPE_A,
+                            CHAR_TYPE_A,
+                            CHAR_TYPE_N,
+                            CHAR_TYPE_N]
     msg = ""
     # Check length
     if len(mbi) != 11:
         msg = "Invalid length = {}".format(len(mbi))
         return False, msg
 
-    # Iterate over character positions:
-    for pos in range(0, len(mbi)):
-        # Validate type C
-        if pos in CHAR_POSITION_C and mbi[pos] not in CHAR_TYPE_C:
-            msg = "Invalid char in pos = {}".format(pos)
-            return False, msg
-
-        # Validate type N
-        if pos in CHAR_POSITION_N and mbi[pos] not in CHAR_TYPE_N:
-            msg = "Invalid char in pos = {}".format(pos)
-            return False, msg
-
-        # Validate type A (exception for 2nd position = "S")
-        if (pos in CHAR_POSITION_A and mbi[pos] not in CHAR_TYPE_A):
-            if pos == 1 and mbi[pos] == "S":
-                pass
-            else:
-                msg = "Invalid char in pos = {}".format(pos)
-                return False, msg
-
-        # Validate type AN
-        if pos in CHAR_POSITION_AN and (mbi[pos] not in CHAR_TYPE_A + CHAR_TYPE_N):
+    # Check 11 character positions are valid.
+    for pos in range(0, 11):
+        if mbi[pos] not in VALID_VALUES_BY_POS[pos]:
             msg = "Invalid char in pos = {}".format(pos)
             return False, msg
 
