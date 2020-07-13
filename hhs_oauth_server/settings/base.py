001c233e871d540cbdc5344e7fc108dd37524ebc
James Fuqian <jfuqian@ellumen.com>
2020-07-13 23:51:29 +0100

    fix apps.logging.apps.LoggingConfig in base settings INSTALLED_APPS to get sls token handler registered with django signal

--- hhs_oauth_server/settings/base.py
+++ hhs_oauth_server/settings/base.py
@@ -85,8 +85,8 @@ INSTALLED_APPS = [
     # DOT must be installed after apps.dot_ext in order to override templates
     'oauth2_provider',
     'axes',
-
-    'apps.logging',
+    # BB2-218 need to register LoggingConfig to get sls token signal handler registered
+    'apps.logging.LoggingConfig',
     'apps.openapi',
 ]
 if env('ENV_SPECIFIC_APPS', False):
