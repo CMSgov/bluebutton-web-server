45fdf14bf48d6c0cc6f8ad0120fabac325cc1c71
David Tisza <36048547+dtisza1@users.noreply.github.com>
2020-08-06 13:29:27 -0400

    BB2-249 Update runtests.py to take command line args (#821)
    Update runtests.py to enable running of specific
    tests or multiple tests, instead of ALL tests.
    
    BB2-249

--- runtests.py
+++ runtests.py
@@ -7,6 +7,27 @@ from django.conf import settings
 from django.test.utils import get_runner
 
 
+'''
+    Reference: https://docs.djangoproject.com/en/3.0/topics/testing/advanced/#defining-a-test-runner
+
+    Command line arguments sys.argv[1:] is a list of specific tests to run.
+
+    For example:
+        $ docker-compose exec web python runtests.py apps.dot_ext.tests
+
+        For more specific use:
+
+        $ docker-compose exec web python runtests.py apps.dot_ext.tests.test_templates
+
+        For a single test:
+        $ docker-compose exec web python runtests.py apps.dot_ext.tests.\
+          test_templates.TestDOTTemplates.test_application_list_template_override
+
+        For multiple arguments:
+
+        $ docker-compose exec web python runtests.py apps.dot_ext.tests apps.accounts.tests.test_login
+'''
+
 # Unset ENV variables so that tests use default values.
 for env_var in ['FHIR_URL', 'DJANGO_MEDICARE_LOGIN_URI',
                 'DJANGO_SLS_USERINFO_ENDPOINT', 'DJANGO_SLS_TOKEN_ENDPOINT',
@@ -19,5 +40,10 @@ if __name__ == '__main__':
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
-    failures = test_runner.run_tests(None)
+
+    # Is there a list of specific tests to run?
+    if len(sys.argv) > 1:
+        failures = test_runner.run_tests(sys.argv[1:])
+    else:
+        failures = test_runner.run_tests(None)
     sys.exit(bool(failures))
