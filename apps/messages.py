95cf15c2c45d35693458b1fda263d7032af2d933
James Fuqian <jfuqian@ellumen.com>
2020-09-19 03:04:56 +0100

    customized permission denied message format with app name.

---
+++ apps/messages.py
@@ -0,0 +1,4 @@
+APPLICATION_TEMPORARILY_INACTIVE = """This application ({}) is temporarily inactive.
+        If you are the app maintainer, please contact the Blue Button 2.0 API team.
+        If you are a Medicare Beneficiary and need assistance,
+        please contact the application support or call 1-800-MEDICARE (1-800-633-4227)"""
