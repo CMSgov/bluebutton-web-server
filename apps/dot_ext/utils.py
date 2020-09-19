296dfab7f5de3a51d82e87a1edb5549ed892e595
James Fuqian <jfuqian@ellumen.com>
2020-09-19 02:58:17 +0100

    changes made per PR feedback.

--- apps/dot_ext/utils.py
+++ apps/dot_ext/utils.py
@@ -46,12 +46,14 @@ def get_app_and_org(request):
     client_id = request.GET.get('client_id', None)
     app = None
     user = None
-
     if client_id is not None:
-        apps = Application.objects.filter(client_id=client_id)
-        if apps:
-            app = apps.first()
-            user = User.objects.get(pk=app.user_id)
-
+        try:
+            app = Application.objects.get(client_id=client_id)
+        except Application.DoesNotExist:
+            pass
+    try:
+        user = User.objects.get(pk=app.user_id) if app else None
+    except User.DoesNotExist:
+        pass
     return app, user
 
