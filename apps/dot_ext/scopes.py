a807bcb2ab4e958f2fc69080051e1e0139a9b82c
Dave Tisza <dtisza@fearless.tech>
2020-09-23 12:45:03 -0400

    Update apps/dot_ext/scopes.py
    - Update get_available_scopes() to return scopes based
      on application require_demographic_scopes choice field.
    
    - Update get_default_scopes() to return scopes based
      on application require_demographic_scopes choice field.
    
    BB2-181

--- apps/dot_ext/scopes.py
+++ apps/dot_ext/scopes.py
@@ -1,7 +1,6 @@
+from django.conf import settings
 from django.db.models import Q
-
 from oauth2_provider.scopes import BaseScopes
-
 from apps.capabilities.models import ProtectedCapability
 
 
@@ -21,14 +20,48 @@ class CapabilitiesScopes(BaseScopes):
         Returns a list that contains all the capabilities related
         to the current application.
         """
-        app_scopes = list(
-            ProtectedCapability.objects.filter(Q(default=True) | Q(application=application)).values_list('slug', flat=True))
-        return app_scopes
+        # Return empty scopes list, if application is none
+        if application is None:
+            return []
+
+        # Get list of all available scopes
+        app_scopes_avail = list(
+            ProtectedCapability.objects.filter(Q(default=True) | Q(application=application))
+                                       .values_list('slug', flat=True).distinct())
+
+        # Set scopes based on application choice. Default is True, if field does not exist yet.
+        if getattr(application, 'require_demographic_scopes', True):
+            # Return all available scopes
+            return app_scopes_avail
+        else:
+            # Remove personal information scopes
+            app_scopes = []
+            for s in app_scopes_avail:
+                if s not in settings.BENE_PERSONAL_INFO_SCOPES:
+                    app_scopes.append(s)
+            return app_scopes
 
     def get_default_scopes(self, application=None, request=None, *args, **kwargs):
         """
         Returns a list that contains all the capabilities related
         to the current application.
         """
+        # Return empty scopes list, if application is none
+        if application is None:
+            return []
+
         # at the moment we assume that the default scopes are all those availables
-        return list(ProtectedCapability.objects.filter(default=True).values_list('slug', flat=True))
+        app_scopes_default = list(ProtectedCapability.objects.filter(default=True)
+                                                             .values_list('slug', flat=True))
+
+        # Set scopes based on application choice. Default is True, if field does not exist yet.
+        if getattr(application, 'require_demographic_scopes', True):
+            # Return all available scopes
+            return app_scopes_default
+        else:
+            # Remove personal information scopes
+            app_scopes = []
+            for s in app_scopes_default:
+                if s not in settings.BENE_PERSONAL_INFO_SCOPES:
+                    app_scopes.append(s)
+            return app_scopes
