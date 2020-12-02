92a70d2482c3ad50f6acaa8938aa23a21b6308e7
James Fuqian <jfuqian@ellumen.com>
2020-12-01 20:24:51 -0800

    code cleanup.

--- apps/fhir/bluebutton/urls.py
+++ apps/fhir/bluebutton/urls.py
@@ -36,33 +36,4 @@ urlpatterns = [
     url(r'ExplanationOfBenefit[/]?',
         SearchViewExplanationOfBenefit.as_view(),
         name='bb_oauth_fhir_eob_search'),
-    # Patient ReadView
-    url(r'PatientV2/(?P<resource_id>[^/]+)',
-        ReadViewPatient.as_view(),
-        name='bb_oauth_fhir_patient_read_or_update_or_delete'),
-
-    # Patient SearchView
-    url(r'PatientV2[/]?',
-        SearchViewPatient.as_view(),
-        name='bb_oauth_fhir_patient_search'),
-
-    # Coverage ReadView
-    url(r'CoverageV2/(?P<resource_id>[^/]+)',
-        ReadViewCoverage.as_view(),
-        name='bb_oauth_fhir_coverage_read_or_update_or_delete'),
-
-    # Coverage SearchView
-    url(r'CoverageV2[/]?',
-        SearchViewCoverage.as_view(),
-        name='bb_oauth_fhir_coverage_search'),
-
-    # EOB ReadView
-    url(r'ExplanationOfBenefitV2/(?P<resource_id>[^/]+)',
-        ReadViewExplanationOfBenefit.as_view(),
-        name='bb_oauth_fhir_eob_read_or_update_or_delete'),
-
-    # EOB SearchView
-    url(r'ExplanationOfBenefitV2[/]?',
-        SearchViewExplanationOfBenefit.as_view(),
-        name='bb_oauth_fhir_eob_search'),
 ]
