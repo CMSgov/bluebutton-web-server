4982c5ea2d768be991e5e07f4d02e4bc81c1aaf1
James Fuqian <jfuqian@ellumen.com>
2020-12-17 09:39:32 -0800

    add admin prepend to dashboard page href urls.

--- apps/bb2_tools/admin.py
+++ apps/bb2_tools/admin.py
@@ -23,7 +23,9 @@ from apps.bb2_tools.models import (
 )
 from apps.fhir.bluebutton.utils import get_patient_by_id
 
-BB2_TOOLS_PATH = "/admin/bb2_tools/"
+ADMIN_PREPEND = getattr(settings, 'ADMIN_PREPEND_URL', '')
+BB2_TOOLS_PATH = "/{}/admin/bb2_tools/".format(ADMIN_PREPEND) if ADMIN_PREPEND else "/admin/bb2_tools/"
+
 LINK_REF_FMT = "<a  href='{0}{1}?q={2}&user__id__exact={3}'>{4}</a>"
 TOKEN_VIEWERS = {MyAccessTokenViewer, MyRefreshTokenViewer, MyArchivedTokenViewer}
 
