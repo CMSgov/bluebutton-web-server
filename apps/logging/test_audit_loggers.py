08fb5e078d081800cbf69fbbba24843adca2354c
James Fuqian <jfuqian@ellumen.com>
2021-01-05 14:37:34 -0800

    code optimize and lift reusable code into base test per PR review and code contrib.

--- apps/logging/test_audit_loggers.py
+++ apps/logging/test_audit_loggers.py
@@ -1,5 +1,3 @@
-import logging
-import io
 import re
 import json
 from django.urls import reverse
@@ -13,13 +11,15 @@ from apps.mymedicare_cb.views import generate_nonce
 from apps.mymedicare_cb.models import AnonUserState
 from apps.mymedicare_cb.tests.responses import patient_response
 
-token_logger = logging.getLogger('audit.authorization.token')
-sls_logger = logging.getLogger('audit.authorization.sls')
-fhir_logger = logging.getLogger('audit.data.fhir')
-authenticate_logger = logging.getLogger('audit.authenticate.sls')
-mymedicare_cb_logger = logging.getLogger('audit.authenticate.mymedicare_cb')
-match_fhir_id_logger = logging.getLogger('audit.authenticate.match_fhir_id')
-hhs_oauth_server_request_logger = logging.getLogger('audit.hhs_oauth_server.request_logging')
+loggers = [
+    'audit.authorization.token',
+    'audit.authorization.sls',
+    'audit.data.fhir',
+    'audit.authenticate.sls',
+    'audit.authenticate.mymedicare_cb',
+    'audit.authenticate.match_fhir_id',
+    'audit.hhs_oauth_server.request_logging'
+]
 
 
 class HTTMockWithResponseHook(HTTMock):
@@ -57,89 +57,31 @@ class TestAuditEventLoggers(BaseApiTest):
         ])
         # Setup the RequestFactory
         self.client = Client()
-        # capture sls logging records
-        self.log_buffer_sls = io.StringIO()
-        self.channel_sls = logging.StreamHandler(self.log_buffer_sls)
-        self.channel_sls.setLevel(logging.INFO)
-        sls_logger.setLevel(logging.INFO)
-        sls_logger.addHandler(self.channel_sls)
-        # capture token logging records
-        self.log_buffer_token = io.StringIO()
-        self.channel_token = logging.StreamHandler(self.log_buffer_token)
-        self.channel_token.setLevel(logging.INFO)
-        token_logger.setLevel(logging.INFO)
-        token_logger.addHandler(self.channel_token)
-        # capture fhir data logging records
-        self.log_buffer_fhir = io.StringIO()
-        self.channel_fhir = logging.StreamHandler(self.log_buffer_fhir)
-        self.channel_fhir.setLevel(logging.INFO)
-        fhir_logger.setLevel(logging.INFO)
-        fhir_logger.addHandler(self.channel_fhir)
-        # capture authenticate_logger logging records
-        self.log_buffer_authn_sls = io.StringIO()
-        self.channel_authn_sls = logging.StreamHandler(self.log_buffer_authn_sls)
-        self.channel_authn_sls.setLevel(logging.INFO)
-        authenticate_logger.setLevel(logging.INFO)
-        authenticate_logger.addHandler(self.channel_authn_sls)
-        # capture mymedicare_cb_logger logging records
-        self.log_buffer_mymedicare_cb = io.StringIO()
-        self.channel_mymedicare_cb = logging.StreamHandler(self.log_buffer_mymedicare_cb)
-        self.channel_mymedicare_cb.setLevel(logging.INFO)
-        mymedicare_cb_logger.setLevel(logging.INFO)
-        mymedicare_cb_logger.addHandler(self.channel_mymedicare_cb)
-        # capture match_fhir_id_logger logging records
-        self.log_buffer_match_fhir_id = io.StringIO()
-        self.channel_match_fhir_id = logging.StreamHandler(self.log_buffer_match_fhir_id)
-        self.channel_match_fhir_id.setLevel(logging.INFO)
-        match_fhir_id_logger.setLevel(logging.INFO)
-        match_fhir_id_logger.addHandler(self.channel_match_fhir_id)
-        # capture match_fhir_id_logger logging records
-        self.log_buffer_hhs_oauth_server_request = io.StringIO()
-        self.channel_hhs_oauth_server_request = logging.StreamHandler(self.log_buffer_hhs_oauth_server_request)
-        self.channel_hhs_oauth_server_request.setLevel(logging.INFO)
-        hhs_oauth_server_request_logger.setLevel(logging.INFO)
-        hhs_oauth_server_request_logger.addHandler(self.channel_hhs_oauth_server_request)
+        self._redirect_loggers(loggers)
 
     def tearDown(self):
-        # do not close stream, only close channel
-        # self.log_buffer_sls.close()
-        self.channel_sls.close()
-        # self.log_buffer_token.close()
-        self.channel_token.close()
-        # self.log_buffer_fhir.close()
-        self.channel_fhir.close()
-        # close authenticate sls logger (channel)
-        self.channel_authn_sls.close()
-        # close mymedicare_cb logger (channel)
-        self.channel_mymedicare_cb.close()
-        # close match_fhir_id logger (channel)
-        self.channel_match_fhir_id.close()
-        # close channel_hhs_oauth_server_request logger (channel)
-        self.channel_hhs_oauth_server_request.close()
+        self._cleanup_logger()
 
-    def collect_logs(self):
-        return {
-            "token_log_content": self.log_buffer_token.getvalue(),
-            "fhir_log_content": self.log_buffer_fhir.getvalue(),
-            "sls_log_content": self.log_buffer_sls.getvalue(),
-            "mymedicare_cb_log_content": self.log_buffer_mymedicare_cb.getvalue(),
-            "authn_sls_log_content": self.log_buffer_authn_sls.getvalue(),
-            "match_fhir_id_log_content": self.log_buffer_match_fhir_id.getvalue(),
-            "hhs_oauth_server_log_content": self.log_buffer_hhs_oauth_server_request.getvalue(),
-        }
+    def get_log_content(self, logger_name):
+        return self._collect_logs(loggers).get(logger_name)
 
     def test_fhir_events_logging(self):
         first_access_token = self.create_token('John', 'Smith')
 
         @all_requests
         def catchall(url, req):
-            fhir_log_content = self.log_buffer_fhir.getvalue()
+            fhir_log_content = self.get_log_content('audit.data.fhir')
             self.assertIsNotNone(fhir_log_content)
+
             log_entry_dict = json.loads(fhir_log_content)
-            self.assertEqual(log_entry_dict["fhir_id"], "-20140000008325")
-            self.assertEqual(log_entry_dict["user"], "patientId:-20140000008325")
-            self.assertEqual(log_entry_dict["path"], "/v1/fhir/Patient")
-            self.assertIsNotNone(log_entry_dict["application"])
+            compare_dict = {'application': {'id': '1', 'name': 'John_Smith_test', 'user': {'id': '1'}},
+                            'fhir_id': '-20140000008325',
+                            'includeAddressFields': 'False',
+                            'path': '/v1/fhir/Patient',
+                            'type': 'fhir_pre_fetch',
+                            'user': 'patientId:-20140000008325'}
+            self.assert_log_entry_valid(log_entry_dict, compare_dict, ['start_time', 'uuid'])
+
             return {
                 'status_code': 200,
                 # TODO replace this with true backend response, this has been post proccessed
@@ -152,36 +94,50 @@ class TestAuditEventLoggers(BaseApiTest):
                 {'count': 5, 'hello': 'world'},
                 Authorization="Bearer %s" % (first_access_token))
 
-            log_contents = self.collect_logs()
             # fhir_log_content, token_log_content
             # check fhir log content
-            fhir_log_content = log_contents['fhir_log_content']
+            fhir_log_content = self.get_log_content('audit.data.fhir')
             self.assertIsNotNone(fhir_log_content)
             log_entries = fhir_log_content.splitlines()
 
+            # Validate fhir_pre_fetch entry
             log_entry_dict = json.loads(log_entries[0])
-            self.assertEqual(log_entry_dict["type"], "fhir_pre_fetch")
-            self.assertEqual(log_entry_dict["fhir_id"], "-20140000008325")
-            self.assertEqual(log_entry_dict["user"], "patientId:-20140000008325")
-            self.assertEqual(log_entry_dict["path"], "/v1/fhir/Patient")
-            self.assertIsNotNone(log_entry_dict["application"])
+            compare_dict = {'application': {'id': '1', 'name': 'John_Smith_test', 'user': {'id': '1'}},
+                            'fhir_id': '-20140000008325',
+                            'includeAddressFields': 'False',
+                            'path': '/v1/fhir/Patient',
+                            'type': 'fhir_pre_fetch',
+                            'user': 'patientId:-20140000008325'}
+            self.assert_log_entry_valid(log_entry_dict, compare_dict, ['start_time', 'uuid'])
 
+            # Validate fhir_post_fetch entry
             log_entry_dict = json.loads(log_entries[1])
-            self.assertEqual(log_entry_dict["type"], "fhir_post_fetch")
-            self.assertEqual(log_entry_dict["fhir_id"], "-20140000008325")
-            self.assertEqual(log_entry_dict["user"], "patientId:-20140000008325")
-            self.assertEqual(log_entry_dict["path"], "/v1/fhir/Patient")
-            self.assertIsNotNone(log_entry_dict["application"])
-            # check token log content
-            token_log_content = log_contents['token_log_content']
+            compare_dict = {'application': {'id': '1', 'name': 'John_Smith_test', 'user': {'id': '1'}},
+                            'code': 200,
+                            'fhir_id': '-20140000008325',
+                            'includeAddressFields': 'False',
+                            'path': '/v1/fhir/Patient',
+                            'size': 1270,
+                            'type': 'fhir_post_fetch',
+                            'user': 'patientId:-20140000008325'}
+            self.assert_log_entry_valid(log_entry_dict, compare_dict, ['start_time', 'uuid', 'elapsed'])
+
+            # Validate AccessToken entry
+            token_log_content = self.get_log_content('audit.authorization.token')
             self.assertIsNotNone(token_log_content)
             log_entries = token_log_content.splitlines()
+
             log_entry_dict = json.loads(log_entries[0])
-            self.assertEqual(log_entry_dict["type"], "AccessToken")
-            self.assertEqual(log_entry_dict["action"], "authorized")
-            self.assertEqual(log_entry_dict["scopes"], "read write patient")
-            self.assertIsNotNone(log_entry_dict["access_token"])
-            self.assertIsNotNone(log_entry_dict["application"])
+            compare_dict = {'action': 'authorized',
+                            'application': {'id': 1,
+                                            'name': 'John_Smith_test',
+                                            'user': {'id': 1, 'username': 'John'}},
+                            'auth_grant_type': 'password',
+                            'id': 1,
+                            'scopes': 'read write patient',
+                            'type': 'AccessToken',
+                            'user': {'id': 1, 'username': 'John'}}
+            self.assert_log_entry_valid(log_entry_dict, compare_dict, ['access_token'])
 
     def test_callback_url_success_sls_logger(self):
         # copy and adapted for SLS logger test
@@ -237,9 +193,7 @@ class TestAuditEventLoggers(BaseApiTest):
             s.save()
             self.client.get(self.callback_url, data={'code': 'test', 'state': state})
 
-            log_contents = self.collect_logs()
-
-            sls_log_content = log_contents['sls_log_content']
+            sls_log_content = self.get_log_content('audit.authorization.sls')
 
             quoted_strings = re.findall("{[^{}]+}", sls_log_content)
             self.assertEqual(len(quoted_strings), 2)
@@ -251,7 +205,7 @@ class TestAuditEventLoggers(BaseApiTest):
             self.assertEqual(sls_userinfo_dict["type"], "SLS_userinfo")
             self.assertEqual(sls_userinfo_dict["sub"], "00112233-4455-6677-8899-aabbccddeeff")
 
-            authn_sls_log_content = log_contents['authn_sls_log_content']
+            authn_sls_log_content = self.get_log_content('audit.authenticate.sls')
             log_entries = authn_sls_log_content.splitlines()
             self.assertEqual(len(log_entries), 2)
             # Authentication:start
@@ -271,7 +225,7 @@ class TestAuditEventLoggers(BaseApiTest):
             self.assertEqual(log_entry_dict["auth_crosswalk_action"], "C")
             self.assertIsNotNone(log_entry_dict["user"])
 
-            mymedicare_cb_log_content = log_contents['mymedicare_cb_log_content']
+            mymedicare_cb_log_content = self.get_log_content('audit.authenticate.mymedicare_cb')
             log_entries = mymedicare_cb_log_content.splitlines()
             self.assertEqual(len(log_entries), 2)
             # mymedicare_cb:create_beneficiary_record
@@ -295,7 +249,7 @@ class TestAuditEventLoggers(BaseApiTest):
             self.assertIsNotNone(log_entry_dict["mbi_hash"])
             self.assertIsNotNone(log_entry_dict["hicn_hash"])
 
-            fhir_log_content = log_contents['fhir_log_content']
+            fhir_log_content = self.get_log_content('audit.data.fhir')
             log_entries = fhir_log_content.splitlines()
             self.assertEqual(len(log_entries), 2)
             # fhir_auth_pre_fetch
@@ -317,7 +271,7 @@ class TestAuditEventLoggers(BaseApiTest):
             self.assertIsNotNone(log_entry_dict["size"])
             self.assertIsNotNone(log_entry_dict["elapsed"])
 
-            match_fhir_id_log_content = log_contents['match_fhir_id_log_content']
+            match_fhir_id_log_content = self.get_log_content('audit.authenticate.match_fhir_id')
             log_entries = match_fhir_id_log_content.splitlines()
             self.assertGreater(len(log_entries), 0)
             # fhir.server.authentication.match_fhir_id
@@ -330,7 +284,7 @@ class TestAuditEventLoggers(BaseApiTest):
             self.assertEqual(log_entry_dict["hash_lookup_type"], "M")
             self.assertEqual(log_entry_dict["hash_lookup_mesg"], "FOUND beneficiary via mbi_hash")
 
-            hhs_oauth_server_log_content = log_contents['hhs_oauth_server_log_content']
+            hhs_oauth_server_log_content = self.get_log_content('audit.hhs_oauth_server.request_logging')
             log_entries = hhs_oauth_server_log_content.splitlines()
             self.assertGreater(len(log_entries), 0)
             # hhs oauth server request logging
@@ -376,8 +330,7 @@ class TestAuditEventLoggers(BaseApiTest):
         response = self.client.post(response['Location'], data=payload)
         self.assertEqual(response.status_code, 302)
         # assert token logger record works by assert some top level fields
-        log_contents = self.collect_logs()
-        token_log_content = log_contents['token_log_content']
+        token_log_content = self.get_log_content('audit.authorization.token')
         self.assertIsNotNone(token_log_content)
         token_log_record = json.loads(token_log_content)
         self.assertEqual(token_log_record["type"], "Authorization")
