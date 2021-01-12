b5603db0ab49d878719764dcc9baa4d093ebfb01
Dave Tisza <dtisza@fearless.tech>
2021-01-12 10:07:12 -0500

    Update external health checks
    - Add "slsx" health check for SLSx service when the feature switch is active
      in apps/health/checks.py
    
    - Rename check service names to be more descriptive in apps/health/checks.py.
      Renname databases to django_rds_database. Rename dataserver to bfd_fhir_dataserver.
    
    - Add more detail about which service is failing in the logging and ServiceUnavailable exception
      in apps/health/views.py
    
    BB2-391

--- apps/health/checks.py
+++ apps/health/checks.py
@@ -1,18 +1,23 @@
-import requests
 import logging
+import requests
+import waffle
+
 from django.db import connection
+
 from apps.fhir.bluebutton.utils import get_resourcerouter
 from apps.fhir.server import connection as backend_connection
+from apps.mymedicare_cb.authorization import OAuth2ConfigSLSx
+
 
 logger = logging.getLogger('hhs_server.%s' % __name__)
 
 
-def databases():
+def django_rds_database():
     connection.ensure_connection()
     return connection.is_usable()
 
 
-def dataserver():
+def bfd_fhir_dataserver():
     resource_router = get_resourcerouter()
     target_url = resource_router.fhir_url + "metadata"
     r = requests.get(target_url,
@@ -27,10 +32,24 @@ def dataserver():
     return r.json()
 
 
+def slsx():
+    # SLS vs. SLSx flow based on feature switch slsx-enable (true = SLSx / false = SLS)
+    # TODO: Remove switch logic after migration to SLSx
+    if waffle.switch_is_active('slsx-enable'):
+        # Perform health check on SLSx service
+        slsx_client = OAuth2ConfigSLSx()
+        try:
+            slsx_client.service_health_check()
+        except requests.exceptions.HTTPError as e:
+            return "SLSx service health check error {reason}".format(reason=e).json()
+    return True
+
+
 internal_services = (
-    databases,
+    django_rds_database,
 )
 
 external_services = (
-    dataserver,
+    bfd_fhir_dataserver,
+    slsx,
 )
