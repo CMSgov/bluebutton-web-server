03df908452fafe331186478afdba6f2d3b26f9f5
Dave Tisza <dtisza@fearless.tech>
2021-01-28 14:35:21 -0500

    Create integration tests
    - Create IntegrationTestFhirApiResources class that is derived from
      django.contrib.staticfiles.testing.StaticLiveServerTestCase. This allows
      tests to utilize a LIVE BB2 server.
      For details see: https://docs.djangoproject.com/en/3.1/topics/testing/tools/#django.test.LiveServerTestCase
    
    - Create test_userinfo_endpoint() method to test the BB2 userinfo endpoint.
    
    - Create test_patient_endpoint() method to test the BB2 Patient resource endpoint.
    
    BB2-124

---
+++ apps/integration_tests/integration_test_fhir_resources.py
@@ -0,0 +1,90 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from oauth2_provider.models import AccessToken
+from rest_framework.test import APIClient
+from waffle.testutils import override_switch
+
+from apps.dot_ext.tests.demographic_scopes_test_cases import SCOPES_TO_URL_BASE_PATH
+from apps.test import BaseApiTest
+
+
+class IntegrationTestFhirApiResources(StaticLiveServerTestCase):
+    '''
+    This sets up a live server in the background to test with.
+    For more details, see https://docs.djangoproject.com/en/3.1/topics/testing/tools/#liveservertestcase
+    This uses APIClient to test the BB2 FHIR API endpoints with the default (Fred) access token.
+    '''
+    fixtures = ['scopes.json']
+
+    @override_switch('require-scopes', active=True)
+    def test_userinfo_endpoint(self):
+        base_path = SCOPES_TO_URL_BASE_PATH['profile']["base_path"]
+
+        # Setup token in APIClient
+        client = APIClient()
+
+        # Perform auth flow here --- when selenium is included later.
+        # For now, creating user thru access token using BaseApiTest for now.
+
+        # Setup instance of BaseApiTest
+        base_api_test = BaseApiTest()
+
+        # Setup client for BaseApiTest client
+        base_api_test.client = client
+
+        # Setup read/write capability for create_token()
+        base_api_test.read_capability = base_api_test._create_capability('Read', [])
+        base_api_test.write_capability = base_api_test._create_capability('Write', [])
+
+        # create user, app, and access token
+        first_name = "John"
+        last_name = "Doe"
+        access_token = base_api_test.create_token(first_name, last_name)
+
+        # Test scope in access_token
+        at = AccessToken.objects.get(token=access_token)
+
+        # Setup Bearer token:
+        client.credentials(HTTP_AUTHORIZATION="Bearer " + at.token)
+
+        # Test FHIR endpoint:
+        url = self.live_server_url + base_path
+        response = client.get(url)
+        # content = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+
+    @override_switch('require-scopes', active=True)
+    def test_patient_endpoint(self):
+        base_path = SCOPES_TO_URL_BASE_PATH['patient/Patient.read']["base_path"]
+
+        # Setup token in APIClient
+        client = APIClient()
+
+        # Perform auth flow here --- when selenium is included later.
+        # For now, creating user thru access token using BaseApiTest for now.
+
+        # Setup instance of BaseApiTest
+        base_api_test = BaseApiTest()
+
+        # Setup client for BaseApiTest client
+        base_api_test.client = client
+
+        # Setup read/write capability for create_token()
+        base_api_test.read_capability = base_api_test._create_capability('Read', [])
+        base_api_test.write_capability = base_api_test._create_capability('Write', [])
+
+        # create user, app, and access token
+        first_name = "John"
+        last_name = "Doe"
+        access_token = base_api_test.create_token(first_name, last_name)
+
+        # Test scope in access_token
+        at = AccessToken.objects.get(token=access_token)
+
+        # Setup Bearer token:
+        client.credentials(HTTP_AUTHORIZATION="Bearer " + at.token)
+
+        # Test FHIR endpoint:
+        url = self.live_server_url + base_path
+        response = client.get(url)
+        # content = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
