36c4481cc743d2192aac074c81ad1eab7942603d
Dave Tisza <dtisza@fearless.tech>
2021-02-02 12:48:46 -0500

    Remove run_integration_tests.sh

--- docker-compose/run_integration_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# This script runs the integration tests.
-#
-#     Add any new integration test files to the runtests.py list here. 
-
-python runtests.py --integration apps.integration_tests.integration_test_fhir_resources.IntegrationTestFhirApiResources
