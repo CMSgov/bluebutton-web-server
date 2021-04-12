b0b56865ddc933f7e85471d2c2ff41920131e81a
Dave Tisza <dtisza@fearless.tech>
2021-04-12 12:47:42 -0400

    Create log_global_state_metrics() function
    - Create log_global_state_metrics() function to
      log a "Logging:global_state" type logging event for
      real_bene_cnt, synth_bene_cnt, global_apps_active_cnt,
      global_apps_inactive_cnt, global_apps_require_demographic_scopes_count
      values in apps/logging/loggers.py
    
    BB2-506

---
+++ apps/logging/loggers.py
@@ -0,0 +1,33 @@
+import json
+import logging
+
+from django.conf import settings
+
+from apps.dot_ext.models import get_application_counts, get_application_require_demographic_scopes_count
+from apps.fhir.bluebutton.models import check_crosswalks
+
+
+"""
+  Logger functions for logging  module
+"""
+logger = logging.getLogger('hhs_server.global_metrics')
+
+
+# For use in apps/logging/management/commands/log_global_metrics.py management command
+def log_global_state_metrics(group_timestamp=None):
+    crosswalk_counts = check_crosswalks()
+    application_counts = get_application_counts()
+    require_demographic_scopes_count = get_application_require_demographic_scopes_count()
+
+    log_dict = {"type": "Logging:global_state",
+                "group_timestamp": group_timestamp,
+                "real_bene_cnt": crosswalk_counts.get('real', None),
+                "synth_bene_cnt": crosswalk_counts.get('synthetic', None),
+                "global_apps_active_cnt": application_counts.get('active_cnt', None),
+                "global_apps_inactive_cnt": application_counts.get('inactive_cnt', None),
+                "global_apps_require_demographic_scopes_count": require_demographic_scopes_count, }
+
+    if settings.LOG_JSON_FORMAT_PRETTY:
+        logger.info(json.dumps(log_dict, indent=2))
+    else:
+        logger.info(json.dumps(log_dict))
