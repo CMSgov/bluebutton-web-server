6ca695c170a64001d5bd9aa79f1d198b02f2c035
Dave Tisza <dtisza@fearless.tech>
2021-04-19 15:15:31 -0400

    Create user_signout() method for OAuth2ConfigSLSx
    - Update exchange_for_access_token() method in OAuth2ConfigSLSx class
      to return session_id for later use in apps/mymedicare_cb/authorization.py
    
    - Create __init__() method in OAuth2ConfigSLSx class to initialize
      self.signout_status_code and self.signout_status_mesg vars
      in apps/mymedicare_cb/authorization.py
    
    - Create user_signout() method in OAuth2ConfigSLSx class to utilize the
      SLSx signout endpoint to log out the session for session_id
      in apps/mymedicare_cb/authorization.py
    
    BB2-544

--- apps/mymedicare_cb/authorization.py
+++ apps/mymedicare_cb/authorization.py
@@ -29,9 +29,15 @@ class OAuth2ConfigSLSx(object):
     token_endpoint = settings.SLSX_TOKEN_ENDPOINT
     token_endpoint_aca_token = settings.MEDICARE_SLSX_AKAMAI_ACA_TOKEN
     redirect_uri = settings.MEDICARE_SLSX_REDIRECT_URI
+    signout_endpoint = settings.SLSX_SIGNOUT_ENDPOINT
     userinfo_endpoint = settings.SLSX_USERINFO_ENDPOINT
     healthcheck_endpoint = settings.SLSX_HEALTH_CHECK_ENDPOINT
     verify_ssl = getattr(settings, 'SLSX_VERIFY_SSL', False)
+
+    def __init__(self):
+        self.signout_status_code = None
+        self.signout_status_mesg = None
+        super().__init__()
 
     @property
     def client_id(self):
@@ -90,7 +96,13 @@ class OAuth2ConfigSLSx(object):
                                    "Exchange user_id is missing in response error")
             raise BBMyMedicareSLSxTokenException(settings.MEDICARE_ERROR_MSG)
 
-        return auth_token, user_id
+        session_id = token_response.get('session_id', None)
+        if user_id is None:
+            log_authenticate_start(auth_flow_dict, "FAIL",
+                                   "Exchange user_id is missing in response error")
+            raise BBMyMedicareSLSxTokenException(settings.MEDICARE_ERROR_MSG)
+
+        return auth_token, user_id, session_id
 
     def auth_header(self):
         return {"Authorization": "Bearer %s" % (self.token['access_token'])}
@@ -143,3 +155,23 @@ class OAuth2ConfigSLSx(object):
     def service_health_check(self):
         response = requests.get(self.healthcheck_endpoint, verify=self.verify_ssl)
         response.raise_for_status()
+
+    def user_signout(self, access_token, request):
+        headers = {"Authorization": "Bearer %s" % (access_token)}
+
+        # keep using deprecated conv - no conflict issue
+        headers.update({"X-SLS-starttime": str(datetime.datetime.utcnow())})
+
+        if request is not None:
+            headers.update({"X-Request-ID": str(getattr(request, '_logging_uuid', None)
+                            if hasattr(request, '_logging_uuid') else '')})
+
+        try:
+            response = requests.get(self.signout_endpoint,
+                                    headers=headers,
+                                    verify=self.verify_ssl)
+            self.signout_status_code = response.status_code
+            response.raise_for_status()
+        except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError) as e:
+            # Only setting mesg for logging if a signout fails per BB2-544
+            self.signout_status_mesg = "SLSx signout error {reason}".format(reason=e)
