0a54c4ed49e1093f33e536f6ce38a76d08ccae90
James Fuqian <jfuqian@ellumen.com>
2021-05-26 08:50:25 -0700

    code cleanup

--- apps/fhir/bluebutton/migrations/0001_initial.py
+++ apps/fhir/bluebutton/migrations/0001_initial.py
@@ -12,7 +12,6 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        # ('server', '0017_resourcerouter_wait_time'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
@@ -25,7 +24,6 @@ class Migration(migrations.Migration):
                 ('date_created', models.DateTimeField(auto_now_add=True)),
                 ('user_id_type', models.CharField(choices=[('H', 'HICN'), ('M', 'MBI'), ('S', 'SSN')], default='H', max_length=1)),
                 ('user_id_hash', models.CharField(blank=True, default='', max_length=64, verbose_name='PBKDF2 of User ID')),
-                # ('fhir_source', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='server.ResourceRouter')),
                 ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
