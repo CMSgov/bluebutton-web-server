236ff2ea61d4d675865e85699e0a3a704889e400
Nick Bragdon <nick.bragdon@adhocteam.us>
2021-06-03 17:59:42 -0400

    Updating to return a better error message

--- apps/fhir/bluebutton/authentication.py
+++ apps/fhir/bluebutton/authentication.py
@@ -1,26 +1,36 @@
 from oauth2_provider.contrib.rest_framework import authentication
 from django.utils import timezone
+from rest_framework import exceptions
 
 
 class OAuth2ResourceOwner(authentication.OAuth2Authentication):
     def authenticate(self, request):
         user_auth_tuple = super(OAuth2ResourceOwner, self).authenticate(request)
+        access_token_query_param = request.GET.get("access_token", None)
+        if access_token_query_param is not None:
+            user_auth_tuple = None
+            raise exceptions.ParseError(
+                "Using the access token in the query parameters is not supported. "
+                "Use the Authorization header instead"
+            )
 
         # fix until https://github.com/jazzband/django-oauth-toolkit/commit/f86dfb8a7f20065850fe3b3629e18723658f835d is stable
-        if not hasattr(request, 'oauth2_error'):
+        if not hasattr(request, "oauth2_error"):
             request.oauth2_error = {}
 
         if user_auth_tuple is not None:
             user, access_token = user_auth_tuple
             request.resource_owner = user
-            if not hasattr(user, 'crosswalk'):
+            if not hasattr(user, "crosswalk"):
                 return None
             request.crosswalk = user.crosswalk
 
             # Update Application activity metric datetime fields
             access_token.application.last_active = timezone.now()
             if access_token.application.first_active is None:
-                access_token.application.first_active = access_token.application.last_active
+                access_token.application.first_active = (
+                    access_token.application.last_active
+                )
             access_token.application.save()
 
             return user, access_token
