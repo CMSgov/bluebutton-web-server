d46162c31acc3c7dc566a743edadd473f5921487
Dave Tisza <dtisza@fearless.tech>
2021-07-15 15:21:09 -0400

    Update ArchivedCrosswalkAdmin & migrations for PR review changes
    - Create new migrations apps/fhir/bluebutton/migrations/0002_archivedcrosswalk.py
    
    - Add `username` to ArchivedCrosswalkAdmin in apps/fhir/bluebutton/admin.py
    
    - Remove `get_crosswalk_id` from ArchivedCrosswalkAdmin in apps/fhir/bluebutton/admin.py
    
     BB2-612

--- apps/fhir/bluebutton/admin.py
+++ apps/fhir/bluebutton/admin.py
@@ -18,12 +18,8 @@ admin.site.register(Crosswalk, CrosswalkAdmin)
 
 
 class ArchivedCrosswalkAdmin(admin.ModelAdmin):
-    list_display = ('archived_at', 'get_crosswalk_id', '_fhir_id', 'user_id_type', '_user_id_hash', '_user_mbi_hash')
-    search_fields = ('get_crosswalk_id', '_fhir_id', '_user_id_hash', '_user_mbi_hash')
-    raw_id_fields = ("crosswalk", )
-
-    def get_crosswalk_id(self, obj):
-        return obj.crosswalk.id
+    list_display = ('archived_at', 'username', '_fhir_id', 'user_id_type', '_user_id_hash', '_user_mbi_hash')
+    search_fields = ('_fhir_id', 'username', '_user_id_hash', '_user_mbi_hash')
 
 
 admin.site.register(ArchivedCrosswalk, ArchivedCrosswalkAdmin)
