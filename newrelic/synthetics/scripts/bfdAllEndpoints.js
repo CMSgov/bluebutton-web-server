b722ff1d78192ca3ce3d862d9528ff58b03be482
Nick Bragdon <nick.bragdon@adhocteam.us>
2021-07-28 17:56:16 -0400

    Adding script for bfd all endpoint check in new relic

---
+++ newrelic/synthetics/scripts/bfdAllEndpoints.js
@@ -0,0 +1,74 @@
+/**
+ * Feel free to explore, or check out the full documentation
+ * https://docs.newrelic.com/docs/synthetics/new-relic-synthetics/scripting-monitors/writing-api-tests
+ * for details.
+ */
+
+const assert = require("assert");
+const BASE_URL = 'https://sandbox.bluebutton.cms.gov/';
+const VERSIONS = ['v1', 'v2'];
+const AUTH_TOKEN = '<Dont check in an access token for security purposes>';
+const PATIENT_ID = '<Patient ID should match the access token used>';
+
+const generatePatientUrl = (version) => {
+    return BASE_URL + version + '/fhir/Patient/' + PATIENT_ID;
+}
+
+const generateCoverageUrl = (version) => {
+    return BASE_URL + version + '/fhir/Coverage?beneficiary=' + PATIENT_ID;
+}
+
+const generateEobUrl = (version) => {
+    return BASE_URL + version + '/fhir/ExplanationOfBenefit?patient' + PATIENT_ID;
+}
+
+const patientSuccessEvaluation = (url, parsedBody) => {
+    assert.ok(parsedBody.id === PATIENT_ID, url + ': Expected the patient id to be the same but instead received: ' + parsedBody.id);
+}
+
+const coverageSuccessEvaluation = (url, parsedBody) => {
+    assert.ok(parsedBody.total > 0, url + ': Expected the total number of coverages to be greater than zero but received: ' + parsedBody.total);
+}
+
+const eobSuccessEvaluation = (url, parsedBody) => {
+    assert.ok(parsedBody.total > 0, url + ': Expected the total number of EOBs to be greater than zero but received: ' + parsedBody.total);
+}
+
+const URL_EVALUATIONS = [
+    {
+        urlGenerator: generatePatientUrl,
+        evaluate: patientSuccessEvaluation
+    },
+    {
+        urlGenerator: generateCoverageUrl,
+        evaluate: coverageSuccessEvaluation
+    },
+    {
+        urlGenerator: generateEobUrl,
+        evaluate: eobSuccessEvaluation
+    }
+];
+
+for (const urlEvaluation of URL_EVALUATIONS) {
+    for (const version of VERSIONS) {
+        const requestOptions = {
+            url: urlEvaluation.urlGenerator(version),
+            headers: {
+                'Authorization': 'Bearer ' + AUTH_TOKEN,
+                'Content-Type': 'application/json'
+            }
+        }
+
+        $http.get(requestOptions, function (err, response, body) {
+            if (err) {
+                console.log(err);
+            }
+            assert.ok(
+                response.statusCode === 200,
+                requestOptions.url + ': Expected response status code to be 200, instead received: ' + response.statusCode
+            );
+            const parsedBody = JSON.parse(body);
+            urlEvaluation.evaluate(requestOptions.url, parsedBody);
+        });
+    }
+}
