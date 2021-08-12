ddec40b11e21d2d561f42bdcbe71da4913f0088c
James Fuqian <jfuqian@ellumen.com>
2021-08-12 10:50:55 -0700

    fix django health eps.

--- dev-local/django/mslsx/mslsx/urls.py
+++ dev-local/django/mslsx/mslsx/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     # mslsx end points
     url(r'^sso/authorize\\?.+', login_page),
     url(r'^login/', login),
-    url(r'^health$', health),
+    url(r'^health', health),
     url(r'^sso/session', token),
     url(r'^v1/users/', userinfo),
     url(r'^sso/signout', signout),
