5e5fa70f3af7d4ab25befb1f85230523179cbc7e
James Fuqian <jfuqian@ellumen.com>
2021-10-05 16:10:51 -0700

    print schema to facilitate debugging.

--- apps/integration_tests/logging_tests.py
+++ apps/integration_tests/logging_tests.py
@@ -186,6 +186,7 @@ class LoggingTests(SeleniumTests):
                                 self.assertTrue(re.match(event_desc.get('path_regex'), p))
                             else:
                                 self.assertEqual(p, event_desc.get('path'))
+                            print("SCHEMA={}".format(event_desc.get('schema')))
                             self.assertTrue(validate_json_schema(event_desc.get('schema'), event_json))
                 except JSONDecodeError:
                     # skip non json line
