92bc61a58186fdb9ff59446aad5985f58fad8c00
Nick Bragdon <nick.bragdon@adhocteam.us>
2021-11-22 18:20:50 -0500

    Adding a new admin form for more easily creating production applications

--- apps/dot_ext/admin.py
+++ apps/dot_ext/admin.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib import admin
 from oauth2_provider.models import AccessToken
 from oauth2_provider.models import get_application_model
-from .forms import CustomRegisterApplicationForm
+from .forms import CreateNewApplicationForm, CustomRegisterApplicationForm
 from .models import ApplicationLabel, AuthFlowUuid
 
 
@@ -27,90 +27,194 @@ class MyAuthFlowUuid(AuthFlowUuid):
         app_label = "bluebutton"
 
 
-class CustomAdminApplicationForm(CustomRegisterApplicationForm):
-    description = forms.CharField(label="Application Description",
-                                  help_text="Note text size and HTML tags are not validated under ADMIN.",
-                                  widget=forms.Textarea, empty_value='', required=False)
+class CreateNewApplication(Application):
+    class Meta:
+        proxy = True
+        app_label = "bluebutton"
 
+
+class CustomAdminCreateNewApplicationForm(CreateNewApplicationForm):
     def __init__(self, *args, **kwargs):
         user = None
         super().__init__(user, *args, **kwargs)
-        self.fields['logo_uri'].widget.attrs['readonly'] = False
+
+    def clean(self):
+        print("upper form cleaning")
+        print(str(self.cleaned_data))
+        return self.cleaned_data
 
     class Meta:
         model = MyApplication
         fields = (
-            'client_id',
-            'user',
-            'client_type',
-            'authorization_grant_type',
-            'client_secret',
-            'name',
-            'skip_authorization',
-            'scope',
-            'require_demographic_scopes',
-            'agree',
-            'op_tos_uri',
-            'op_policy_uri',
-            'client_uri',
-            'website_uri',
-            'redirect_uris',
-            'logo_uri',
-            'logo_image',
-            'tos_uri',
-            'policy_uri',
-            'software_id',
-            'contacts',
-            'support_email',
-            'support_phone_number',
-            'description',
-            'active',
-            'first_active',
-            'last_active',
+            "name",
+            "contacts",
+            "redirect_uris",
+            "require_demographic_scopes",
+            "policy_uri",
+            "tos_uri",
+            "website_uri",
+            "support_email",
+            "support_phone_number",
+            "logo_image",
+            "application_description",
+        )
+
+
+class CustomAdminApplicationForm(CustomRegisterApplicationForm):
+    description = forms.CharField(
+        label="Application Description",
+        help_text="Note text size and HTML tags are not validated under ADMIN.",
+        widget=forms.Textarea,
+        empty_value="",
+        required=False,
+    )
+
+    def __init__(self, *args, **kwargs):
+        user = None
+        super().__init__(user, *args, **kwargs)
+        self.fields["logo_uri"].widget.attrs["readonly"] = False
+
+    class Meta:
+        model = MyApplication
+        fields = (
+            "client_id",
+            "user",
+            "client_type",
+            "authorization_grant_type",
+            "client_secret",
+            "name",
+            "skip_authorization",
+            "scope",
+            "require_demographic_scopes",
+            "agree",
+            "op_tos_uri",
+            "op_policy_uri",
+            "client_uri",
+            "website_uri",
+            "redirect_uris",
+            "logo_uri",
+            "logo_image",
+            "tos_uri",
+            "policy_uri",
+            "software_id",
+            "contacts",
+            "support_email",
+            "support_phone_number",
+            "description",
+            "active",
+            "first_active",
+            "last_active",
         )
 
     def clean(self):
         return self.cleaned_data
 
     def clean_agree(self):
-        return self.cleaned_data.get('agree')
+        return self.cleaned_data.get("agree")
 
 
 @admin.register(MyApplication)
 class MyApplicationAdmin(admin.ModelAdmin):
     form = CustomAdminApplicationForm
-    list_display = ("name", "user", "authorization_grant_type", "client_id",
-                    "require_demographic_scopes", "scopes",
-                    "created", "updated", "active", "skip_authorization")
-    list_filter = ("client_type", "authorization_grant_type",
-                   "require_demographic_scopes", "active", "skip_authorization")
+    list_display = (
+        "name",
+        "user",
+        "authorization_grant_type",
+        "client_id",
+        "require_demographic_scopes",
+        "scopes",
+        "created",
+        "updated",
+        "active",
+        "skip_authorization",
+    )
+    list_filter = (
+        "client_type",
+        "authorization_grant_type",
+        "require_demographic_scopes",
+        "active",
+        "skip_authorization",
+    )
     radio_fields = {
         "client_type": admin.HORIZONTAL,
         "authorization_grant_type": admin.VERTICAL,
     }
 
-    search_fields = ('name', 'user__username', '=client_id', '=require_demographic_scopes', '=authorization_grant_type')
+    search_fields = (
+        "name",
+        "user__username",
+        "=client_id",
+        "=require_demographic_scopes",
+        "=authorization_grant_type",
+    )
 
-    raw_id_fields = ("user", )
+    raw_id_fields = ("user",)
+
+
+@admin.register(CreateNewApplication)
+class CreateNewApplicationAdmin(admin.ModelAdmin):
+    form = CreateNewApplicationForm
+    list_display = (
+        "name",
+        "user",
+        "authorization_grant_type",
+        "client_id",
+        "created",
+        "updated",
+        "active",
+        "skip_authorization",
+    )
+    list_filter = (
+        "client_type",
+        "active",
+        "skip_authorization",
+    )
+
+    search_fields = (
+        "name",
+        "user__username",
+        "=client_id",
+    )
+
+    raw_id_fields = ("user",)
 
 
 @admin.register(MyAccessToken)
 class MyAccessTokenAdmin(admin.ModelAdmin):
-    list_display = ('user', 'application', 'expires', 'scope', 'token',)
-    search_fields = ('user__username', 'application__name', 'token', 'source_refresh_token__token')
-    raw_id_fields = ("user", 'application')
+    list_display = (
+        "user",
+        "application",
+        "expires",
+        "scope",
+        "token",
+    )
+    search_fields = (
+        "user__username",
+        "application__name",
+        "token",
+        "source_refresh_token__token",
+    )
+    raw_id_fields = ("user", "application")
 
 
 @admin.register(MyAuthFlowUuid)
 class MyAuthFlowUuidAdmin(admin.ModelAdmin):
-    list_display = ('created', 'auth_uuid', 'state', 'code', 'client_id',
-                    'auth_pkce_method', 'auth_crosswalk_action', 'auth_share_demographic_scopes')
-    search_fields = ('auth_uuid', 'state', 'code')
+    list_display = (
+        "created",
+        "auth_uuid",
+        "state",
+        "code",
+        "client_id",
+        "auth_pkce_method",
+        "auth_crosswalk_action",
+        "auth_share_demographic_scopes",
+    )
+    search_fields = ("auth_uuid", "state", "code")
 
 
 @admin.register(ApplicationLabel)
 class ApplicationLabelAdmin(admin.ModelAdmin):
     model = ApplicationLabel
-    filter_horizontal = ('applications',)
+    filter_horizontal = ("applications",)
     list_display = ("name", "slug", "short_description")
     list_filter = ("name", "slug")
--- apps/dot_ext/forms.py
+++ apps/dot_ext/forms.py
@@ -1,4 +1,5 @@
 import logging
+import uuid
 
 from django import forms
 from django.conf import settings
@@ -6,7 +7,9 @@ from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 from oauth2_provider.forms import AllowForm as DotAllowForm
 from oauth2_provider.models import get_application_model
+from apps.accounts.models import UserProfile
 from apps.dot_ext.validators import validate_logo_image, validate_notags
+from django.contrib.auth.models import Group, User
 
 import apps.logging.request_logger as bb2logging
 
@@ -15,67 +18,84 @@ logger = logging.getLogger(bb2logging.HHS_SERVER_LOGNAME_FMT.format(__name__))
 
 class CustomRegisterApplicationForm(forms.ModelForm):
 
-    logo_image = forms.ImageField(label='Logo URI Image Upload', required=False,
-                                  help_text="Upload your logo image file here in JPEG (.jpg) format! "
-                                  "The maximum file size allowed is %sKB and maximum dimensions are %sx%s pixels. "
-                                  "This will update the Logo URI after saving."
-                                  % (settings.APP_LOGO_SIZE_MAX, settings.APP_LOGO_WIDTH_MAX,
-                                     settings.APP_LOGO_HEIGHT_MAX))
+    logo_image = forms.ImageField(
+        label="Logo URI Image Upload",
+        required=False,
+        help_text="Upload your logo image file here in JPEG (.jpg) format! "
+        "The maximum file size allowed is %sKB and maximum dimensions are %sx%s pixels. "
+        "This will update the Logo URI after saving."
+        % (
+            settings.APP_LOGO_SIZE_MAX,
+            settings.APP_LOGO_WIDTH_MAX,
+            settings.APP_LOGO_HEIGHT_MAX,
+        ),
+    )
 
-    description = forms.CharField(label="Application Description",
-                                        help_text="This is plain-text up to 1000 characters in length.",
-                                        widget=forms.Textarea, empty_value='', required=False,
-                                        max_length=1000, validators=[validate_notags])
+    description = forms.CharField(
+        label="Application Description",
+        help_text="This is plain-text up to 1000 characters in length.",
+        widget=forms.Textarea,
+        empty_value="",
+        required=False,
+        max_length=1000,
+        validators=[validate_notags],
+    )
 
     def __init__(self, user, *args, **kwargs):
-        agree_label = u'Yes I have read and agree to the <a target="_blank" href="%s">API Terms of Service Agreement</a>*' % (
-            settings.TOS_URI)
+        agree_label = (
+            u'Yes I have read and agree to the <a target="_blank" href="%s">API Terms of Service Agreement</a>*'
+            % (settings.TOS_URI)
+        )
         super(CustomRegisterApplicationForm, self).__init__(*args, **kwargs)
-        self.fields['authorization_grant_type'].choices = settings.GRANT_TYPES
-        self.fields['client_type'].initial = 'confidential'
-        self.fields['agree'].label = mark_safe(agree_label)
-        self.fields['name'].label = "Name*"
-        self.fields['name'].required = True
-        self.fields['client_type'].label = "Client Type*"
-        self.fields['authorization_grant_type'].label = "Authorization Grant Type*"
-        self.fields['redirect_uris'].label = "Redirect URIs*"
-        self.fields['logo_uri'].disabled = True
+        self.fields["authorization_grant_type"].choices = settings.GRANT_TYPES
+        self.fields["client_type"].initial = "confidential"
+        self.fields["agree"].label = mark_safe(agree_label)
+        self.fields["name"].label = "Name*"
+        self.fields["name"].required = True
+        self.fields["client_type"].label = "Client Type*"
+        self.fields["authorization_grant_type"].label = "Authorization Grant Type*"
+        self.fields["redirect_uris"].label = "Redirect URIs*"
+        self.fields["logo_uri"].disabled = True
 
     class Meta:
         model = get_application_model()
         fields = (
-            'name',
-            'client_type',
-            'authorization_grant_type',
-            'redirect_uris',
-            'logo_uri',
-            'logo_image',
-            'website_uri',
-            'description',
-            'policy_uri',
-            'tos_uri',
-            'support_email',
-            'support_phone_number',
-            'contacts',
-            'agree',
-            'require_demographic_scopes',
+            "name",
+            "client_type",
+            "authorization_grant_type",
+            "redirect_uris",
+            "logo_uri",
+            "logo_image",
+            "website_uri",
+            "description",
+            "policy_uri",
+            "tos_uri",
+            "support_email",
+            "support_phone_number",
+            "contacts",
+            "agree",
+            "require_demographic_scopes",
         )
 
-    required_css_class = 'required'
+    required_css_class = "required"
 
     def clean(self):
-        client_type = self.cleaned_data.get('client_type')
-        authorization_grant_type = self.cleaned_data.get(
-            'authorization_grant_type')
+        client_type = self.cleaned_data.get("client_type")
+        authorization_grant_type = self.cleaned_data.get("authorization_grant_type")
 
         msg = ""
         validate_error = False
 
         # Validate choices
-        if not (client_type == 'confidential' and authorization_grant_type == 'authorization-code'):
+        if not (
+            client_type == "confidential"
+            and authorization_grant_type == "authorization-code"
+        ):
             validate_error = True
-            msg += 'Only a confidential client and ' \
-                   'authorization-code grant type are allowed at this time.'
+            msg += (
+                "Only a confidential client and "
+                "authorization-code grant type are allowed at this time."
+            )
 
         if validate_error:
             msg_output = _(msg)
@@ -86,45 +106,53 @@ class CustomRegisterApplicationForm(forms.ModelForm):
         return self.cleaned_data
 
     def clean_name(self):
-        name = self.cleaned_data.get('name')
+        name = self.cleaned_data.get("name")
         app_model = get_application_model()
-        if app_model.objects.filter(name__iexact=name).exclude(pk=self.instance.pk).exists():
-            raise forms.ValidationError("""
+        if (
+            app_model.objects.filter(name__iexact=name)
+            .exclude(pk=self.instance.pk)
+            .exists()
+        ):
+            raise forms.ValidationError(
+                """
                                         It looks like this application name
                                         is already in use with another app.
                                         Please enter a different application
                                         name to prevent future errors.
                                         Note that names are case-insensitive.
-                                        """)
+                                        """
+            )
         return name
 
     def clean_agree(self):
-        agree = self.cleaned_data.get('agree')
+        agree = self.cleaned_data.get("agree")
         if not agree:
-            msg = _('You must agree to the API Terms of Service Agreement')
+            msg = _("You must agree to the API Terms of Service Agreement")
             raise forms.ValidationError(msg)
         return agree
 
     def clean_redirect_uris(self):
-        redirect_uris = self.cleaned_data.get('redirect_uris')
-        if getattr(settings, 'BLOCK_HTTP_REDIRECT_URIS', True):
+        redirect_uris = self.cleaned_data.get("redirect_uris")
+        if getattr(settings, "BLOCK_HTTP_REDIRECT_URIS", True):
             if redirect_uris:
                 for u in redirect_uris.split():
                     if u.startswith("http://"):
-                        msg = _('Redirect URIs must not use http.')
+                        msg = _("Redirect URIs must not use http.")
                         raise forms.ValidationError(msg)
         return redirect_uris
 
     def clean_logo_image(self):
-        logo_image = self.cleaned_data.get('logo_image')
-        if getattr(logo_image, 'name', False):
+        logo_image = self.cleaned_data.get("logo_image")
+        if getattr(logo_image, "name", False):
             validate_logo_image(logo_image)
         return logo_image
 
     def clean_require_demographic_scopes(self):
-        require_demographic_scopes = self.cleaned_data.get('require_demographic_scopes')
+        require_demographic_scopes = self.cleaned_data.get("require_demographic_scopes")
         if type(require_demographic_scopes) != bool:
-            msg = _('Does your application need to collect beneficary demographic information must be (Yes/No).')
+            msg = _(
+                "Does your application need to collect beneficary demographic information must be (Yes/No)."
+            )
             raise forms.ValidationError(msg)
         return require_demographic_scopes
 
@@ -132,12 +160,146 @@ class CustomRegisterApplicationForm(forms.ModelForm):
         app = self.instance
         # Only log agreement from a Register form
         if app.agree and type(self) == CustomRegisterApplicationForm:
-            logmsg = "%s agreed to %s for the application %s" % (app.user, app.op_tos_uri,
-                                                                 app.name)
+            logmsg = "%s agreed to %s for the application %s" % (
+                app.user,
+                app.op_tos_uri,
+                app.name,
+            )
             logger.info(logmsg)
         app = super().save(*args, **kwargs)
         app.save()
-        uri = app.store_media_file(self.cleaned_data.pop('logo_image', None), "logo.jpg")
+        uri = app.store_media_file(
+            self.cleaned_data.pop("logo_image", None), "logo.jpg"
+        )
+        if uri:
+            app.logo_uri = uri
+            app.save()
+        return app
+
+
+class CreateNewApplicationForm(forms.ModelForm):
+    logo_image = forms.ImageField(
+        label="Logo URI Image Upload",
+        required=False,
+        help_text="Upload your logo image file here in JPEG (.jpg) format! "
+        "The maximum file size allowed is %sKB and maximum dimensions are %sx%s pixels. "
+        "This will update the Logo URI after saving."
+        % (
+            settings.APP_LOGO_SIZE_MAX,
+            settings.APP_LOGO_WIDTH_MAX,
+            settings.APP_LOGO_HEIGHT_MAX,
+        ),
+    )
+    application_description = forms.CharField(
+        label="Application Description",
+        help_text="This is plain-text up to 1000 characters in length.",
+        widget=forms.Textarea,
+        empty_value="",
+        required=False,
+        max_length=1000,
+        validators=[validate_notags],
+    )
+    organization_name = forms.CharField(required=True)
+
+    class Meta:
+        model = get_application_model()
+        fields = (
+            "name",
+            "organization_name",
+            "contacts",
+            "redirect_uris",
+            "require_demographic_scopes",
+            "policy_uri",
+            "tos_uri",
+            "website_uri",
+            "support_email",
+            "support_phone_number",
+            "logo_image",
+            "application_description",
+        )
+
+    def clean_name(self):
+        print("clean name")
+
+        name = self.cleaned_data.get("name")
+        print(str(name))
+        app_model = get_application_model()
+        if (
+            app_model.objects.filter(name__iexact=name)
+            .exclude(pk=self.instance.pk)
+            .exists()
+        ):
+            raise forms.ValidationError(
+                """
+                                        It looks like this application name
+                                        is already in use with another app.
+                                        Please enter a different application
+                                        name to prevent future errors.
+                                        Note that names are case-insensitive.
+                                        """
+            )
+        return name
+
+    def clean_logo_image(self):
+        print("clean logo image")
+
+        logo_image = self.cleaned_data.get("logo_image")
+        print(str(logo_image))
+        if getattr(logo_image, "name", False):
+            validate_logo_image(logo_image)
+        return logo_image
+
+    def clean_redirect_uris(self):
+        print("clean redirect uris")
+
+        redirect_uris = self.cleaned_data.get("redirect_uris")
+        print(str(redirect_uris))
+        if getattr(settings, "BLOCK_HTTP_REDIRECT_URIS", True):
+            if redirect_uris:
+                for u in redirect_uris.split():
+                    if u.startswith("http://"):
+                        msg = _("Redirect URIs must not use http.")
+                        raise forms.ValidationError(msg)
+        return redirect_uris
+
+    def clean_require_demographic_scopes(self):
+        print("clean demo scopes")
+
+        require_demographic_scopes = self.cleaned_data.get("require_demographic_scopes")
+        print(str(require_demographic_scopes))
+        if type(require_demographic_scopes) != bool:
+            msg = _(
+                "Does your application need to collect beneficary demographic information must be (Yes/No)."
+            )
+            raise forms.ValidationError(msg)
+        return require_demographic_scopes
+
+    def save(self, *args, **kwargs):
+        app = self.instance
+        print("save")
+        print(str(self.cleaned_data))
+
+        new_user_model = User.objects.create(
+            username=self.cleaned_data.get("name") + "@example.com",
+            password=str(uuid.uuid4()),
+            is_active=True,
+        )
+        group = Group.objects.get(name="BlueButton")
+        new_user_model.groups.add(group)
+        new_user_model.save()
+
+        UserProfile.objects.create(
+            user=new_user_model,
+            organization_name=self.cleaned_data.get("organization_name"),
+        )
+
+        app = super().save(*args, **kwargs)
+        app.agree = True
+        app.user = new_user_model
+        app.save()
+        uri = app.store_media_file(
+            self.cleaned_data.pop("logo_image", None), "logo.jpg"
+        )
         if uri:
             app.logo_uri = uri
             app.save()
@@ -154,12 +316,17 @@ class SimpleAllowForm(DotAllowForm):
         scope = cleaned_data.get("scope", None)
 
         if scope is None:
-            cleaned_data['scope'] = ""
+            cleaned_data["scope"] = ""
             scope = ""
 
         # Remove demographic information scopes, if beneficiary is not sharing
         if cleaned_data.get("share_demographic_scopes") == "False":
-            cleaned_data['scope'] = ' '.join([s for s in scope.split(" ")
-                                             if s not in settings.BENE_PERSONAL_INFO_SCOPES])
+            cleaned_data["scope"] = " ".join(
+                [
+                    s
+                    for s in scope.split(" ")
+                    if s not in settings.BENE_PERSONAL_INFO_SCOPES
+                ]
+            )
 
         return cleaned_data
