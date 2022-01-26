8e23e9a482a7b677cad22b16c5b56d36a11458c2
Dave Tisza <dtisza@fearless.tech>
2022-01-26 12:25:09 -0500

    Update log_global_state_metrics management command
    - Add "--no-report" argument flag to DISABLE the reporting to the console for suppressing in tests.
    
    BB2-944

--- apps/logging/management/commands/log_global_state_metrics.py
+++ apps/logging/management/commands/log_global_state_metrics.py
@@ -10,8 +10,14 @@ class Command(BaseCommand):
         "Managment command to log global state type metrics when called on a schedule."
     )
 
+    def add_arguments(self, parser):
+        # Optional argument to prevent console report in unit tests.
+        parser.add_argument('-n', '--no-report', action="store_true", help='Do not include report for console.', )
+
     def handle(self, *args, **options):
         # Timestamp used to group both types of logging events from this command
         group_timestamp = format_timestamp(datetime.now())
+       
+        report_flag = False if options.get("no_report", None) else True
 
-        log_global_state_metrics(group_timestamp)
+        log_global_state_metrics(group_timestamp, report_flag)
