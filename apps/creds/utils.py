be589f2590b870bd900c9b1aca6015df14e56752
James Fuqian <jfuqian@ellumen.com>
2022-02-15 09:00:32 -0800

    [BB2-1105] Generate New App Creds on each Creds Request fetch/download (#1012)
    * changed fetch/download logic so that related application's client_id and client_secret re-generated.
    
    * fix CI alert.
    
    * generate only client_secret at creds request record creation time, keep client_id unchanged - per PR review.
    
    * fix edge case: deliver creds for app owned by 'root'

--- apps/creds/utils.py
+++ apps/creds/utils.py
@@ -1,9 +1,11 @@
+import string
+
 from django.conf import settings
 from django.contrib.auth.models import User
 from oauth2_provider.models import get_application_model
+from rest_framework import exceptions, status
 
 from apps.accounts.models import UserProfile
-
 from .models import CredentialingReqest
 
 
@@ -14,39 +16,58 @@ def get_url(creds_request_id):
     return "{}/creds/{}".format(settings.HOSTNAME_URL, creds_request_id)
 
 
-def get_creds_by_id(creds_request_id: str):
-    return get_creds_by_obj(CredentialingReqest.objects.get(id=creds_request_id))
+def get_app_creds(creds_request_id: string):
+    creds_req = CredentialingReqest.objects.get(id=creds_request_id)
+
+    creds_dict = get_app_usr_info(creds_req)
+
+    if creds_req.updated_at is not None:
+        raise exceptions.PermissionDenied(
+            "Credentials already fetched (download), doing it again not allowed.",
+            code=status.HTTP_403_FORBIDDEN,
+        )
+
+    app = Application.objects.get(pk=creds_req.application_id)
+
+    creds_dict.update(
+        {
+            "client_id": app.client_id,
+            "client_secret": app.client_secret,
+        }
+    )
+
+    return creds_dict
 
 
-def get_creds_by_obj(creds_req: CredentialingReqest):
-    creds_dict = {
+def get_app_usr_info(creds_req: CredentialingReqest):
+
+    app_usr_info = {
         "user_name": None,
         "org_name": None,
         "app_id": None,
         "app_name": None,
-        "client_id": None,
-        "client_secret": None,
+        "creds_req_id": creds_req.id,
     }
-    if creds_req:
 
-        app = Application.objects.get(pk=creds_req.application_id)
+    app = Application.objects.get(pk=creds_req.application_id)
 
-        if app:
-            creds_dict.update(
-                {
-                    "app_id": app.id,
-                    "app_name": app.name,
-                    "client_id": app.client_id,
-                    "client_secret": app.client_secret,
-                }
-            )
+    if app:
+        app_usr_info.update(
+            {
+                "app_id": app.id,
+                "app_name": app.name,
+            }
+        )
 
-            user = User.objects.get(pk=app.user_id)
+        user = User.objects.get(pk=app.user_id)
 
-            if user:
-                creds_dict.update({"user_name": user.username})
+        if user:
+            app_usr_info.update({"user_name": user.username})
+            try:
                 usrprofile = UserProfile.objects.get(user=user)
                 if usrprofile:
-                    creds_dict.update({"org_name": usrprofile.organization_name})
+                    app_usr_info.update({"org_name": usrprofile.organization_name})
+            except UserProfile.DoesNotExist:
+                pass
 
-    return creds_dict
+    return app_usr_info
