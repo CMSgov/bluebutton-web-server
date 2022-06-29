42d0c9778138668718d475d96245a67a5b35d61e
JAMES FUQIAN <jfuqian@ellumen.com>
2022-06-29 16:45:04 -0700

    added debugging and tracing..

--- manage.py
+++ manage.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 import os
 import sys
-# import dotenv
 from dotenv import load_dotenv
+import logging
+
+logger = logging.getLogger("bb2_entry_point")
+
+logger.error("Hey Django got executed................................")
 
 if __name__ == "__main__":
+    logger.error("Hey Django got executed................................ about to load env")
     if os.environ.get('DJANGO_DOTENV_FILE', None):
         # dotenv.read_dotenv(os.environ.get('DJANGO_DOTENV_FILE'))
         load_dotenv(os.environ.get('DJANGO_DOTENV_FILE'))
+        logger.error("Hey Django got executed................................ load env {}".format(os.environ.get('DJANGO_DOTENV_FILE')))
     else:
         # dotenv.read_dotenv()
         load_dotenv()
+        logger.error("Hey Django got executed................................ load env from default env (.env)")
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                           "hhs_oauth_server.settings.base")
