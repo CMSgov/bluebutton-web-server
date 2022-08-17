2a6463181a1b18e46b9ec0d9252948eeeaa77f6a
Don Seymour <don.seymour@adhocteam.us>
2022-08-17 15:45:00 -0500

    [ BB2-1318 ] local dev switch from keybase to param store (#1035)
    * modified developer scripts and readme updates
    
    * add docker login for cbc int test and readme aws
    
    * add mfa token script and tutorial to readme
    
    * example mfa login_aws function
    
    * add cygwin condition for base64 decode
    
    * fix syntax conditional error
    
    * bump versions of Django and PyJWT to mediate vulns.
    
    * Revert "bump versions of Django and PyJWT to mediate vulns."
    
    This reverts commit 2977936ab09edab4d4279a47a228d5cc59be3c4c.
    
    Co-authored-by: JAMES FUQIAN <jfuqian@ellumen.com>

--- docker-compose/run_integration_tests_local.sh
+++ docker-compose/run_integration_tests_local.sh
@@ -8,9 +8,9 @@
 #
 # NOTE:
 #
-#   When backend is a remote BFD, e.g. prodution sandbox:
+#   When backend is a remote BFD, e.g. production/sandbox:
 #
-#   1. You must be logged in to Keybase and have the BB2 team file system mounted.
+#   1. You must be logged in to AWS CLI.
 #
 #   2. You must also be connected to the VPN.
 #
@@ -19,23 +19,18 @@
 # SETTINGS:  You may need to customize these for your local setup.
 HOSTNAME_URL="http://localhost:8000"
 
-KEYBASE_ENV_FILE="team/bb20/infrastructure/creds/ENV_secrets_for_local_integration_tests.env"
-KEYBASE_CERTFILES_SUBPATH="team/bb20/infrastructure/certs/local_integration_tests/fhir_client/certstore/"
-
 DJANGO_FHIR_CERTSTORE="/certstore"
 CERTSTORE_TEMPORARY_MOUNT_PATH="/tmp/certstore"
-CERT_FILENAME="client_data_server_bluebutton_local_integration_tests_certificate.pem"
-KEY_FILENAME="client_data_server_bluebutton_local_integration_tests_private_key.pem"
 
 DOCKER_IMAGE="public.ecr.aws/f5g8o1y9/bb2-cbc-build"
-DOCKER_TAG="py36-an27-tf11"
+#DOCKER_TAG="py36-an27-tf11"
+DOCKER_TAG="py37-an27-tf12-boto3-botocore"
 
 # Backend FHIR server to use for integration tests of FHIR resource endpoints:
 FHIR_URL="https://prod-sbx.bfd.cms.gov"
 
 # List of integration tests to run. To be passed in to runtests.py.
 INTEGRATION_TESTS_LIST="apps.integration_tests.integration_test_fhir_resources.IntegrationTestFhirApiResources"
-
 
 # Echo function that includes script name on each line for console log readability
 echo_msg () {
@@ -77,11 +72,11 @@ export SUPER_USER_PASSWORD=dummy
 export BB20_ENABLE_REMOTE_DEBUG=dummy
 export BB20_REMOTE_DEBUG_WAIT_ATTACH=dummy
 
+# Set SYSTEM
+SYSTEM=$(uname -s)
+
 # Set bash builtins for safety
 set -e -u -o pipefail
-
-# Set KeyBase ENV path based on your type of system
-SYSTEM=$(uname -s)
 
 DEBUG_OPTS=""
 EXPO_PORTS=""
@@ -128,41 +123,15 @@ then
     docker-compose run ${EXPO_PORTS} -e FHIR_URL=${FHIR_URL} -e HOSTNAME_URL=${HOSTNAME_URL} \
         web bash -c "python ${DEBUG_OPTS} runtests.py --integration ${INTEGRATION_TESTS_LIST}"
 else
-    # cbc or dc mode
-    echo_msg " - Setting Keybase location based on SYSTEM type: ${SYSTEM}"
-    echo_msg
+    # bc or dc mode
+    # BFD prod-sbx settings
+    export DJANGO_USER_ID_SALT=$(aws ssm get-parameters --names /bb2/test/app/django_user_id_salt --query "Parameters[].Value" --output text --with-decryption)
+    export DJANGO_USER_ID_ITERATIONS=$(aws ssm get-parameters --names /bb2/test/app/django_user_id_iterations --query "Parameters[].Value" --output text --with-decryption)
 
-    if [[ ${SYSTEM} == "Linux" ]]
-    then
-        keybase_env_path="/keybase"
-    elif [[ ${SYSTEM} == "Darwin" ]]
-    then
-        keybase_env_path="/Volumes/keybase"
-    else
-        # support cygwin
-        keybase_env_path="/cygdrive/k"
-        CERTSTORE_TEMPORARY_MOUNT_PATH="./docker-compose/certstore"
-        DJANGO_FHIR_CERTSTORE="/code/docker-compose/certstore"
-    fi
-
-    # Keybase ENV file
-    keybase_env="${keybase_env_path}/${KEYBASE_ENV_FILE}"
-
-    echo_msg " - Sourcing ENV secrets from: ${keybase_env}"
-    echo_msg
-
-    # Check that ENV file exists in correct location
-    if [ ! -f "${keybase_env}" ]
-    then
-        echo_msg
-        echo_msg "ERROR: The ENV secrets could NOT be found at: ${keybase_env}"
-        echo_msg
-        exit 1
-    fi
-
-    # Source ENVs
-    source "${keybase_env}"
-
+    # SLSx test env settings
+    export DJANGO_SLSX_CLIENT_ID=$(aws ssm get-parameters --names /bb2/test/app/slsx_client_id --query "Parameters[].Value" --output text --with-decryption)
+    export DJANGO_SLSX_CLIENT_SECRET=$(aws ssm get-parameters --names /bb2/test/app/slsx_client_secret --query "Parameters[].Value" --output text --with-decryption)
+    export DJANGO_PASSWORD_HASH_ITERATIONS=$(aws ssm get-parameters --names /bb2/test/app/django_password_hash_iterations --query "Parameters[].Value" --output text --with-decryption)
 
     # Check ENV vars have been sourced
     if [ -z "${DJANGO_USER_ID_SALT}" ]
@@ -187,36 +156,19 @@ else
         echo_msg "  - OK: Created the temporary certstore mount path at: ${CERTSTORE_TEMPORARY_MOUNT_PATH}"
     fi
 
-    # Keybase cert files
-    keybase_certfiles="${keybase_env_path}/${KEYBASE_CERTFILES_SUBPATH}"
-    keybase_cert_file="${keybase_certfiles}/${CERT_FILENAME}"
-    keybase_key_file="${keybase_certfiles}/${KEY_FILENAME}"
-
-    # Check that certfiles in keybase dir exist
-    if [ -f "${keybase_cert_file}" ]
-    then
-        echo_msg
-        echo_msg "  - OK: The cert file was found at: ${keybase_cert_file}"
-    else
-        echo_msg
-        echo_msg "ERROR: The cert file could NOT be found at: ${keybase_cert_file}"
-        exit 1
-    fi
-    if [ -f ${keybase_key_file} ]
-    then
-        echo_msg
-        echo_msg "  - OK: The key file was found at: ${keybase_key_file}"
-    else
-        echo_msg
-        echo_msg "ERROR: The key file could NOT be found at: ${keybase_key_file}"
-        exit 1
-    fi
-
-    # Copy certfiles from KeyBase to local for container mount
-    echo_msg "  - COPY certfiles from KeyBase to local temp for container mount..."
+    # Copy certfiles from AWS Secrets Manager to local for container mount
+    echo_msg "  - COPY certfiles from AWS Secrets Manager to local temp for container mount..."
     echo_msg
-    cp ${keybase_cert_file} "${CERTSTORE_TEMPORARY_MOUNT_PATH}/ca.cert.pem"
-    cp ${keybase_key_file} "${CERTSTORE_TEMPORARY_MOUNT_PATH}/ca.key.nocrypt.pem"
+
+    if [[ ${SYSTEM} == "Linux" || ${SYSTEM} == "Darwin" ]]
+    then
+        aws secretsmanager get-secret-value --secret-id /bb2/local_integration_tests/fhir_client/certstore/local_integration_tests_certificate --query 'SecretString' --output text |base64 -d > ${CERTSTORE_TEMPORARY_MOUNT_PATH}/ca.cert.pem
+        aws secretsmanager get-secret-value --secret-id /bb2/local_integration_tests/fhir_client/certstore/local_integration_tests_private_key --query 'SecretString' --output text |base64 -d > ${CERTSTORE_TEMPORARY_MOUNT_PATH}/ca.key.nocrypt.pem
+    else
+        # support cygwin
+        aws secretsmanager get-secret-value --secret-id /bb2/local_integration_tests/fhir_client/certstore/local_integration_tests_certificate --query 'SecretString' --output text |base64 -di > ${CERTSTORE_TEMPORARY_MOUNT_PATH}/ca.cert.pem
+        aws secretsmanager get-secret-value --secret-id /bb2/local_integration_tests/fhir_client/certstore/local_integration_tests_private_key --query 'SecretString' --output text |base64 -di > ${CERTSTORE_TEMPORARY_MOUNT_PATH}/ca.key.nocrypt.pem
+    fi
 
     # Check if tests are to run in docker-compose
     if [[ $1 == dc* ]]
@@ -266,12 +218,15 @@ else
         echo_msg
         echo_msg "    INTEGRATION_TESTS_LIST: ${INTEGRATION_TESTS_LIST}"
         echo_msg
+        aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/f5g8o1y9
         docker run \
             -e DJANGO_USER_ID_SALT=${DJANGO_USER_ID_SALT} \
             -e DJANGO_USER_ID_ITERATIONS=${DJANGO_USER_ID_ITERATIONS} \
+            -e DJANGO_SLSX_CLIENT_ID=${DJANGO_SLSX_CLIENT_ID} \
+            -e DJANGO_SLSX_CLIENT_SECRET=${DJANGO_SLSX_CLIENT_SECRET} \
+            -e DJANGO_PASSWORD_HASH_ITERATIONS=${DJANGO_PASSWORD_HASH_ITERATIONS} \
             -e DJANGO_FHIR_CERTSTORE=${DJANGO_FHIR_CERTSTORE} \
             -e FHIR_URL=${FHIR_URL} \
-            --env-file ${keybase_env} \
             --mount type=bind,source="$(pwd)",target=/app,readonly \
             --mount type=bind,source="${CERTSTORE_TEMPORARY_MOUNT_PATH}",target=/certstore,readonly \
             --rm \
