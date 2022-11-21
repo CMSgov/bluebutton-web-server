b528f64a15bd19de7cee89cf7cd30b2afe9e22c9
Dave Tisza <dtisza@fearless.tech>
2022-11-21 13:26:03 -0500

    Improve DataAccessGrant admin

--- apps/authorization/admin.py
+++ apps/authorization/admin.py
@@ -2,7 +2,23 @@ from django.contrib import admin
 from .models import DataAccessGrant
 
 
-#class DataAccessGrantAdmin(admin.ModelAdmin):
+class DataAccessGrantAdmin(admin.ModelAdmin):
+    list_display = (
+        "application",
+        "beneficiary",
+        "expiration_date",
+        "created_at",
+    )
+
+    list_filter = (
+        "created_at",
+        "expiration_date",
+    )
+
+    search_fields = (
+        "application",
+        "beneficiary",
+    )
 
 
-#admin.site.register(DataAccessGrant, DataAccessGrantAdmin)
+admin.site.register(DataAccessGrant, DataAccessGrantAdmin)
