1b57fc70b9d6c7d2dc0cb69ee54dd9c0d8e5b476
JAMES FUQIAN <jfuqian@ellumen.com>
2023-04-20 08:20:18 -0700

    fix linting

--- apps/integration_tests/selenium_generic.py
+++ apps/integration_tests/selenium_generic.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
 
 from .selenium_cases import (
