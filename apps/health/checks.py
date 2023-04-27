7ee8cb1e04f8ce431f7876718bf3999072c931ca
Adrian Jones <adrian.jones@adhocteam.us>
2023-04-27 14:29:51 -0400

    BB2-2297 Use different method for health checkpoint due to ps not finding splunkd

--- apps/health/checks.py
+++ apps/health/checks.py
@@ -21,8 +21,8 @@ def django_rds_database(v2=False):
 
 def splunk_services(v2=False):
     if switch_is_active('splunk_monitor'):
-        pl = subprocess.Popen(['ps', '-U', '0'], stdout=subprocess.PIPE).communicate()[0]
-        if "splunkd" in str(pl):
+        pl = subprocess.Popen(['systemctl', '-l'], stdout=subprocess.PIPE).communicate()[0]
+        if "SplunkForwarder.serviceloadedactive" in str(pl).replace(" ",""):
             return True
         return False
     return True
