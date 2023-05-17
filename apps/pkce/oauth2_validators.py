03da1f5114dd8c1ba70c6230c2de16b61080159b
JAMES FUQIAN <jfuqian@ellumen.com>
2023-05-16 21:50:28 -0700

    fix broken authorization logic etc., PKCE default to False.

--- apps/pkce/oauth2_validators.py
+++ apps/pkce/oauth2_validators.py
@@ -13,6 +13,12 @@ class PKCEValidatorMixin(object):
     def save_authorization_code(self, client_id, code, request, *args, **kwargs):
         expires = timezone.now() + timedelta(
             seconds=oauth2_settings.AUTHORIZATION_CODE_EXPIRE_SECONDS)
+
+        challenge = request.code_challenge if hasattr(request, 'code_challenge') and request.code_challenge else ""
+        # this is needed to match the challenge verification logic in oauth provider
+        challenge = challenge.rstrip('=')
+        challenge_method = getattr(request, 'code_challenge_method', "")
+
         g = Grant(
             application=request.client,
             user=request.user,
@@ -20,11 +26,16 @@ class PKCEValidatorMixin(object):
             expires=expires,
             redirect_uri=request.redirect_uri,
             scope=" ".join(request.scopes),
+            code_challenge=challenge,
+            code_challenge_method=challenge_method if challenge_method is not None else "",
         )
+
+        # may be demarcated in atomic?
         g.save()
-        if hasattr(request, 'code_challenge') and request.code_challenge:
+
+        if challenge and challenge_method:
             CodeChallenge.objects.create(
                 grant=g,
-                challenge=request.code_challenge,
-                challenge_method=getattr(request, 'code_challenge_method', None),
+                challenge=challenge,
+                challenge_method=challenge_method,
             )
