1282cd3498549e32f9990b1b7af88502ac204b73
JAMES FUQIAN <jfuqian@ellumen.com>
2023-05-22 16:07:41 -0700

    fix lint

--- apps/fhir/server/utils.py
+++ apps/fhir/server/utils.py
@@ -59,7 +59,7 @@ def init_text_list(replace_with=''):
                                              type(replace_with)))
 
     if type(replace_with) is list:
-        return(list_to_text(replace_with))
+        return (list_to_text(replace_with))
 
     new_text_list = [replace_with, ]
     return (list_to_text(new_text_list))
