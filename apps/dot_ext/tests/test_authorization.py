72aa7d443880a33b1e80814cc8daf8f605756536
Adrian Jones <84713593+ajshred@users.noreply.github.com>
2024-01-26 17:22:19 -0500

    BB2-2935 Disallow token refreshes on expired 13-month access grants (#1163)
    * Disallow token refreshes on expired 13-month access grants
    
    * Fix linting error
    
    * Update from ConsentRequired to InvalidGrantError after team discussion
    
    * Added a failure case for refresh when grant or refresh token is not found.

--- apps/dot_ext/tests/test_authorization.py
+++ apps/dot_ext/tests/test_authorization.py
@@ -1,5 +1,8 @@
 import json
 import base64
+import pytz
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 # from oauth2_provider.compat import parse_qs, urlparse
 from urllib.parse import parse_qs, urlparse
 from oauth2_provider.models import get_access_token_model, get_refresh_token_model
@@ -322,6 +325,71 @@ class TestAuthorizeWithCustomScheme(BaseApiTest):
         tkn = response.json()['access_token']
         refresh_tkn = response.json()['refresh_token']
         at = AccessToken.objects.get(token=tkn)
+        at.delete()
+        refresh_request_data = {
+            'grant_type': 'refresh_token',
+            'refresh_token': refresh_tkn,
+            'redirect_uri': redirect_uri,
+            'client_id': application.client_id,
+            'client_secret': application.client_secret_plain,
+        }
+        response = self.client.post(reverse('oauth2_provider:token'), data=refresh_request_data)
+        self.assertEqual(response.status_code, 400)
+
+    @override_flag('limit_data_access', active=True)
+    def test_refresh_13_month_with_expired_grant(self):
+        redirect_uri = 'http://localhost'
+        # create a user
+        self._create_user('anna', '123456')
+        capability_a = self._create_capability('Capability A', [])
+        capability_b = self._create_capability('Capability B', [])
+        # create an application and add capabilities
+        application = self._create_application(
+            'an app',
+            grant_type=Application.GRANT_AUTHORIZATION_CODE,
+            client_type=Application.CLIENT_CONFIDENTIAL,
+            redirect_uris=redirect_uri)
+        application.scope.add(capability_a, capability_b)
+        # user logs in
+        request = HttpRequest()
+        self.client.login(request=request, username='anna', password='123456')
+        # post the authorization form with only one scope selected
+        payload = {
+            'client_id': application.client_id,
+            'response_type': 'code',
+            'redirect_uri': redirect_uri,
+            'scope': ['capability-a'],
+            'expires_in': 86400,
+            'allow': True,
+        }
+        response = self.client.post(reverse('oauth2_provider:authorize'), data=payload)
+        self.client.logout()
+        self.assertEqual(response.status_code, 302)
+        # now extract the authorization code and use it to request an access_token
+        query_dict = parse_qs(urlparse(response['Location']).query)
+        authorization_code = query_dict.pop('code')
+        token_request_data = {
+            'grant_type': 'authorization_code',
+            'code': authorization_code,
+            'redirect_uri': redirect_uri,
+            'client_id': application.client_id,
+            'client_secret': application.client_secret_plain,
+        }
+        c = Client()
+        response = c.post('/v1/o/token/', data=token_request_data)
+        self.assertEqual(response.status_code, 200)
+        # Now we have a token and refresh token
+        tkn = response.json()['access_token']
+        refresh_tkn = response.json()['refresh_token']
+        at = AccessToken.objects.get(token=tkn)
+        dag = DataAccessGrant.objects.get(
+            beneficiary=at.user,
+            application=application
+        )
+        dag.expiration_date = datetime.now().replace(
+            tzinfo=pytz.UTC
+        ) + relativedelta(months=-1)
+        dag.save()
         at.delete()
         refresh_request_data = {
             'grant_type': 'refresh_token',
