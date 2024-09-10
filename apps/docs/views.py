4803b879ba2b00562347d3c4cc59d3eb91507a9b
Shivam Tiwari <shivam.tiwari@semanticbits.com>
2024-09-10 10:57:08 -0400

    BB2-3389: Serve openapi oauth2-redirect.html from server instead from static folder

--- apps/docs/views.py
+++ apps/docs/views.py
@@ -3,6 +3,7 @@ from rest_framework import exceptions
 from waffle import switch_is_active
 
 OPENAPI_PAGE = "openapi.html"
+OPENAPI_OAUTH2_REDIRECT_PAGE = "oauth2-redirect.html"
 
 
 def openapi(request):
@@ -11,3 +12,11 @@ def openapi(request):
         # response NOT FOUND 
         raise exceptions.NotFound("enable_swaggerui not active.")
     return render(request, OPENAPI_PAGE)
+
+
+def openapi_oauth2_redirect(request):
+    # serve swagger ui landing page
+    if not switch_is_active("enable_swaggerui"):
+        # response NOT FOUND 
+        raise exceptions.NotFound("enable_swaggerui not active.")
+    return render(request, OPENAPI_OAUTH2_REDIRECT_PAGE)
