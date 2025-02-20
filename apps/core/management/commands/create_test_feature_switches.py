8a385b488c31b842bf6ec2edc607b093638d8c7d
jimmyfagan <jimmyfagan@navapbc.com>
2025-02-20 09:13:53 -0700

    Add additional switch descriptions.

--- apps/core/management/commands/create_test_feature_switches.py
+++ apps/core/management/commands/create_test_feature_switches.py
@@ -4,19 +4,20 @@ from waffle.models import Switch
 from apps.core.models import Flag
 
 WAFFLE_FEATURE_SWITCHES = (
-    ("enable_swaggerui", True, ""),
-    ("enable_testclient", True, ""),
-    ("expire_grant_endpoint", True, "This enables the /v<1/2>/o/expire_authenticated_user/<patient_id>/ endpoint"),
-    ("interim-prod-access", True, ""),
-    ("login", True, ""),
-    ("outreach_email", True, ""),
-    ("require-scopes", True, ""),
+    ("enable_swaggerui", True, "This enables a page for the openapi docs and a link to the page from the main page."),
+    ("enable_testclient", True, "This enables the test client."),
+    ("expire_grant_endpoint", True, "This enables the /v<1/2>/o/expire_authenticated_user/<patient_id>/ endpoint."),
+    ("interim-prod-access", True, "This controls access to the interim prod access form. \
+        This is outdated and will be removed in a future ticket"),
+    ("login", True, "This enables login related URLs and code. See apps/accounts/urls.py file for more info."),
+    ("outreach_email", True, "This enables developer outreach emails. Not active in prod."),
+    ("require-scopes", True, "Thie enables enforcement of permission checking of scopes."),
     ("show_django_message_sdk", True, "This enables the Django message in the developer sandbox home."),
-    ("show_testclient_link", True, ""),
-    ("signup", True, ""),
+    ("show_testclient_link", True, "This controls the display of the test client link from the main page."),
+    ("signup", True, "This enables signup related URLs and code paths. Not active in prod."),
     ("splunk_monitor", False, "This is used in other environments to ensure splunk forwarder is running."),
-    ("testclient_v2", True, ""),
-    ("wellknown_applications", True, ""),
+    ("testclient_v2", True, "This enables the v2 auth links in the test client"),
+    ("wellknown_applications", True, "This enables the /.well-known/applications end-point. Active in prod, but not in sbx/test."),
 )
 
 WAFFLE_FEATURE_FLAGS = (
