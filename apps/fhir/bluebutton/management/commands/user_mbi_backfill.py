c019d0ee19d30b61bcb006bc9786226055670a60
James Demery <jamesdemery@navapbc.com>
2025-09-19 14:08:44 -0400

    Log instead of print, add mbi extraction / db update

--- apps/fhir/bluebutton/management/commands/user_mbi_backfill.py
+++ apps/fhir/bluebutton/management/commands/user_mbi_backfill.py
@@ -1,19 +1,17 @@
-from datetime import datetime, timezone
 from django.core.management.base import BaseCommand
-from django.db import connection, transaction
 from apps.fhir.bluebutton.models import Crosswalk
 from apps.fhir.bluebutton.utils import get_patient_by_id
 import apps.logging.request_logger as bb2logging
 from django.test import RequestFactory
 
 import logging
-from typing import List
-import requests
+from typing import List, Optional, Dict, Any
 
 
 logger = logging.getLogger(bb2logging.HHS_SERVER_LOGNAME_FMT.format(__name__))
 
 FHIR_BASE_URL = "https://{ENV}.bluebutton.cms.gov/v2/fhir/"
+MBI_URL = "http://hl7.org/fhir/sid/us-mbi"
 
 class Command(BaseCommand):
     help = (
@@ -44,37 +42,54 @@ class Command(BaseCommand):
         # batch_size = options['batch_size']
         # dry_run = options['dry-run']
         # env = options['env']
-        batch_size = 1
-        dry_run= True
+        batch_size = 10
+        dry_run = False
         env = 'test'
         script_url = FHIR_BASE_URL.format(ENV=env)
+        logger.info("batch size %s" % (batch_size))
+        logger.info("env %s" % (env))
+        logger.info("script_url %s" % (script_url))
 
-        print("batch size: ", batch_size)
-        print("dry_run: ", dry_run)
-        print("env: ", env)
-        print("script_url: ", script_url)
         records = self.retrieve_records(batch_size)
-        self.process_records(records, script_url)
+        self.process_records(records, script_url, dry_run)
 
     def retrieve_records(self, batch_size: int) -> List[Crosswalk]:
         null_mbi_records = Crosswalk.objects.filter(_user_mbi__isnull=True)[:batch_size]
-        print("NUM RECS: ", len(null_mbi_records))
+        logger.info("# of records returned %s" % (len(null_mbi_records)))
         return null_mbi_records
     
-    def process_records(self, crosswalk_records: List[Crosswalk], script_url: str) -> None:
+    def process_records(self, crosswalk_records: List[Crosswalk], script_url: str, dry_run: bool) -> None:
         for crosswalk in crosswalk_records:
             try:
                 patient_info = 0
                 fhir_id = crosswalk.fhir_id
                 rf = RequestFactory()
                 request = rf.get("/")
-                print("fhir_id: ", fhir_id)
+                logger.info("fhir_id %s" % (fhir_id))
                 if not fhir_id:
                     print("can't update this record, no fhir_id")
                     continue
+                
                 patient_info = get_patient_by_id(fhir_id, request)
-                print("patient_info: ", patient_info)
+                logger.info("patient_info %s" % (patient_info))
+                user_mbi = self.extract_mbi(patient_info)
+
+                logger.info("user_mbi %s" % (user_mbi))
+                if user_mbi and not dry_run:
+                    self.update_mbi(user_mbi, crosswalk)
+                else:
+                    logger.info("MBI not found, can't update crosswalk record for fhir_id = %s" % (fhir_id))
 
             except Exception as e:
-                print("error: ", e)
+                logger.error("error in process: %s" % (e))
                 continue
+
+    def extract_mbi(self, patient_info: Dict[str, Any]) -> Optional[str]:
+        for identifier in patient_info.get("identifier", []):
+            if identifier.get("system") == MBI_URL:
+                return identifier.get("value")
+        return None
+    
+    def update_mbi(self, user_mbi: str, crosswalk: Crosswalk) -> None:
+        crosswalk._user_mbi = user_mbi
+        crosswalk.save()
