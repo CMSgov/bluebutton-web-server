80c3254abb158e7cfb5f4aa4ac945e1c022a5c28
James Demery <jamesdemery@navapbc.com>
2025-10-08 11:49:18 -0400

    BB2-1110: Restrict health check url (#1391)
    * BB2-1110: Ensure only /health and /health/ return the all's well message, others throw 404
    
    * Switch to single quotes and remove utf-8 deecode statement
    
    * Add back in previously removed health endpoints. Enforce that specific strings must be present in the URL after /health/ for a 200
    
    * Revert test name
    
    * Update integration test
    
    * Add in tests that mock failure for each external endpoint
    
    * Remove print

--- apps/health/views.py
+++ apps/health/views.py
@@ -10,7 +10,6 @@ from .checks import (
     slsx_services,
     bfd_services,
     db_services,
-    splunk_services,
 )
 
 import apps.logging.request_logger as bb2logging
@@ -69,7 +68,3 @@ class CheckBFD(Check):
 
 class CheckDB(Check):
     services = db_services
-
-
-class CheckSplunk(Check):
-    services = splunk_services
