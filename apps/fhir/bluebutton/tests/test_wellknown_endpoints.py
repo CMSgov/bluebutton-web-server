536480c432025e193ca6c1bbe728582632a3e137
James Demery <jamesdemery@navapbc.com>
2025-10-14 09:15:30 -0400

    Add test file (written by Matt), so we have coverage of some of these new endpoints

---
+++ apps/fhir/bluebutton/tests/test_wellknown_endpoints.py
@@ -0,0 +1,121 @@
+from unittest import skipIf
+from apps.test import TestCase
+from django.conf import settings
+import os
+import requests
+from collections import namedtuple as NT
+
+# Introduced in bb2-4184
+# Rudimentary tests to make sure endpoints exist and are returning
+# basic responses that make sense. Not a deep test.
+#
+# PRECONDITION
+# You must be on the VPN. Tested against TEST w.r.t. certs.
+
+print(os.getenv("RUN_ONLINE_TESTS"))
+
+
+Appropriate = NT("Status", "url,version,status_code")
+Status = NT("Status", "url,status_code")
+ContainsVersion = NT("ContainsVersion", "url,version,path")
+MissingFields = NT("MissingFields", "url,version,fields,status_code")
+
+BASEURL = "http://localhost:8000"
+TESTS = [
+    Appropriate("v1/connect/.well-known/openid-configuration", 'v1', 200),
+    Appropriate("v2/connect/.well-known/openid-configuration", 'v2', 200),
+    Appropriate("v3/connect/.well-known/openid-configuration", 'v3', 200),
+    MissingFields("v3/fhir/.well-known/smart-configuration", 'v3', ["fhir_metadata_uri", "userinfo_endpoint"], 200),
+    Status(".well-known/openid-configuration", 200),
+    Status(".well-known/openid-configuration-v2", 200)
+]
+
+
+class BlueButtonTestEndpoints(TestCase):
+
+    # FIXME This test does not pass locally for me. I get a 401.
+    @skipIf(settings.OFFLINE, "Can't reach external sites.")
+    def test_userinfo_returns_200(self):
+        response = requests.get(f'{BASEURL}/v3/connect/userinfo')
+        print(response.content)
+        print(response)
+
+    # This makes sure URLs return 200s.
+    @skipIf(settings.OFFLINE, "Can't reach external sites.")
+    def test_url_status_codes(self):
+        for test in TESTS:
+            if isinstance(test, Appropriate) or isinstance(test, Status) or isinstance(test, MissingFields):
+                response = requests.get(f"{BASEURL}/{test.url}")
+                self.assertEqual(response.status_code, test.status_code)
+
+    # This looks at the given set of URLs and makes sure that the version value encoded in the
+    # reponses are correctly versioned. Note the handling of v1/v2.
+    @skipIf(settings.OFFLINE, "Can't reach external sites.")
+    def test_urls_appropriate(self):
+        for test in TESTS:
+            if isinstance(test, Appropriate):
+                response = requests.get(f"{BASEURL}/{test.url}")
+                if response.status_code == test.status_code:
+                    the_json = response.json()
+                    # These should match the version
+                    for field in ['issuer']:
+                        self.assertIn(test.version, the_json[field])
+
+                    # These will be v2 for all v1 urls
+                    if test.version == 'v1':
+                        version = 'v2'
+                    else:
+                        version = test.version
+
+                    for field in ['authorization_endpoint',
+                                  'revocation_endpoint',
+                                  'token_endpoint',
+                                  'userinfo_endpoint',
+                                  'fhir_metadata_uri']:
+                        self.assertIn(version, the_json[field])
+                else:
+                    self.fail("Failed to connect with a good status code.")
+
+    # @skipIf(settings.OFFLINE, "Can't reach external sites.")
+    def test_smart_configuration_missing_fields_in_v3(self):
+        for test in TESTS:
+            if isinstance(test, MissingFields):
+                response = requests.get(f"{BASEURL}/{test.url}")
+                if response.status_code == 200:
+                    the_json = response.json()
+                    for field in test.fields:
+                        self.assertNotIn(field, the_json)
+                else:
+                    self.fail("Failed to connect with status 200")
+
+    # "extension": [
+    #     {
+    #         "url": "http://fhir-registry.smarthealthit.org/StructureDefinition/oauth-uris",
+    #         "extension": [
+    #             {
+    #                 "url": "token",
+    #                 "valueUri": "http://localhost:8000/v3/o/token"
+    #             },
+    #             {
+    #                 "url": "authorize",
+    #                 "valueUri": "http://localhost:8000/v3/o/authorize"
+    #             },
+    #             {
+    #                 "url": "revoke",
+    #                 "valueUri": "http://localhost:8000/v3/o/revoke_token"
+    #             }
+    #         ]
+    #     }
+    # ]
+    # Make sure FHIR v3 extensions are correct when the metadata is fetched; the extensions object
+    # is commented above for reference. This would be a good use of jsonpath in our codebase...
+    @skipIf(settings.OFFLINE, "Can't reach external sites.")
+    def test_fhir_metadata_extensions_have_v3(self):
+        response = requests.get(f"{BASEURL}/v3/fhir/metadata")
+        if response.status_code == 200:
+            json = response.json()
+            self.assertIn('v3', json["implementation"]["url"])
+            for obj in json["rest"]:
+                for ext in obj["security"]["extension"]:
+                    for e in ext["extension"]:
+                        self.assertIn('v3', e["valueUri"])
