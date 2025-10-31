bf87389fec1835b8938ed27643b895bb5b9a42c7
Matthew Jadud <mattjadud@navapbc.com>
2025-10-31 12:28:30 -0400

    Apply suggestion from @jimmyfagan
    Co-authored-by: jimmyfagan <90421499+jimmyfagan@users.noreply.github.com>

--- hhs_oauth_server/settings/dev.py
+++ hhs_oauth_server/settings/dev.py
@@ -3,7 +3,7 @@ from .base import *
 
 # removing security enforcement in development mode
 # 20251029 NOTE: Setting this variable to `False` will also disable/remove
-# CSS styling fromt he entire application when running locally.
+# CSS styling from the entire application when running locally.
 DEBUG = True
 SECRET_KEY = env('DJANGO_SECRET_KEY', '1234567890')
 
