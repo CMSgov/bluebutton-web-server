dff1203e32f198b4fad5a98b0efefd9c26835858
clewellyn-nava <connorlewellyn@navapbc.com>
2025-10-31 09:47:54 -0400

    unify static and sitestatic folders

--- hhs_oauth_server/settings/base.py
+++ hhs_oauth_server/settings/base.py
@@ -323,7 +323,6 @@ STATIC_URL = "/static/"
 STATIC_ROOT = "collectedstatic"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "sitestatic"),
     os.path.join(BASE_DIR, "bluebutton-css"),
     os.path.join(BASE_DIR, "static"),
 ]
