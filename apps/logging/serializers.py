a58570a8215407b08c9972f6bda1c98670f7afbd
James Demery <jamesdemery@navapbc.com>
2025-11-07 12:39:00 -0500

    Revert log to reduce test failures (will revisit this log once all functionality is working)

--- apps/logging/serializers.py
+++ apps/logging/serializers.py
@@ -167,8 +167,7 @@ class FHIRRequest(Request):
         return {
             'type': 'fhir_pre_fetch',
             'uuid': self.uuid(),
-            'bfd_bene_fhir_id': self.fhir_id(),
-            # 'fhir_id_v3': self.fhir_id(),
+            'fhir_id_v2': self.fhir_id(),
             'api_ver': self.api_ver if self.api_ver is not None else 'v1',
             'includeAddressFields': self.includeAddressFields(),
             'user': self.user(),
