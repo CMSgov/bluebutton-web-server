c4beb4bd9361e39b907926f64edb1d6c642fd183
clewellyn-nava <connorlewellyn@navapbc.com>
2025-11-10 15:24:01 -0500

    fix constraint on command

--- apps/fhir/bluebutton/management/commands/migrate_fhir_id_to_v2.py
+++ apps/fhir/bluebutton/management/commands/migrate_fhir_id_to_v2.py
@@ -61,15 +61,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # Prevent running if migration 0010 is applied or 0080 is not applied
-        try:
-            recorder = MigrationRecorder(connection=None)
-            if recorder.Migration.objects.filter(app='bluebutton', name__startswith='0010').exists() or not recorder.Migration.objects.filter(app='bluebutton', name__startswith='0008'):
-                raise RuntimeError(
-                    'Cannot run migrate_fhir_id_to_v2: bluebutton-0010 is applied OR bluebutton-0008 is not applied.'
-                )
-        except OperationalError:
-            # If the migration table doesn't exist yet, continue. This could happen if it's being applied during deployment server setup which is assumed safe.
-            pass
+        recorder = MigrationRecorder(connection=None)
+        if recorder.Migration.objects.filter(app='bluebutton', name__startswith='0010').exists() or not recorder.Migration.objects.filter(app='bluebutton', name__startswith='0008').exists():
+            raise RuntimeError(
+                'Cannot run migrate_fhir_id_to_v2: bluebutton-0010 is applied OR bluebutton-0008 is not applied.'
+            )
         
         if options.get('reverse'):
             print('Running reverse_migrate_v2_to_fhir_id...')
