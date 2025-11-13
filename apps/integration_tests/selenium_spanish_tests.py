60a2060da02b27ee3c1d06bec951c6624f3af8cf
jimmyfagan <jimmyfagan@navapbc.com>
2025-11-13 17:07:26 -0600

    Update expected version string

--- apps/integration_tests/selenium_spanish_tests.py
+++ apps/integration_tests/selenium_spanish_tests.py
@@ -1,8 +1,6 @@
 from .selenium_generic import SeleniumGenericTests
-from .selenium_cases import (
-    API_V2,
-    SPANISH_TESTS
-)
+from .selenium_cases import SPANISH_TESTS
+from apps.constants import Versions
 
 USE_NEW_PERM_SCREEN = "true"
 
@@ -20,7 +18,7 @@ class TestPermissionScreenSpanish(SeleniumGenericTests):
     def test_toggle_language_and_date_format(self):
         step = [0]
         test_name = "toggle_language"
-        api_ver = API_V2
+        api_ver = Versions.V2
         self._print_testcase_banner(test_name, api_ver, step[0], self.use_mslsx, True)
         if not self.use_mslsx:
             self._play(SPANISH_TESTS[test_name], step, api_ver=api_ver)
@@ -37,7 +35,7 @@ class TestPermissionScreenSpanish(SeleniumGenericTests):
     def test_authorize_lang_param(self):
         step = [0]
         test_name = "authorize_lang_param"
-        api_ver = API_V2
+        api_ver = Versions.V2
         self._print_testcase_banner(test_name, api_ver, step[0], self.use_mslsx, True)
         if not self.use_mslsx:
             self._play(SPANISH_TESTS[test_name], step, api_ver=api_ver)
@@ -54,7 +52,7 @@ class TestPermissionScreenSpanish(SeleniumGenericTests):
     def test_authorize_lang_spanish_button(self):
         step = [0]
         test_name = "authorize_lang_spanish_button"
-        api_ver = API_V2
+        api_ver = Versions.V2
         self._print_testcase_banner(test_name, api_ver, step[0], self.use_mslsx, True)
         if USE_NEW_PERM_SCREEN == "true":
             # the validation of expire date etc. only applicable to new perm screen
