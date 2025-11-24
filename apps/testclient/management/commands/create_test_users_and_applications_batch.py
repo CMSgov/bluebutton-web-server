25c215f079d5b9be63643fd5717da954add9da38
clewellyn-nava <connorlewellyn@navapbc.com>
2025-11-24 09:42:59 -0500

    attempted changes to create test users and applications

--- apps/testclient/management/commands/create_test_users_and_applications_batch.py
+++ apps/testclient/management/commands/create_test_users_and_applications_batch.py
@@ -5,7 +5,7 @@ import uuid
 
 import apps.logging.request_logger as logging
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from os import listdir
 from random import randint, randrange, choice, sample
 from django.contrib.auth.models import Group
@@ -15,6 +15,7 @@ from django.contrib.auth.models import User
 from django.conf import settings
 from django.utils import timezone
 from oauth2_provider.models import AccessToken, RefreshToken
+from tqdm import tqdm
 
 from apps.accounts.models import UserProfile
 from apps.fhir.bluebutton.models import Crosswalk
@@ -34,7 +35,9 @@ APPLICATION_SCOPES_FULL = ['patient/Patient.read', 'profile',
                            'capability-a', 'capability-b']
 APPLICATION_SCOPES_NON_DEMOGRAPHIC = ['patient/ExplanationOfBenefit.read',
                                       'patient/Coverage.read', 'capability-a', 'capability-b']
-DEFAULT_BENE_COUNT = 100
+
+# Keep up with prod count
+DEFAULT_BENE_COUNT = 675000
 DEFAULT_DEV_COUNT = 150
 DEFAULT_MAX_APPS_PER_DEV = 5
 
@@ -60,92 +63,114 @@ def get_first_available_number(firstname):
     return int(begin) + 1
 
 def create_dev_users_apps_and_bene_crosswalks(
-        group,
         bene_count,
         dev_count,
         app_max,
         refresh_count,
         archived_tokens,
-        archived_grants
+        archived_grants,
+        auto_generate
 ):
-    #
-    # generate dev users dev0001, dev0002, dev0003 etc, with password, email, security questions, etc.
-    # Counts are based on the inputs, but if undefined,will default to the defaults defined above
-    # each dev user can have 1 to many applications: dev0001-app01, dev0001-app02
-    # generate crosswalk bene users with FHIR-ID. HICN-HASH, MBI-HASH etc
-    #
+    """Creates bene and dev users, associated apps, and crosswalks for testing
 
-    # effectively turn off logging
+    Args:
+        bene_count (_type_): _description_
+        dev_count (_type_): _description_
+        app_max (_type_): _description_
+        refresh_count (_type_): _description_
+        archived_tokens (_type_): _description_
+        archived_grants (_type_): _description_
+        auto_generate (_type_): _description_
+    """
     mymedicare_cb_logger.setLevel(logging.CRITICAL)
     outreach_logger.setLevel(logging.CRITICAL)
 
+    group = create_group()
     bene_pk_list = []
-    files = [f for f in listdir('./synthetic-data') if re.match(r'synthetic-beneficiary-.*\.rif', f)]
+    
     count = 0
     file_cnt = 0
     synthetic_bene_cnt = 0
     begin_number = get_first_available_number('bene')
+    if auto_generate:
+        for i in tqdm(range(bene_count)):
+            id = str(i).zfill(10)
+            fn = f'FN{id}'
+            ln = f'LN{id}'
+            fhir_id = f'-{id}'
+            args = {
+                'username': str(uuid.uuid4()),
+                'user_hicn_hash': hash_hicn(id),
+                'user_mbi': f'MBI{id}',
+                'user_id_type': 'H',
+                'fhir_id_v2': fhir_id,
+                'first_name': fn,
+                'last_name': ln,
+                'email': f'{fn}.{ln}@xyz.com'
+            }
+            slsx_client = OAuth2ConfigSLSx(args)
+            user = create_beneficiary_record(slsx_client, fhir_id)
+            bene_pk_list.append(user.pk)
+    else:
+        files = [f for f in listdir('./synthetic-data') if re.match(r'synthetic-beneficiary-.*\.rif', f)]
+        for f in files:
+            print('file={}'.format(f))
+            bene_rif = open('./synthetic-data/{}'.format(f), 'r')
+            while True:
+                if count == bene_count:
+                    break
+                line = bene_rif.readline()
+                if not line:
+                    break
+                if line.startswith('DML_IND|BENE_ID'):
+                    continue
+                else:
+                    flds = line.split('|')
+                    fhir_id = flds[1]
+                    mbi = flds[14]
+                    hicn = flds[18]
+                    fn = f'bene{count + begin_number}'
+                    ln = f'user{count + begin_number}'
 
-    for f in files:
-        print('file={}'.format(f))
-        bene_rif = open('./synthetic-data/{}'.format(f), 'r')
-        while True:
-            if count == bene_count:
+                    # skip fred
+                    if fhir_id != '-20140000008325':
+                        args = {
+                            'username': str(uuid.uuid1()),
+                            'user_hicn_hash': hash_hicn(hicn),
+                            'user_mbi': mbi,
+                            'user_id_type': 'H',
+                            'fhir_id_v2': fhir_id,
+                            'first_name': fn,
+                            'last_name': ln,
+                            'email': fn + '.' + ln + '@xyz.net',
+                        }
+                        slsx_client = OAuth2ConfigSLSx(args)
+                        try:
+                            u = create_beneficiary_record(slsx_client, fhir_id)
+                            date_picked = datetime.now(timezone.utc) - timedelta(days=randrange(700))
+                            u.date_joined = date_picked.replace(tzinfo=pytz.utc)
+                            u.save()
+                            c = Crosswalk.objects.get(user=u)
+                            c.created = u.date_joined
+                            c.save()
+                            bene_pk_list.append(u.pk)
+                            synthetic_bene_cnt += 1
+                            count += 1
+                        except ValidationError:
+                            # If there is something wrong during 'create_beneficiary_record'
+                            # i.e. 'user already exists', just try the next .rif record
+                            continue
+            bene_rif.close()
+            file_cnt += 1
+            print('RIF file processed = {}, synthetic bene generated = {}'.format(file_cnt, synthetic_bene_cnt))
+            if file_cnt >= 1:
                 break
-            line = bene_rif.readline()
-            if not line:
-                break
-            if line.startswith('DML_IND|BENE_ID'):
-                continue
-            else:
-                flds = line.split('|')
-                fhir_id = flds[1]
-                mbi = flds[14]
-                hicn = flds[18]
-                fn = 'bene{}'.format(count + begin_number)
-                ln = 'user{}'.format(count + begin_number)
-
-                # skip fred
-                if fhir_id != '-20140000008325':
-                    args = {
-                        'username': str(uuid.uuid1()),
-                        'user_hicn_hash': hash_hicn(hicn),
-                        'user_mbi': mbi,
-                        'user_id_type': 'H',
-                        'fhir_id_v2': fhir_id,
-                        'first_name': fn,
-                        'last_name': ln,
-                        'email': fn + '.' + ln + '@xyz.net',
-                    }
-                    slsx_client = OAuth2ConfigSLSx(args)
-                    try:
-                        u = create_beneficiary_record(slsx_client, fhir_id)
-                        date_picked = datetime.utcnow() - timedelta(days=randrange(700))
-                        u.date_joined = date_picked.replace(tzinfo=pytz.utc)
-                        u.save()
-                        c = Crosswalk.objects.get(user=u)
-                        c.created = u.date_joined
-                        c.save()
-                        bene_pk_list.append(u.pk)
-                        synthetic_bene_cnt += 1
-                        count += 1
-                        print('.', end='', flush=True)
-                        time.sleep(.05)
-                    except ValidationError:
-                        # If there is something wrong during 'create_beneficiary_record'
-                        # i.e. 'user already exists', just try the next .rif record
-                        continue
-        bene_rif.close()
-        file_cnt += 1
-        print('RIF file processed = {}, synthetic bene generated = {}'.format(file_cnt, synthetic_bene_cnt))
-        if file_cnt >= 1:
-            break
 
     # create dev users according dev-count parameter, default to 100
     # generate access tokens + refresh tokens + archived tokens for random picked benes for each app
     app_index = 0
     begin_number = get_first_available_number('DevUserFN')
-    for i in range(dev_count):
+    for i in tqdm(range(dev_count)):
         dev_u_fn = 'DevUserFN{}'.format(i + begin_number)
         dev_u_ln = 'DevUserLN{}'.format(i + begin_number)
         u = User.objects.create_user(username='{}.{}'.format(dev_u_fn, dev_u_ln),
@@ -166,7 +191,6 @@ def create_dev_users_apps_and_bene_crosswalks(
         u.groups.add(group)
         # apps per DEV user
         app_cnt = randint(1, app_max) if app_max > 0 else 0
-        print('>>>>generating apps for user={}'.format(u.username))
         for i in range(app_cnt):
             app_index += 1
             app_name = 'app{}_{}'.format(i, u)
@@ -205,7 +229,6 @@ def create_dev_users_apps_and_bene_crosswalks(
             for t in titles:
                 c = ProtectedCapability.objects.get(title=t)
                 a.scope.add(c)
-        print('<<<<<generated apps for user={}'.format(u.username))
 
     # go through benes: each bene sign up to 1, 2, 3 apps
     # most 70% 1 app, 25% 2 apps, 5% 3 apps
@@ -274,14 +297,12 @@ def create_test_access_refresh_archived_objects(
                                     scope=scope)
     at.created = date_created.replace(tzinfo=pytz.utc)
     at.save()
-    print('<<< access token created for ' + user.username + '/' + application.name)
 
     for i in range(refresh_count):
         rt = RefreshToken.objects.create(user=user, application=application,
                                      token=uuid.uuid4().hex)
         rt.created = at.created
         rt.save()
-        print('<<< ' + user.username + ' refresh token ' + str(i) + ' generated')
 
     # archived token: created, updated, archived_at datetime fields
     for i in range(archived_token_count):
@@ -297,7 +318,6 @@ def create_test_access_refresh_archived_objects(
         date_archived = ot.created + timedelta(days=10)
         ot.archived_at = date_archived.replace(tzinfo=pytz.utc)
         ot.save()
-        print('<<< ' + user.username + ' archived token ' + str(i) + ' generated')
 
     past_date = timezone.now() - timedelta(days=2)
     for i in range(archived_grant_count):
@@ -308,8 +328,11 @@ def create_test_access_refresh_archived_objects(
                                       archived_at=past_date)
         past_date = past_date - timedelta(days=2)
         adag.save()
-        print('<<< ' + user.username + 'archived grant ' + str(i) + ' generated')
 
+def delete_users_apps_crosswalks():
+    User.objects.all().delete()
+    Application.objects.all().delete()
+    Crosswalk.objects.all().delete()
 
 class Command(BaseCommand):
     help = ('Create dev users and create'
@@ -333,6 +356,8 @@ class Command(BaseCommand):
         parser.add_argument('-g', '--archived-access-grants', default=0,
                             help='Archived access grants per user/app combination. '
                             'If none, defaults to 0.')
+        parser.add_argument('--auto-generate', default=False, help='Do not prompt for user input.')
+        parser.add_argument('--delete', default=False, help='Delete all test users and applications.')
 
     def handle(self, *args, **options):
         bene_count = int(options['bene_count'])
@@ -341,14 +366,18 @@ class Command(BaseCommand):
         refresh_tokens = int(options['refresh_tokens'])
         archived_tokens = int(options['archived_tokens'])
         archived_grants = int(options['archived_access_grants'])
-        g = create_group()
+        auto_generate = bool(options['auto_generate'])
+        delete = bool(options['delete'])
+        
+        if delete:
+            delete_users_apps_crosswalks()
         create_dev_users_apps_and_bene_crosswalks(
-            g,
             bene_count,
             dev_count,
             app_max,
             refresh_tokens,
             archived_tokens,
-            archived_grants)
+            archived_grants,
+            auto_generate)
         # update grants
         update_grants()
