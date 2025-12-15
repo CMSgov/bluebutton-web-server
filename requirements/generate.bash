e615e83d675a8caa5e1d494fa5eea291c4217b75
Matt Jadud <mattjadud@navapbc.com>
2025-12-15 15:26:02 -0500

    We need req.dev.txt packages, too.

--- requirements/generate.bash
+++ requirements/generate.bash
@@ -8,16 +8,23 @@ pip-compile --generate-hashes \
     --output-file=/output/requirements.txt \
     /requirements/requirements.in
 
-echo Starting download
+echo Starting requirements.txt download
 pip download -r /output/requirements.txt \
     --dest /vendor \
     --platform manylinux2014_x86_64 \
     --abi cp311 \
     --no-deps
-echo Done with download
+echo Done with requirements.txt download
 
 echo Generating requirements.dev.txt
 pip-compile --generate-hashes \
     --output-file=/output/requirements.dev.txt \
     /requirements/requirements.dev.in
 
+echo Starting requirements.dev.txt download
+pip download -r /output/requirements.dev.txt \
+    --dest /vendor \
+    --platform manylinux2014_x86_64 \
+    --abi cp311 \
+    --no-deps
+echo Done with requirements.dev.txt download
