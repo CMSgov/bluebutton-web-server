774b86c3359f06c7fba215f833627eeaffc58c5a
Matt Jadud <mattjadud@navapbc.com>
2025-12-15 15:00:17 -0500

    Re-adding vendored packages
    These are generated off the strongly-pinned `requirements.txt`.
    
    Using these will allow us to make a smoother transition to not having a `vendored` directory.

--- requirements/generate.bash
+++ requirements/generate.bash
@@ -3,5 +3,21 @@
 python --version
 echo $PYTHON_VERSION
 
-pip-compile --generate-hashes --output-file=/output/requirements.txt /requirements/requirements.in
-pip-compile --generate-hashes --output-file=/output/requirements.dev.txt /requirements/requirements.dev.in
+echo Generating requriements.txt
+pip-compile --generate-hashes \
+    --output-file=/output/requirements.txt \
+    /requirements/requirements.in
+
+echo Starting download
+pip download -r /output/requirements.txt \
+    --dest /vendor \
+    --platform manylinux2014_x86_64 \
+    --abi cp311 \
+    --no-deps
+echo Done with download
+
+echo Generating requirements.dev.txt
+pip-compile --generate-hashes \
+    --output-file=/output/requirements.dev.txt \
+    /requirements/requirements.dev.in
+
