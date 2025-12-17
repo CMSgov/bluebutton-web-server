f4c86ae45ff1452c37d354d4cb00dcae7e2a0fb3
clewellyn-nava <connorlewellyn@navapbc.com>
2025-12-17 09:40:28 -0500

    remove unnecessary env check

--- dev-local/utility-functions.bash
+++ dev-local/utility-functions.bash
@@ -25,21 +25,6 @@ check_valid_env () {
         return -2
     fi 
 
-
-    if [[ "${bfd}" == "local" && "${auth}" == "live" ]]; then
-        echo "⚠️ ${bfd}/${auth} may work for SLSX testing, but not for BFD calls."
-    fi 
-
-    if [[ "${bfd}" == "test" && "${auth}" == "mock" ]]; then
-        echo "⛔ ${bfd}/${auth} is not a valid combination. Exiting."
-        return -3
-    fi 
-
-    if [[ "${bfd}" == "sbx" && "${auth}" == "mock" ]]; then
-        echo "⛔ ${bfd}/${auth} is not a valid combination. Exiting."
-        return -4
-    fi 
-
     echo "✅ check_valid_env"
 }
 
