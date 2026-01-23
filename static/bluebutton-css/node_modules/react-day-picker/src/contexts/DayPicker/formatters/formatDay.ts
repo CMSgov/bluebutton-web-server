6a0717a10a578461770f7028de2edbbf62b86e9a
clewellyn-nava <connorlewellyn@navapbc.com>
2026-01-23 15:04:24 -0500

    remove node_modules

--- static/bluebutton-css/node_modules/react-day-picker/src/contexts/DayPicker/formatters/formatDay.ts
+++
@@ -1,9 +0,0 @@
-import type { Locale } from 'date-fns';
-import format from 'date-fns/format';
-
-/**
- * The default formatter for the Day button.
- */
-export function formatDay(day: Date, options?: { locale?: Locale }): string {
-  return format(day, 'd', options);
-}
