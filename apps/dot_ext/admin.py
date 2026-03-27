da35906be6ff2b9a70ead5001335cf430d691119
James Demery <jamesdemery@navapbc.com>
2026-03-27 16:03:17 -0400

    Ensure new application fields show in each admin view

--- apps/dot_ext/admin.py
+++ apps/dot_ext/admin.py
@@ -81,6 +81,8 @@ class CustomAdminApplicationForm(CustomRegisterApplicationForm):
             "active",
             "first_active",
             "last_active",
+            "allowed_auth_type",
+            "jwks_uri",
         )
 
     def clean_agree(self):
@@ -153,6 +155,8 @@ class MyApplicationAdmin(admin.ModelAdmin, ExportCsvMixin):
             "require_demographic_scopes",
             "data_access_type",
             "internal_application_labels",
+            "allowed_auth_type",
+            "jwks_uri",
         )
 
     raw_id_fields = ("user",)
