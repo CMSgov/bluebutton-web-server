3838bca0e3924f877e9f9e28bb7b12f7efbba011
Ryan Morosa <Ryan.Morosa@icf.com>
2026-03-30 12:01:18 -0600

    Try to mirror get_patient_by_mbi_hash more closely with get_patient_match_response_from_bfd

--- apps/fhir/bluebutton/utils.py
+++ apps/fhir/bluebutton/utils.py
@@ -835,10 +835,11 @@ def get_patient_match_response_from_bfd(url: str, json_payload: str, headers: Di
     req = requests.Request("POST", url, headers=headers, data=json_payload)
     prepped = req.prepare()
     response = s.send(prepped, cert=certs, verify=False)
+    response.raise_for_status()
 
-    return response
+    return response.json()
 
-def handle_patient_match_response(response: Dict[str, Any]) -> Dict[str, Any] | JsonResponse:
+def handle_patient_match_response(response_json: Dict[str, Any]) -> Dict[str, Any] | JsonResponse:
     """
     This is a utility function to handle the response from a patient match call to BFD. If a patient match is found, 
     a token will be returned to the requester to indicate that a patient match was found. If no patient match is found, 
@@ -851,19 +852,16 @@ def handle_patient_match_response(response: Dict[str, Any]) -> Dict[str, Any] | 
     Returns:
         dict: The response from BFD as a json/dict object
     """
-    if response.status_code == 200:
-        response_json = response.json()
-        response_json_entry = response_json.get('entry', [])
-        if response_json_entry and len(response_json_entry) > 1 and response_json_entry[1]['resource']['resourceType'] == "Patient":
-            # The length of the 'entry' list is greater than 1, which indicates a patient match was found and returned in the response
-            # Return a token to the requester to indicate that a patient match was found
-            logging.log.debug("Patient match found for patient_match call")
-        else:
-            # The length of the 'entry' list is 1, which indicates no patient match was found and no patient resource was returned in the response
-            # Throw an error to indicate that no patient match was found
-            logging.log.debug("No patient match found for patient_match call")
-            return JsonResponse({'status_code': 403, 'message': 'No patient match found.'}, status=403)
+    response_json_entry = response_json.get('entry', [])
+    # This can probably be modified in the future to check for other resourceTypes besides patient, 
+    # but for now this is sufficient to determine if a patient match was found or not, 
+    # since the only resource that should be returned in the 'entry' list for a patient match call is a patient resource
+    if response_json_entry and len(response_json_entry) > 1 and response_json_entry[1]['resource']['resourceType'] == "Patient":
+        # The length of the 'entry' list is greater than 1, which indicates a patient match was found and returned in the response
+        # Return a token to the requester to indicate that a patient match was found
+        logging.log.debug("Patient match found for patient_match call")
     else:
-        # Throw an error to indicate that there was an issue with the call to BFD to validate the client_credentials token call
-        logging.log.error(f"Error response from BFD when attempting to validate client_credentials token call: {response.status_code} - {response.text}")
-        return JsonResponse({'status_code': 502, 'message': 'Error validating client credentials.'}, status=502)
+        # The length of the 'entry' list is 1, which indicates no patient match was found and no patient resource was returned in the response
+        # Throw an error to indicate that no patient match was found
+        logging.log.debug("No patient match found for patient_match call")
+        return JsonResponse({'status_code': 403, 'message': 'No patient match found.'}, status=403)
