88caa4aac484479575904a55db2d87f5766a5d98
James Demery <jamesdemery@navapbc.com>
2026-04-10 08:19:40 -0400

    Allow launch/patient scope for client credentials calls

--- apps/dot_ext/views/authorization.py
+++ apps/dot_ext/views/authorization.py
@@ -64,7 +64,6 @@ from apps.constants import (
     CLIENT_CREDENTIALS,
     CLIENT_CREDENTIALS_ACCEPTED_JWT_ALGORITHMS,
     HHS_SERVER_LOGNAME_FMT,
-    LAUNCH_SCOPE,
     OPENID_SCOPE,
     USER_TYPE_ALIGNED_NETWORKS_BENEFICIARY,
 )
@@ -1009,13 +1008,13 @@ class TokenView(DotTokenView):
                 if allow_client_credentials_call:
                     # since we're not getting the user info from SLS, don't return openid scope in this flow
                     scopes = request.POST.get('scope', '').split()
-                    if OPENID_SCOPE in scopes or LAUNCH_SCOPE in scopes:
+                    if OPENID_SCOPE in scopes:
                         request.POST._mutable = True
                         request.POST['scope'] = ' '.join(
-                            s for s in scopes if s != OPENID_SCOPE and s != LAUNCH_SCOPE
+                            s for s in scopes if s != OPENID_SCOPE
                         )
                         request.POST._mutable = False
-
+                    print("what are the scopes now 22: ", request.POST)
                     # Allow client credentials call to proceed, to be implemented in a later ticket
                     log.info(
                         f'client_credentials token call was made for app: {app.name}'
@@ -1090,6 +1089,7 @@ class TokenView(DotTokenView):
                             )
                     except Exception as e:
                         log.error(f'Error validating jwt: {type(e)}')
+                        log.error(f'Error was : {str(e)}')
                         return JsonResponse(
                             {
                                 'status_code': HTTPStatus.BAD_REQUEST,
