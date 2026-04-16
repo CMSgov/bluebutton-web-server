bff8786d804e26a422366de1951ff2c2e56839a8
Ryan Morosa <Ryan.Morosa@icf.com>
2026-04-16 13:29:59 -0600

    Edit static root

--- hhs_oauth_server/settings/base_ec2.py
+++ hhs_oauth_server/settings/base_ec2.py
@@ -386,7 +386,7 @@ MEDIA_ROOT = os.path.join(ASSETS_ROOT, 'media')
 
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
-STATIC_ROOT = 'collectedstatic'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collectedstatic')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
