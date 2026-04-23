97c71954f245ffb81599627fc0fb20b572a6d260
noor1027 <noorulla@gmail.com>
2026-04-23 12:48:28 -0500

    Fix static site CodeBuild IAM: add deploy log permissions and wildcard secret access

--- ops/services/00-bootstrap/codebuild_static_site_iam.tf
+++ ops/services/00-bootstrap/codebuild_static_site_iam.tf
@@ -35,7 +35,7 @@ data "aws_iam_policy_document" "static_site_logs" {
       "logs:PutLogEvents",
     ]
     resources = [
-      "arn:aws:logs:${local.region}:${local.account_id}:log-group:/bb2/site-static/*",
+      "arn:aws:logs:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:log-group:/bb2/site-static/*",
     ]
   }
 }
