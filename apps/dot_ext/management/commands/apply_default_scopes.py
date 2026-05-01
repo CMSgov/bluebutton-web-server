b1772fb429f79f5d76e85e85d8bac766780eaf89
Anna Montare <267455234+annamontare-nava@users.noreply.github.com>
2026-05-01 11:52:41 -0600

    update command to handle require_demographic_scopes

--- apps/dot_ext/management/commands/apply_default_scopes.py
+++ apps/dot_ext/management/commands/apply_default_scopes.py
@@ -1,26 +1,39 @@
 from django.core.management.base import BaseCommand
 
 from apps.capabilities.models import ProtectedCapability
+from apps.dot_ext.constants import BENE_PERSONAL_INFO_SCOPES
 from apps.dot_ext.models import Application
 
 
 class Command(BaseCommand):
     help = 'Ensure all apps have default scopes.'
 
-    # TODO tests
-    def handle(self, *args, **options):
-        default_scopes = ProtectedCapability.objects.filter(default__exact=True)
-
-        self.stdout.write('Default scopes:')
-        for scope in default_scopes:
-            self.stdout.write(f' - {scope}')
-
+    def _display_scopes(self, scopes, label):
+        self.stdout.write(label)
+        for scope in scopes:
+            self.stdout.write(f' - {scope.slug}: {scope}')
         self.stdout.write()
+
+    def handle(self, *args, **options):
+        # TODO is there a way to write this function more readably?
+        default_scopes = ProtectedCapability.objects.filter(default__exact=True)
+        # TODO what about demographic scopes that are not default?
+        demographic_scopes = ProtectedCapability.objects.filter(slug__in=BENE_PERSONAL_INFO_SCOPES)
+        default_non_demographic = default_scopes.difference(demographic_scopes)
+
+        self._display_scopes(default_scopes, 'Default scopes:')
+        self._display_scopes(demographic_scopes, 'Demographic scopes:')
+
         self.stdout.write('Applying to all apps.')
 
         for app in Application.objects.all():
             # TODO is there a way to only update those that need updating?
-            app.scope.add(*default_scopes)
+            if app.require_demographic_scopes is False:
+                app.scope.add(*default_non_demographic)
+                app.scope.remove(*demographic_scopes)
+            else: # True or None
+                app.scope.add(*default_scopes)
+            # TODO is the save call necessary?
             app.save()
 
         self.stdout.write('Done.')
