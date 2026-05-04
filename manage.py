ab41601983045a07689e34d1272c45b5b85b51be
clewellyn-nava <connorlewellyn@navapbc.com>
2026-05-04 09:19:19 -0400

    address github comments

--- manage.py
+++ manage.py
@@ -7,6 +7,9 @@ from dotenv import load_dotenv
 if __name__ == '__main__':
     load_dotenv()
 
+    # this conditional loads the test setting if you are running the test runner
+    # this is so that it doesn't load the watchtower logging and make useless log groups
+    # and spam them with logs. This should be worked on in the future.
     TESTING = 'test' in sys.argv
     if TESTING:
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hhs_oauth_server.settings.test')
