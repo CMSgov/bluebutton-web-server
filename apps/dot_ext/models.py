256fb85a9e9763ae8a34de8966a3e099d3c31135
annamontare-nava <267455234+annamontare-nava@users.noreply.github.com>
2026-05-07 07:01:24 -0600

    remove comment with historical information
    Co-authored-by: jimmyfagan <90421499+jimmyfagan@users.noreply.github.com>

--- apps/dot_ext/models.py
+++ apps/dot_ext/models.py
@@ -164,8 +164,6 @@ class Application(AbstractApplication):
     last_active = models.DateTimeField(blank=True, null=True)
 
     # Does this application need to collect beneficiary demographic information? YES = True NO = False/Null
-    # There was previously a comment here that said null values should be treated as
-    # "yes". In BB2-4654, we changed this to treat null values as "no".
     require_demographic_scopes = models.BooleanField(
         default=True, null=True, verbose_name='Are demographic scopes required?'
     )
