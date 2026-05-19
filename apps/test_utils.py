a954e4da37f48e7865ecfcd02faba8039f400c68
Ryan Morosa <Ryan.Morosa@icf.com>
2026-05-19 11:32:09 -0600

    Fix unit test class and function names

--- apps/test_utils.py
+++ apps/test_utils.py
@@ -6,13 +6,13 @@ from django.test import TestCase
 from apps.utils import has_matching_protected_resource
 
 
-class IsValidScopeTest(TestCase):
+class HasMatchingProtectedResourceTest(TestCase):
     def setUp(self):
         eob_protected_resources = json.dumps(['GET', '/v[123]/fhir/ExplanationOfBenefit[/?].*$'])
         coverage_protected_resources = json.dumps(['GET', '/v[123]/fhir/Coverage[/]?$'])
         self.protected_resources = [eob_protected_resources, coverage_protected_resources]
 
-    def test_is_valid_scope_returns_false(self) -> bool:
+    def test_has_matching_protected_resource_returns_false(self) -> bool:
         """
         Returns False since the app has EOB and Coverage scopes but is trying to make a Patient call.
         """
@@ -21,7 +21,7 @@ class IsValidScopeTest(TestCase):
 
         assert has_match is False
 
-    def test_is_valid_scope_returns_true(self) -> bool:
+    def test_has_matching_protected_resource_returns_true(self) -> bool:
         """
         Returns True since the app has EOB and Coverage scopes and is trying to make a Coverage call.
         """
