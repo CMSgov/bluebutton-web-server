a2df0eed84db0fa3d36819ec63c14d3265bd2f08
James Demery <jamesdemery@navapbc.com>
2026-06-18 14:20:02 -0400

    BB2-4649: Update v3 Permissions Screen Styling (#1634)
    * Initial work on v3 permissions screen
    
    * Add accordions and more styling
    
    * Add part D conditional
    
    * Minor styling and typo fixes - include_samhsa true if part d eob only is true
    
    * Remove print
    
    * Clean up some styles and add switch account link
    
    * Ensure part d claims screen matches figma
    
    * Address PR feedback

--- apps/dot_ext/v3/urls.py
+++ apps/dot_ext/v3/urls.py
@@ -1,61 +1,60 @@
 from django.urls import path, re_path
-from apps.dot_ext.constants import AUTHORIZE_INSTANCE_PARAM, TOKEN_ENDPOINT_V3_KEY
 from oauth2_provider import views as oauth2_views
 from waffle.decorators import waffle_switch
 
 from apps.dot_ext import views
+from apps.dot_ext.constants import AUTHORIZE_INSTANCE_PARAM, TOKEN_ENDPOINT_V3_KEY
 
-
-app_name = "oauth2_provider_v3"
+app_name = 'oauth2_provider_v3'
 
 
 base_urlpatterns = [
-    path("authorize/", waffle_switch("v3_endpoints")(views.AuthorizationView.as_view(version=3)), name="authorize-v3"),
+    path('authorize/', waffle_switch('v3_endpoints')(views.AuthorizationView.as_view(version=3)), name='authorize-v3'),
     re_path(
-        rf"^authorize/(?P<{AUTHORIZE_INSTANCE_PARAM}>[\w-]+)/$",
-        waffle_switch("v3_endpoints")(views.ApprovalView.as_view(version=3)),
-        name="authorize-instance-v3",
+        rf'^authorize/(?P<{AUTHORIZE_INSTANCE_PARAM}>[\w-]+)/$',
+        waffle_switch('v3_endpoints')(views.ApprovalView.as_view(version=3)),
+        name='authorize-instance-v3',
     ),
-    re_path(r"^token/?$", waffle_switch("v3_endpoints")(views.TokenView.as_view()), name=TOKEN_ENDPOINT_V3_KEY),
-    path("revoke_token/", waffle_switch("v3_endpoints")(views.RevokeTokenView.as_view()), name="revoke-token-v3"),
-    path("revoke/", waffle_switch("v3_endpoints")(views.RevokeView.as_view()), name="revoke-v3"),
-    path("introspect/", waffle_switch("v3_endpoints")(views.IntrospectTokenView.as_view()), name="introspect-v3"),
+    re_path(r'^token/?$', waffle_switch('v3_endpoints')(views.TokenView.as_view()), name=TOKEN_ENDPOINT_V3_KEY),
+    path('revoke_token/', waffle_switch('v3_endpoints')(views.RevokeTokenView.as_view()), name='revoke-token-v3'),
+    path('revoke/', waffle_switch('v3_endpoints')(views.RevokeView.as_view()), name='revoke-v3'),
+    path('introspect/', waffle_switch('v3_endpoints')(views.IntrospectTokenView.as_view()), name='introspect-v3'),
 ]
 
 
 management_urlpatterns = [
     # Application management views
-    path("applications/", waffle_switch("v3_endpoints")(oauth2_views.ApplicationList.as_view()), name="list-v3"),
+    path('applications/', waffle_switch('v3_endpoints')(oauth2_views.ApplicationList.as_view()), name='list-v3'),
     path(
-        "applications/register/",
-        waffle_switch("v3_endpoints")(views.ApplicationRegistration.as_view()),
-        name="register-v3",
+        'applications/register/',
+        waffle_switch('v3_endpoints')(views.ApplicationRegistration.as_view()),
+        name='register-v3',
     ),
     re_path(
-        r"^applications/(?P<pk>[\w-]+)/$",
-        waffle_switch("v3_endpoints")(oauth2_views.ApplicationDetail.as_view()),
-        name="detail-v3",
+        r'^applications/(?P<pk>[\w-]+)/$',
+        waffle_switch('v3_endpoints')(oauth2_views.ApplicationDetail.as_view()),
+        name='detail-v3',
     ),
     re_path(
-        r"^applications/(?P<pk>[\w-]+)/delete/$",
-        waffle_switch("v3_endpoints")(views.ApplicationDelete.as_view()),
-        name="delete-v3",
+        r'^applications/(?P<pk>[\w-]+)/delete/$',
+        waffle_switch('v3_endpoints')(views.ApplicationDelete.as_view()),
+        name='delete-v3',
     ),
     re_path(
-        r"^applications/(?P<pk>[\w-]+)/update/$",
-        waffle_switch("v3_endpoints")(views.ApplicationUpdate.as_view()),
-        name="update-v3",
+        r'^applications/(?P<pk>[\w-]+)/update/$',
+        waffle_switch('v3_endpoints')(views.ApplicationUpdate.as_view()),
+        name='update-v3',
     ),
     # Token management views
     path(
-        "authorized_tokens/",
-        waffle_switch("v3_endpoints")(oauth2_views.AuthorizedTokensListView.as_view()),
-        name="authorized-token-list-v3",
+        'authorized_tokens/',
+        waffle_switch('v3_endpoints')(oauth2_views.AuthorizedTokensListView.as_view()),
+        name='authorized-token-list-v3',
     ),
     re_path(
-        r"^authorized_tokens/(?P<pk>[\w-]+)/delete/$",
-        waffle_switch("v3_endpoints")(oauth2_views.AuthorizedTokenDeleteView.as_view()),
-        name="authorized-token-delete-v3",
+        r'^authorized_tokens/(?P<pk>[\w-]+)/delete/$',
+        waffle_switch('v3_endpoints')(oauth2_views.AuthorizedTokenDeleteView.as_view()),
+        name='authorized-token-delete-v3',
     ),
 ]
 
