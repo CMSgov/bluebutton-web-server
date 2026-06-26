5d1884ba53f7f9a862100687f0670a1fd4d732c5
Anna Montare <267455234+annamontare-nava@users.noreply.github.com>
2026-06-26 14:55:59 -0600

    remove newrelic agent from the code

--- hhs_oauth_server/wsgi.py
+++ hhs_oauth_server/wsgi.py
@@ -1,5 +1,4 @@
 import os
-import newrelic.agent
 
 # import dotenv
 from dotenv import load_dotenv
@@ -8,10 +7,6 @@ from django.core.wsgi import get_wsgi_application
 # project root folder
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DJANGO_CUSTOM_SETTINGS_DIR = os.path.join(BASE_DIR, '..')
-
-# If the New Relic config file is present, load and configure the agent
-if os.path.isfile(os.path.join(DJANGO_CUSTOM_SETTINGS_DIR, 'newrelic.ini')):
-    newrelic.agent.initialize(os.path.join(DJANGO_CUSTOM_SETTINGS_DIR, 'newrelic.ini'))
 
 # If the .env file is present, load it
 if os.path.isfile(os.path.join(DJANGO_CUSTOM_SETTINGS_DIR, '.env')):
