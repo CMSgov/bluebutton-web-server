222c89956421d68beb1a15ae18e94a1ac456ff93
Ryan Morosa <Ryan.Morosa@icf.com>
2026-07-21 15:59:10 -0700

    Additional changes to make things into variables

--- apps/fhir/bluebutton/tests/test_CAN_integration.py
+++ apps/fhir/bluebutton/tests/test_CAN_integration.py
@@ -24,20 +24,21 @@ BB2_TOKEN_URL = os.getenv('BB2_TOKEN_URL', 'http://localhost:8000/v3/o/token')
 TESTCLIENT_CC_KID = f'bb2-{os.getenv("TARGET_ENV", "local")}-cc-1'
 DAMON_MYCHART_PHONE_NUMBER = '6082113314'
 OTP_CODE = '123456'
+CLEAR_REDIRECT_URI = 'http://localhost:3001/api/clear/callback'
 
 # Start with building the url
-# https://verified.clearme.com/integrations/oauth2/auth?response_type=code&client_id=bluebutton-sample&state=teststate&redirect_uri=http://localhost:3001/api/clear/callback&scope=offline%20openid%20offline_access&code_challenge=<insert_generated_code_challenge_here>&code_challenge_method=S256
+# https://verified.clearme.com/integrations/oauth2/auth?response_type=code&client_id={client_id}&state=teststate&redirect_uri={CLEAR_REDIRECT_URI}&scope=offline%20openid%20offline_access&code_challenge={code-challenge}&code_challenge_method=S256
 # Get the generation of the code challenge and the code verifier
 # TODO: Mimic login flow somehow for synthetic user to receive a code value
 # POST to the clear token endpoint
 # curl --location 'https://verified.clearme.com/integrations/oauth2/token' \
 # --header 'Content-Type: application/x-www-form-urlencoded' \
 # --header 'Cookie: ' \
-# --data-urlencode 'client_id=bluebutton-sample' \
+# --data-urlencode 'client_id={client_id}' \
 # --data-urlencode 'client_secret=<secret-from-aws>' \
 # --data-urlencode 'scope=offline openid offline_access' \
 # --data-urlencode 'grant_type=authorization_code' \
-# --data-urlencode 'redirect_uri=http://localhost:3001/api/clear/callback' \
+# --data-urlencode 'redirect_uri={CLEAR_REDIRECT_URI}' \
 # --data-urlencode 'code=<code-from-url>' \
 # --data-urlencode 'code_verifier=<generated-code-verifier>'
 # Get the client secret from AWS SSM as a environment variable
@@ -90,7 +91,7 @@ def generate_pkce_data() -> tuple:
     return code_verifier, code_challenge
 
 
-def get_clear_authorization_code(code_challenge: str) -> str:
+def get_clear_authorization_code(client_id: str, code_challenge: str) -> str:
     """
     Retrieves the authorization code from the Clear integration.
 
@@ -99,8 +100,8 @@ def get_clear_authorization_code(code_challenge: str) -> str:
     """
     driver = webdriver.Chrome()
 
-    # Start with building the url
-    clear_login_url = f'https://verified.clearme.com/integrations/oauth2/auth?response_type=code&client_id=bluebutton-sample&state=teststate&redirect_uri=http://localhost:3001/api/clear/callback&scope=offline%20openid%20offline_access&code_challenge={code_challenge}&code_challenge_method=S256'
+    # See here for building the url: https://docs.clearme.com/docs/individual-access-token
+    clear_login_url = f'https://verified.clearme.com/integrations/oauth2/auth?response_type=code&client_id={client_id}&state=teststate&redirect_uri={CLEAR_REDIRECT_URI}&scope=offline%20openid%20offline_access&code_challenge={code_challenge}&code_challenge_method=S256'
 
     # Simulate the user login and authorization flow
     driver.get(clear_login_url)
@@ -141,11 +142,12 @@ def get_clear_authorization_code(code_challenge: str) -> str:
     return authorization_code
 
 
-def get_clear_id_token(client_secret: str, code: str, code_verifier: str) -> str:
+def get_clear_id_token(client_id: str, client_secret: str, code: str, code_verifier: str) -> str:
     """
     Exchanges the authorization code for an ID token from Clear.
 
     Args:
+        client_id (str): The client ID for the Clear integration.
         client_secret (str): The client secret for the Clear integration.
         code (str): The authorization code received from Clear.
         code_verifier (str): The code verifier used in the PKCE flow.
@@ -153,14 +155,15 @@ def get_clear_id_token(client_secret: str, code: str, code_verifier: str) -> str
     Returns:
         str: The ID token received from Clear.
     """
+    # See here for building the post request: https://docs.clearme.com/docs/individual-access-token
     url = 'https://verified.clearme.com/integrations/oauth2/token'
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     data = {
-        'client_id': 'bluebutton-sample',
+        'client_id': client_id,
         'client_secret': client_secret,
         'scope': 'offline openid offline_access',
         'grant_type': 'authorization_code',
-        'redirect_uri': 'http://localhost:3001/api/clear/callback',
+        'redirect_uri': CLEAR_REDIRECT_URI,
         'code': code,
         'code_verifier': code_verifier,
     }
@@ -220,11 +223,12 @@ def get_access_token_response(client_assertion: str, client_assertion_type: str,
 def test_clear_integration_flow():
     code_verifier, code_challenge = generate_pkce_data()
     # Use selenium to simulate the user login and authorization flow to get the authorization code
-    auth_code = get_clear_authorization_code(code_challenge)
+    client_id = os.getenv('CLEAR_CLIENT_ID', 'your-client-id-here')
+    auth_code = get_clear_authorization_code(client_id, code_challenge)
 
     # TODO: Replace with actual env var name
     client_secret = os.getenv('CLEAR_CLIENT_SECRET', 'your_client_secret_here')
-    id_token = get_clear_id_token(client_secret, auth_code, code_verifier)
+    id_token = get_clear_id_token(client_id, client_secret, auth_code, code_verifier)
 
     ial_payload = construct_ial_payload(id_token)
 
