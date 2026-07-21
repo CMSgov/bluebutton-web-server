38d40147d9087c7ed90b4828c04b6d2d9b2dd840
Ryan Morosa <Ryan.Morosa@icf.com>
2026-07-21 11:53:30 -0700

    Get auth code with selenium

--- apps/fhir/bluebutton/tests/test_CAN_integration.py
+++ apps/fhir/bluebutton/tests/test_CAN_integration.py
@@ -3,20 +3,21 @@ import hashlib
 import os
 import secrets
 import time
+import urllib
 import uuid
 
 import jwt
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 from apps.constants import CLIENT_CREDENTIALS, TEST_APP_CLIENT_ID
 from apps.dot_ext.constants import CLIENT_ASSERTION_TYPE_VALUE
 
 # Start with building the url
 # https://verified.clearme.com/integrations/oauth2/auth?response_type=code&client_id=bluebutton-sample&state=teststate&redirect_uri=http://localhost:3001/api/clear/callback&scope=offline%20openid%20offline_access&code_challenge=<insert_generated_code_challenge_here>&code_challenge_method=S256
-
 # Get the generation of the code challenge and the code verifier
 # TODO: Mimic login flow somehow for synthetic user to receive a code value
-
 # POST to the clear token endpoint
 # curl --location 'https://verified.clearme.com/integrations/oauth2/token' \
 # --header 'Content-Type: application/x-www-form-urlencoded' \
@@ -28,26 +29,18 @@ from apps.dot_ext.constants import CLIENT_ASSERTION_TYPE_VALUE
 # --data-urlencode 'redirect_uri=http://localhost:3001/api/clear/callback' \
 # --data-urlencode 'code=<code-from-url>' \
 # --data-urlencode 'code_verifier=<generated-code-verifier>'
-
-
 # Get the client secret from AWS SSM as a environment variable
-
 # TODO: Construct the payload for algorithm and token type
 # {
 #   "alg": "RS384",
 #   "kid": "my-key-id-1",
 #   "typ": "JWT"
 # }
-
 # TODO: Get a private key to encode the jwt, possibly store this within SOPS/SSM and read it in as an environment variable
 # For now, let's just pretend we have the variables read in already
-
 # The output of after we send in the a POST request to BBAPI's token endpoint as the client_assertion
-
 # Use the client_assertion and other params to hit the token endpoint and get back access_token and refresh_token
-
 # Use the generate pkce data function from apps/testclient/utils.py to generate the code challenge and code verifier
-
 # TODO: Construct this (scaffolding below)
 # {
 #   "iss": "<paste_client_id_here>",
@@ -63,10 +56,76 @@ from apps.dot_ext.constants import CLIENT_ASSERTION_TYPE_VALUE
 #     }
 #   }
 # }
-
 # Ensure the iss and sub are the client id
 # jti is a randomly generated uuid
 # exp must be within 5 minutes of the current timestamp from unix epoch time
+
+
+def generate_pkce_data() -> tuple:
+    """
+    Generates a code verifier and code challenge for the PKCE flow.
+
+    Returns:
+        tuple: A tuple containing the code verifier and code challenge.
+    """
+    # Generate a random code verifier
+    code_verifier = secrets.token_urlsafe(64)
+
+    # Generate the code challenge using SHA256 and base64url encoding
+    code_challenge = base64.urlsafe_b64encode(hashlib.sha256(code_verifier.encode('utf-8')).digest()).decode('utf-8')
+
+    return code_verifier, code_challenge
+
+
+def get_clear_authorization_code(code_challenge: str) -> str:
+    """
+    Retrieves the authorization code from the Clear integration.
+
+    Returns:
+        str: The authorization code.
+    """
+    driver = webdriver.Chrome()
+
+    # Start with building the url
+    clear_login_url = f'https://verified.clearme.com/integrations/oauth2/auth?response_type=code&client_id=bluebutton-sample&state=teststate&redirect_uri=http://localhost:3001/api/clear/callback&scope=offline%20openid%20offline_access&code_challenge={code_challenge}&code_challenge_method=S256'
+
+    # Simulate the user login and authorization flow
+    driver.get(clear_login_url)
+
+    driver.implicitly_wait(10)  # Wait for the page to load
+
+    phone_input = driver.find_element(By.XPATH, '//input[@placeholder="Phone"]')
+    phone_input.send_keys('6082113314')  # Replace with Daymoen Mychart's phone number
+
+    time.sleep(1)  # Wait for a few seconds before clicking the continue button
+
+    continue_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Continue')]")
+    continue_button.click()
+
+    time.sleep(1)  # Wait for a few seconds after clicking the continue button
+
+    agree_and_continue_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Agree & Continue')]")
+    agree_and_continue_button.click()
+
+    time.sleep(1)
+
+    code_input = driver.find_element(By.XPATH, '//input[@placeholder="Enter your Code"]')
+    code_input.send_keys('123456')
+
+    time.sleep(1)
+
+    skip_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Skip')]")
+    skip_button.click()
+
+    time.sleep(10)
+
+    url = driver.current_url
+
+    parsed_url = urllib.parse.urlparse(url)
+    query_params = urllib.parse.parse_qs(parsed_url.query)
+
+    authorization_code = query_params.get('code', [None])[0]
+    return authorization_code
 
 
 def get_clear_id_token(client_secret: str, code: str, code_verifier: str) -> str:
@@ -145,37 +204,18 @@ def get_access_token_response(client_assertion: str, client_assertion_type: str,
     return response.json()
 
 
-def generate_pkce_data() -> tuple:
-    """
-    Generates a code verifier and code challenge for the PKCE flow.
-
-    Returns:
-        tuple: A tuple containing the code verifier and code challenge.
-    """
-    # Generate a random code verifier
-    code_verifier = secrets.token_urlsafe(64)
-
-    # Generate the code challenge using SHA256 and base64url encoding
-    code_challenge = base64.urlsafe_b64encode(hashlib.sha256(code_verifier.encode('utf-8')).digest()).decode('utf-8')
-
-    return code_verifier, code_challenge
-
-
 # What it would look like all together in a test function
 def test_clear_integration_flow():
     """
     Test the Clear integration flow by simulating the steps to obtain an access token from the Blue Button API.
     """
     code_verifier, code_challenge = generate_pkce_data()
-    # Start with building the url
-    # clear_login_url = f'https://verified.clearme.com/integrations/oauth2/auth?response_type=code&client_id=bluebutton-sample&state=teststate&redirect_uri=http://localhost:3001/api/clear/callback&scope=offline%20openid%20offline_access&code_challenge={code_challenge}&code_challenge_method=S256'
-    # TODO: Somehow need to get the code from the Clear login url, possibly by mimicking a user login or using a test account
-    # Code challenge will need to be used here in the URL to get the code from Clear, and then the code will be used to get the id_token from Clear
-    authorization_code = 'replace_me_with_actual_authorization_code'
+    # Use selenium to simulate the user login and authorization flow to get the authorization code
+    auth_code = get_clear_authorization_code(code_challenge)
 
     # TODO: Replace with actual env var name
     client_secret = os.getenv('CLEAR_CLIENT_SECRET', 'your_client_secret_here')
-    id_token = get_clear_id_token(client_secret, authorization_code, code_verifier)
+    id_token = get_clear_id_token(client_secret, auth_code, code_verifier)
 
     ial_payload = construct_ial_payload(id_token)
 
