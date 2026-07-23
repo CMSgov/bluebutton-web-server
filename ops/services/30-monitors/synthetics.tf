3d3310d4f5fd2ec76e30b1a164f71e3372892784
Anna Montare <267455234+annamontare-nava@users.noreply.github.com>
2026-07-23 14:20:33 -0600

    add synthetic tests for all the health endpoints

--- ops/services/30-monitors/synthetics.tf
+++ ops/services/30-monitors/synthetics.tf
@@ -9,7 +9,7 @@ data "aws_ssm_parameter" "akamai_aca_token" {
 
 locals {
   hostname_url            = nonsensitive(data.aws_ssm_parameter.hostname_url.value)
-  hostname_url_normalized = "%{if !startswith(local.hostname_url, "https://")}https://%{endif}${local.hostname_url}"
+  hostname_url_normalized = "%{if !startswith(local.hostname_url, "https://")}https://%{endif}${trimsuffix(local.hostname_url, "/")}"
 }
 
 module "synthetics" {
@@ -42,6 +42,7 @@ resource "datadog_synthetics_test" "homepage_uptime" {
 
   tags = module.synthetics.base_tags
 
+  # TODO should we use a global variable instead?
   dynamic "config_variable" {
     for_each = local.env == "test" ? [1] : []
     content {
@@ -49,7 +50,6 @@ resource "datadog_synthetics_test" "homepage_uptime" {
       type    = "text"
       secure  = true
       pattern = data.aws_ssm_parameter.akamai_aca_token.value
-
     }
   }
 
@@ -73,5 +73,72 @@ resource "datadog_synthetics_test" "homepage_uptime" {
     operator = "is"
     target   = "text/html; charset=utf-8"
     property = "content-type"
+  }
+}
+
+locals {
+  health_endpoints = [for p in [
+    "",
+    "/external",
+    "/external_v2",
+    "/sls",
+    "/db",
+    "/bfd",
+    "/bfd_v2",
+  ] : "/health${p}"]
+}
+
+resource "datadog_synthetics_test" "health" {
+  for_each = toset(local.health_endpoints)
+
+  name    = "${local.app}-${local.env}-${each.key}"
+  type    = "api"
+  subtype = "http"
+  status  = "live"
+  message = "Synthetics test ${local.app}-${local.env}-${each.key} has failed. ${module.common_datadog_monitors.notify}"
+
+  locations = module.synthetics.non_private_location_ids
+
+  options_list {
+    tick_every           = 60
+    monitor_name         = "[${upper(local.env)}] [${local.app}] Synthetics — ${each.key}"
+    min_failure_duration = local.monitor_config.synthetics.min_failure_duration
+  }
+
+  tags = module.synthetics.base_tags
+
+  config_variable {
+    name    = "AKAMAI_COOKIE"
+    type    = "text"
+    secure  = true
+    pattern = data.aws_ssm_parameter.akamai_aca_token.value
+  }
+
+  request_headers = {
+    cookie = "{{ AKAMAI_COOKIE }}"
+  }
+
+  request_definition {
+    method = "GET"
+    url    = "${local.hostname_url_normalized}${each.key}"
+  }
+
+  assertion {
+    type     = "statusCode"
+    operator = "is"
+    target   = "200"
+  }
+
+  assertion {
+    type     = "header"
+    operator = "is"
+    target   = "application/json"
+    property = "content-type"
+  }
+
+  assertion {
+    type     = "body"
+    operator = "is"
+    target   = "{\"message\":\"all's well\"}"
   }
 }
