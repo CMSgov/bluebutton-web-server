679e929244ddaab60e51aac91097a495722decea
Anna Montare <267455234+annamontare-nava@users.noreply.github.com>
2026-08-06 09:14:26 -0600

    allow string "Bearer" to be case-insensitive
    This matches spec, see PR comments.

--- apps/fhir/bluebutton/views/generic.py
+++ apps/fhir/bluebutton/views/generic.py
@@ -99,8 +99,8 @@ class FhirDataView(APIView):
         logger.debug('Request.path: %s' % request.path)
         req_meta = request.META
 
-        if 'HTTP_AUTHORIZATION' in req_meta and req_meta['HTTP_AUTHORIZATION'].startswith('Bearer '):
-            access_token = req_meta['HTTP_AUTHORIZATION'].removeprefix('Bearer ')
+        if 'HTTP_AUTHORIZATION' in req_meta and req_meta['HTTP_AUTHORIZATION'].lower().startswith('Bearer '):
+            access_token = req_meta['HTTP_AUTHORIZATION'].partition(' ')[2]
             try:
                 at = AccessToken.objects.get(token=access_token)
                 log_message = {
