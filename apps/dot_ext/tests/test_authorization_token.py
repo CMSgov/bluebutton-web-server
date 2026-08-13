71c5946adc53e97d5f0b47daebe5cfc63ec89f86
James Demery <jamesdemery@navapbc.com>
2026-08-13 11:26:59 -0400

    Modify unit test

--- apps/dot_ext/tests/test_authorization_token.py
+++ apps/dot_ext/tests/test_authorization_token.py
@@ -31,6 +31,7 @@ from apps.constants import (
 from apps.dot_ext.constants import (
     APPLICATION_DOES_NOT_HAVE_CLIENT_CREDENTIALS_ENABLED,
     APPLICATION_HAS_CLIENT_CREDENTIALS_ENABLED_NON_CLIENT_CREDENTIALS_AUTH_CALL_MADE,
+    AUTH_CODE_AND_CLIENT_CREDENTIALS_TYPE,
     AUTH_CODE_TYPE,
     CC_SYSTEM_MEDICARE_NUMBER,
     CLEAR_HIGHER_ISS,
@@ -606,154 +607,6 @@ class TestTokenResponseFields(BaseApiTest):
                 assert '"req_app_name": "CC App"' in request_logs.output[0]
 
     @patch.dict(os.environ, {'TARGET_ENV': 'local'})
-    @patch('apps.dot_ext.views.authorization.get_and_update_from_refresh')
-    @patch('apps.dot_ext.views.authorization.TokenView._validate_authorization_jwt')
-    @patch('apps.dot_ext.views.authorization.TokenView._validate_ial_jwt')
-    @patch('apps.dot_ext.views.authorization.TokenView._create_or_retrieve_user')
-    @patch('apps.dot_ext.views.authorization.get_patient_match_response_json')
-    @override_switch('v3_endpoints', active=True)
-    @override_switch('enable_auditevents', active=True)
-    def test_client_credentials_token_and_refresh_with_oidc4da_token_format(
-        self, mock_get_patient, mock_create_user, mock_validate_ial, mock_validate_auth, mock_get_and_update
-    ):
-        """Verify that a client_credentials token response includes "patient" and "refresh_token", and that the refresh_token can be used to refresh the access token."""
-
-        # Mocking the matched user
-        mock_create_user.return_value = self.user
-
-        mock_get_and_update.return_value = None
-
-        # Create fake JWT for first validation step
-        internal_id_token = jwt.encode(
-            {
-                'iss': IDME_LOWER_ISS,
-                'sub': '123',
-                'aud': 'https://fake.bluebutton.cms.gov',
-                'jti': 'jti-1',
-                'exp': 9999999999,
-                'iat': 1775317326,
-            },
-            'secret',
-            algorithm='HS256',
-        )
-        mock_validate_auth.return_value = internal_id_token
-
-        # min necessary fields (apart from address.)
-        mock_validate_ial.return_value = {
-            'iss': IDME_LOWER_ISS,
-            'sub': '123',
-            'aud': ['urn:oid:12345'],
-            'jti': 'jti-2',
-            'exp': 9999999999,
-            'iat': 1775317326,
-            'uuid': uuid.uuid4,
-            'auth_time': 9999999999,
-            'at_hash': '12345689',
-            'verified_claims': {
-                'verification': {'trust_framework': 'nist_800_63a', 'assurance_level': 'ial2'},
-                'family_name': 'Smith',
-                'given_name': 'John',
-                'name_historical': {
-                    'family_name': 'Smith',
-                    'given_name': 'John',
-                },
-                'birthdate': '1970-01-01',
-                'gender': 'Male',
-                'phone_number': '15124592223',
-                'phone_number_historical': ['15124592223', '15124592224'],
-                'ssn_itin': 123456789,
-                'ssn_itin_last_4': '6789',
-            },
-            'address': {
-                'formatted': '123 Any Rd., Indianapolis, IN, 46250',
-                'street_address': '123 Any Rd.',
-                'locality': 'Indianapolis',
-                'region': 'IN',
-                'postal_code': '46250',
-                'country': 'USA',
-            },
-        }
-
-        # Mock patient match result
-        # is_patient_match_found expects at least 2 entries in successful match
-        mock_get_patient.return_value = {
-            'type': 'searchset',
-            'entry': [
-                {'resource': {'id': 'org-example', 'resourceType': 'Organization'}},
-                {
-                    'resource': {
-                        'resourceType': 'Patient',
-                        'id': self.patient_fhir_v3,
-                        'identifier': [
-                            {
-                                'system': CC_SYSTEM_MEDICARE_NUMBER,
-                                'value': self.test_mbi,
-                            }
-                        ],
-                    }
-                },
-            ],
-        }
-
-        assertion = jwt.encode({'iss': self.application.client_id}, 'secret', algorithm='HS256')
-
-        token_request_data = {
-            'grant_type': CLIENT_CREDENTIALS,
-            'client_assertion_type': CLIENT_ASSERTION_TYPE_VALUE,
-            'client_assertion': assertion,
-            'scope': 'patient/ExplanationOfBenefit.rs openid',
-        }
-
-        response = self.client.post(
-            f'/v{Versions.V3}/o/token/',
-            data=urlencode(token_request_data),
-            content_type='application/x-www-form-urlencoded',
-        )
-
-        self.assertEqual(response.status_code, HTTPStatus.OK, response.content)
-        data = response.json()
-
-        self.assertIn('patient', data)
-        self.assertEqual(data['patient'], self.patient_fhir_v3)
-        self.assertIn('access_token', data)
-        # see authorization.py -> we can revisit this, but UserInfo will not return anything in the client_credentials flow.
-        self.assertNotIn('openid', data['scope'])
-        # other scopes ought to be fine, however.
-        self.assertIn('patient/ExplanationOfBenefit.rs', data['scope'])
-        self.assertIn('refresh_token', data)
-
-        # BB2-4965: Even though patient/AuditEvent.rs was not in the requested scopes, it was automatically added
-        # to the token scope as all client_credentials auth flows should result in a token with patient/AuditEvent.rs
-        # on it
-        access_token = get_access_token_model().objects.get(token=data['access_token'])
-        assert AUDIT_EVENT_SCOPE in access_token.scope
-
-        refresh_request_data = {
-            'grant_type': REFRESH_TOKEN,
-            'refresh_token': data['refresh_token'],
-            'client_id': TEST_APP_CLIENT_ID,
-            'client_secret': TEST_APP_CLIENT_SECRET,
-        }
-
-        response = self.client.post(
-            f'/v{Versions.V3}/o/token/',
-            data=urlencode(refresh_request_data),
-            content_type='application/x-www-form-urlencoded',
-        )
-
-        self.assertEqual(response.status_code, HTTPStatus.OK, response.content)
-        data = response.json()
-
-        self.assertIn('patient', data)
-        self.assertEqual(data['patient'], self.patient_fhir_v3)
-        self.assertIn('access_token', data)
-        # see authorization.py -> we can revisit this, but UserInfo will not return anything in the client_credentials flow.
-        self.assertNotIn('openid', data['scope'])
-        # other scopes ought to be fine, however.
-        self.assertIn('patient/ExplanationOfBenefit.rs', data['scope'])
-        self.assertIn('refresh_token', data)
-
-    @patch.dict(os.environ, {'TARGET_ENV': 'local'})
     @patch('apps.dot_ext.views.authorization.TokenView._validate_authorization_jwt')
     @patch('apps.dot_ext.views.authorization.TokenView._validate_ial_jwt')
     @patch('apps.dot_ext.views.authorization.get_patient_match_response_json')
@@ -1040,3 +893,136 @@ def test_client_credentials_returns_patient_match_not_found_401_integration(
     )
     assert response.status_code == HTTPStatus.UNAUTHORIZED
     assert response.json()['message'] == PATIENT_DATA_CANNOT_BE_FOUND
+
+
+@patch.dict(os.environ, {'TARGET_ENV': 'local'})
+@patch('apps.dot_ext.views.authorization.get_and_update_from_refresh')
+@patch('apps.dot_ext.views.authorization.TokenView._validate_authorization_jwt')
+@patch('apps.dot_ext.views.authorization.TokenView._validate_ial_jwt')
+@patch('apps.dot_ext.views.authorization.TokenView._create_or_retrieve_user')
+@patch('apps.dot_ext.views.authorization.get_patient_match_response_json')
+@override_switch('v3_endpoints', active=True)
+@override_switch('enable_auditevents', active=True)
+def test_client_credentials_token_and_refresh_with_oidc4da_token_format(
+    mock_get_patient,
+    mock_create_user,
+    mock_validate_ial,
+    mock_validate_auth,
+    mock_get_and_update,
+    basic_user,
+    create_application,
+    create_capability,
+):
+    """Verify that an ID token with the OIDC4IDA token format can successfully make it through a CAN request with the
+    client_credentials_validation switch inactive"""
+
+    # Mocking the matched user
+    # self.patient_fhir_v3 = 'patient-123-v3'
+    # self.user = self._create_user('patient_user', 'password123', fhir_id_v3=self.patient_fhir_v3)
+    user = basic_user(username='patient_user', fhir_id_v3='patient-123-v3')
+    mock_create_user.return_value = user
+
+    eob_capability = create_capability(name=EOB_SCOPE, urls=[['GET', '/v[123]/fhir/ExplanationOfBenefit[/]?$']])
+    application = create_application(
+        name='Test App',
+        jwks_uri='http://localhost:8000/.well-known/jwks.json',
+        capability=eob_capability,
+        allowed_auth_type=AUTH_CODE_AND_CLIENT_CREDENTIALS_TYPE,
+    )
+
+    mock_get_and_update.return_value = None
+
+    # Create fake JWT for first validation step
+    internal_id_token = jwt.encode(
+        {
+            'iss': IDME_LOWER_ISS,
+            'sub': '123',
+            'aud': 'https://fake.bluebutton.cms.gov',
+            'jti': 'jti-1',
+            'exp': 9999999999,
+            'iat': 1775317326,
+        },
+        'secret',
+        algorithm='HS256',
+    )
+    mock_validate_auth.return_value = internal_id_token
+
+    # min necessary fields (apart from address.)
+    mock_validate_ial.return_value = {
+        'iss': IDME_LOWER_ISS,
+        'sub': '123',
+        'aud': ['urn:oid:12345'],
+        'jti': 'jti-2',
+        'exp': 9999999999,
+        'iat': 1775317326,
+        'uuid': uuid.uuid4,
+        'auth_time': 9999999999,
+        'at_hash': '12345689',
+        'verified_claims': {
+            'verification': {'trust_framework': 'nist_800_63a', 'assurance_level': 'ial2'},
+            'family_name': 'Smith',
+            'given_name': 'John',
+            'name_historical': {
+                'family_name': 'Smith',
+                'given_name': 'John',
+            },
+            'birthdate': '1970-01-01',
+            'gender': 'Male',
+            'phone_number': '15124592223',
+            'phone_number_historical': ['15124592223', '15124592224'],
+            'ssn_itin': 123456789,
+            'ssn_itin_last_4': '6789',
+        },
+        'address': {
+            'formatted': '123 Any Rd., Indianapolis, IN, 46250',
+            'street_address': '123 Any Rd.',
+            'locality': 'Indianapolis',
+            'region': 'IN',
+            'postal_code': '46250',
+            'country': 'USA',
+        },
+    }
+
+    # Mock patient match result
+    # is_patient_match_found expects at least 2 entries in successful match
+    mock_get_patient.return_value = {
+        'type': 'searchset',
+        'entry': [
+            {'resource': {'id': 'org-example', 'resourceType': 'Organization'}},
+            {
+                'resource': {
+                    'resourceType': 'Patient',
+                    'id': 'patient-123-v3',
+                    'identifier': [
+                        {
+                            'system': CC_SYSTEM_MEDICARE_NUMBER,
+                            'value': '1SA0A00AA00',
+                        }
+                    ],
+                }
+            },
+        ],
+    }
+
+    assertion = jwt.encode({'iss': application.client_id}, 'secret', algorithm='HS256')
+
+    token_request_data = {
+        'grant_type': CLIENT_CREDENTIALS,
+        'client_assertion_type': CLIENT_ASSERTION_TYPE_VALUE,
+        'client_assertion': assertion,
+        'scope': 'patient/ExplanationOfBenefit.rs openid',
+    }
+
+    response = Client().post(
+        f'/v{Versions.V3}/o/token/',
+        data=urlencode(token_request_data),
+        content_type='application/x-www-form-urlencoded',
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    data = response.json()
+
+    assert 'patient' in data
+    assert data['patient'] == 'patient-123-v3'
+    assert 'access_token' in data
+    assert 'refresh_token' in data
