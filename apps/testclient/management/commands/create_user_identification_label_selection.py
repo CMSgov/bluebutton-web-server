e8d2cb4e6540a54ebc31410cb8a6d19c82aa5ed5
annamontare-nava <267455234+annamontare-nava@users.noreply.github.com>
2026-08-12 21:58:38 -0600

    Cover management commands with ruff (#1686)
    * ruff format all the things
    
    This will hopefully reduce clutter in future PRs
    
    * update ruff and regenerate requirements
    
    * remove incorrect comment
    
    The comment did not align with the config.
    
    * add isort rule
    
    * isort all the things
    
    `ruff check --select I --fix`
    
    * update todo comment
    
    * remove explicit ignores for rules that are no longer part of ruff's default
    
    Ruff no longer includes these in its defaults, so we don't need to
    ignore them
    
    * Revert "remove explicit ignores for rules that are no longer part of ruff's default"
    
    We actually do need these it seems, since we are explicitly selecting
    "F" rules, which I guess includes *all* of them.
    
    This reverts commit ae023a606d1eab6e35bab915b696ee45b8be9c7a.
    
    * Reapply "remove explicit ignores for rules that are no longer part of ruff's default"
    
    This reverts commit f29d1b84e45b64a692d20cdab14cb86388b51342.
    
    * ignore wildcard import rule on specific lines
    
    * add isort fixes to pre-commit
    
    * update ruff version in pre-commit
    
    * remove leading 0 in version number
    
    it appears this still resolved correctly
    
    * cover management commands with our ruff config
    
    * ruff format
    
    * update pre-commit to include management commands in ruff
    
    * remove exclude patterns from ruff-check and ruff-format hooks
    
    Exclusions are already set in our ruff.toml, so setting them here too
    is redundant.

--- apps/testclient/management/commands/create_user_identification_label_selection.py
+++ apps/testclient/management/commands/create_user_identification_label_selection.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+
 from apps.accounts.models import UserIdentificationLabel
 from apps.testclient.constants import DEVELOPER_SIGNUP_ROLE_CHOICES
 
@@ -7,7 +8,7 @@ def create_user_identification_label():
     UserIdentificationLabel.objects.all().delete()
     for c in DEVELOPER_SIGNUP_ROLE_CHOICES:
         choice = UserIdentificationLabel.objects.create(name=c[1], slug=c[2], weight=c[0])
-        print("group created %s " % (choice))
+        print('group created %s ' % (choice))
 
 
 class Command(BaseCommand):
