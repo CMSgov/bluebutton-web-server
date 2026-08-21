e69d6b9b6668bc9ec957cd0103b459a28f48cb4b
Ryan Morosa <Ryan.Morosa@icf.com>
2026-08-21 10:32:06 -0700

    Make driver a fixture and add more env vars and get selenium working

--- apps/fhir/bluebutton/tests/test_CAN_integration.py
+++ apps/fhir/bluebutton/tests/test_CAN_integration.py
@@ -19,7 +19,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from waffle.testutils import override_switch
 
-from apps.constants import CLIENT_CREDENTIALS, EOB_SCOPE
+from apps.constants import (
+    CLIENT_CREDENTIALS,
+    COVERAGE_SCOPE,
+    EOB_SCOPE,
+    PATIENT_SCOPE,
+    TEST_APP_CLIENT_ID,
+    TEST_APP_CLIENT_SECRET,
+)
 from apps.dot_ext.constants import CLIENT_ASSERTION_TYPE_VALUE, CLIENT_CREDENTIALS_TYPE
 
 # The v3 token endpoint. This MUST be identical everywhere it appears: it is both
@@ -36,6 +43,21 @@ CAN_PRIVATE_KEY = os.getenv('CAN_PRIVATE_KEY')
 # The key id advertised in the testclient's self-hosted JWKS and stamped on the
 # client_assertion header.
 TEST_APP_KID = 'my-key-id-1'
+APP_CLIENT_ID = os.getenv('APP_CLIENT_ID')
+ENABLE_CLIENT = os.getenv('ENABLE_CLIENT', 'true')
+
+# Convert the raw string to a boolean for easier use in the test
+is_client_enabled = ENABLE_CLIENT.lower() == 'true'
+
+
+@pytest.fixture(scope='function')
+def driver():
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Remote(command_executor='http://selenium:4444/wd/hub', options=options)
+
+    yield driver
+
+    driver.quit()
 
 
 def generate_pkce_data() -> tuple:
@@ -95,7 +117,7 @@ def find_element_and_send_keys(wait: WebDriverWait, by_method: str, locator_valu
         print(f'Failed to click {locator_value}.')
 
 
-def get_clear_authorization_code(client_id: str, code_challenge: str) -> str:
+def get_clear_authorization_code(driver, client_id: str, code_challenge: str) -> str:
     """
     Retrieves the authorization code from the Clear integration.
 
@@ -106,11 +128,6 @@ def get_clear_authorization_code(client_id: str, code_challenge: str) -> str:
     Returns:
         str: The authorization code.
     """
-    driver = webdriver.Remote(
-        command_executor='http://selenium:4444/wd/hub',
-        options=webdriver.ChromeOptions(),
-    )
-
     # See here for building the url: https://docs.clearme.com/docs/individual-access-token
     clear_login_url = f'https://verified.clearme.com/integrations/oauth2/auth?response_type=code&client_id={client_id}&state=teststate&redirect_uri={CLEAR_REDIRECT_URI}&scope=offline%20openid%20offline_access&code_challenge={code_challenge}&code_challenge_method=S256'
 
@@ -118,44 +135,67 @@ def get_clear_authorization_code(client_id: str, code_challenge: str) -> str:
     driver.get(clear_login_url)
     # We use WebDriverWait so that we don't have to put manual sleeps in between each action
     # XPATH was the most reliable way to get the object from the screen
-    wait = WebDriverWait(driver, 20)
-    find_element_and_send_keys(
-        wait=wait,
-        by_method=By.XPATH,
-        locator_value='//input[@placeholder="Phone"]',
-        input_text=DAMON_MYCHART_PHONE_NUMBER,
-    )
-    find_element_and_click(
-        wait=wait,
-        by_method=By.XPATH,
-        locator_value='//button[contains(text(), "Continue")]',
-    )
-    find_element_and_click(
-        wait=wait,
-        by_method=By.XPATH,
-        locator_value='//button[contains(text(), "Agree & Continue")]',
-    )
-    find_element_and_send_keys(
-        wait=wait,
-        by_method=By.XPATH,
-        locator_value='//input[@placeholder="6 Digit Code"]',
-        input_text=OTP_CODE,
-    )
+    # wait = WebDriverWait(driver, 30)
+    time.sleep(10)
+    # find_element_and_send_keys(
+    #     wait=wait,
+    #     by_method=By.XPATH,
+    #     locator_value='//input[@type="tel"]',
+    #     input_text=DAMON_MYCHART_PHONE_NUMBER,
+    # )
+    phone_input = driver.find_element(By.NAME, 'phone')
+    # print(phone_input)
+    phone_input.send_keys(DAMON_MYCHART_PHONE_NUMBER)
+    # element = wait.until(EC.element_to_be_clickable((By.NAME, 'phone')))
+    # element.send_keys(DAMON_MYCHART_PHONE_NUMBER)
+    time.sleep(3)
+    continue_button = driver.find_element(By.XPATH, '//button[(text()="Continue")]')
+    continue_button.click()
+    time.sleep(3)
+    agree_and_continue_button = driver.find_element(By.XPATH, '//button[(text()="Agree & Continue")]')
+    agree_and_continue_button.click()
+    time.sleep(3)
+    six_digit_code = driver.find_element(By.XPATH, '//input[@aria-label="6 Digit Code"]')
+    time.sleep(2)
+    six_digit_code.send_keys(OTP_CODE)
+    time.sleep(3)
+    skip_button = driver.find_element(By.XPATH, '//button[contains(text(), "Skip")]')
+    # print('SKIP BUTTON FOUND: ', skip_button)
+    # time.sleep(5)
+    # skip_button.click()
+    driver.execute_script('arguments[0].click();', skip_button)
+    time.sleep(20)
+    # find_element_and_click(
+    #     wait=wait,
+    #     by_method=By.XPATH,
+    #     locator_value='//button[contains(text(), "Continue")]',
+    # )
+    # find_element_and_click(
+    #     wait=wait,
+    #     by_method=By.XPATH,
+    #     locator_value='//button[contains(text(), "Agree & Continue")]',
+    # )
+    # find_element_and_send_keys(
+    #     wait=wait,
+    #     by_method=By.XPATH,
+    #     locator_value='//input[@placeholder="6 Digit Code"]',
+    #     input_text=OTP_CODE,
+    # )
 
-    # This sleep seems to be necessary because otherwise it loads too quickly and stalls.
-    # It seems to be some type of race condition.
-    time.sleep(1)
-    find_element_and_click(
-        wait=wait,
-        by_method=By.XPATH,
-        locator_value='//button[contains(text(), "Skip")]',
-    )
+    # # This sleep seems to be necessary because otherwise it loads too quickly and stalls.
+    # # It seems to be some type of race condition.
+    # time.sleep(1)
+    # find_element_and_click(
+    #     wait=wait,
+    #     by_method=By.XPATH,
+    #     locator_value='//button[contains(text(), "Skip")]',
+    # )
 
-    try:
-        # Need to wait until code gets generated in url before proceeding
-        wait.until(EC.url_contains('code'))
-    except NoSuchElementException:
-        print('The code was not found on this page')
+    # try:
+    #     # Need to wait until code gets generated in url before proceeding
+    #     wait.until(EC.url_contains('code'))
+    # except NoSuchElementException:
+    #     print('The code was not found on this page')
 
     url = driver.current_url
 
@@ -218,66 +258,11 @@ def construct_ial_payload(id_token: str, app_client_id: str) -> dict:
     }
 
 
-def get_access_token_response(client_assertion: str, client_assertion_type: str, grant_type: str, scope: str) -> dict:
-    """
-    Exchanges the client assertion for an access token from the Blue Button API.
-
-    Args:
-        client_assertion (str): The client assertion JWT.
-        client_assertion_type (str): The type of client assertion.
-        grant_type (str): The grant type for the token request.
-        scope (str): The scope for the token request.
-
-    Returns:
-        dict: A dictionary containing the access token and other related information.
-    """
-    client = Client()
-    url = '/v3/o/token'
-    data = {
-        'client_assertion': client_assertion,
-        'client_assertion_type': client_assertion_type,
-        'grant_type': grant_type,
-        'scope': scope,
-    }
-
-    response = client.post(
-        url,
-        data=urlencode(data),
-        content_type='application/x-www-form-urlencoded',
-    )
-    return response
-
-
-@pytest.mark.skipif(os.getenv('CAN_INTEGRATION_TEST') != '1', reason='Selenium tests disabled')
-@pytest.mark.integration
-@pytest.mark.django_db
-@override_switch('client_credentials_validation', active=True)
-@override_switch('v3_endpoints', active=True)
-def test_clear_integration_flow(basic_user, create_application, create_capability):
-    """
-    Integration test specifically against clear that does the following:
-     - Runs through the auth process with a synth user in CLEAR to generate a valid IAL JWT (via selenium)
-     - Generates an authorization JWT that contains the IAL JWT, using the test-app that is auto built into the app
-     - Runs through the auth process in Blue Button using the authorization JWT
-    """
-    # Needed to add this otherwise it would fail when looking up groups in a later method
-    Group.objects.create(name='BlueButton')
-    # Set up basic user, capability, and application
-    user = basic_user()
-    eob_capability = create_capability(name=EOB_SCOPE, urls=[['GET', '/v[3]/fhir/ExplanationOfBenefit[/]?$']])
-    application = create_application(
-        name='test',
-        grant_type='client-credentials',
-        user=user,
-        allowed_auth_type=CLIENT_CREDENTIALS_TYPE,
-        jwks_uri='http://localhost:8000/.well-known/jwks.json',
-        capability=eob_capability,
-    )
-    app_client_id = application.client_id
+def get_payload(driver, app_client_id, scope):
     code_verifier, code_challenge = generate_pkce_data()
 
     # Use selenium to simulate the user login and authorization flow to get the authorization code
-    auth_code = get_clear_authorization_code(CLEAR_CLIENT_ID, code_challenge)
+    auth_code = get_clear_authorization_code(driver, CLEAR_CLIENT_ID, code_challenge)
     # Exchange code for id token
     id_token = get_clear_id_token(CLEAR_CLIENT_ID, CLEAR_CLIENT_SECRET, auth_code, code_verifier)
     ial_payload = construct_ial_payload(id_token, app_client_id)
@@ -290,12 +275,85 @@ def test_clear_integration_flow(basic_user, create_application, create_capabilit
         headers={'kid': TEST_APP_KID, 'typ': 'JWT'},
     )
 
-    # Make the call to the token endpoint with the client_assertion and other params to get back access_token and refresh_token
     client_assertion_type = CLIENT_ASSERTION_TYPE_VALUE
     grant_type = CLIENT_CREDENTIALS
-    scope = EOB_SCOPE
 
-    access_token_response = get_access_token_response(client_assertion, client_assertion_type, grant_type, scope)
+    return {
+        'client_assertion': client_assertion,
+        'client_assertion_type': client_assertion_type,
+        'grant_type': grant_type,
+        'scope': scope,
+    }
+
+
+def get_access_token_response(json_payload: dict) -> dict:
+    """
+    Exchanges the client assertion for an access token from the Blue Button API.
+
+    Args:
+        json_payload (dict): The JSON payload containing the token request parameters.
+
+    Returns:
+        dict: A dictionary containing the access token and other related information.
+    """
+    if is_client_enabled:
+        client = Client()
+        url = '/v3/o/token'
+        data = json_payload
+
+        response = client.post(
+            url,
+            data=urlencode(data),
+            content_type='application/x-www-form-urlencoded',
+        )
+        return response
+    else:
+        url = BB2_TOKEN_URL
+        custom_headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+
+        response = requests.post(url, data=urlencode(json_payload), headers=custom_headers)
+        return response
+
+
+@pytest.mark.skipif(os.getenv('CAN_INTEGRATION_TEST') != '1', reason='Selenium tests disabled')
+@pytest.mark.integration
+@pytest.mark.django_db
+@override_switch('client_credentials_validation', active=True)
+@override_switch('v3_endpoints', active=True)
+def test_clear_integration_flow(driver, basic_user, create_application, create_capability):
+    """
+    Integration test specifically against clear that does the following:
+     - Runs through the auth process with a synth user in CLEAR to generate a valid IAL JWT (via selenium)
+     - Generates an authorization JWT that contains the IAL JWT, using the test-app that is auto built into the app
+     - Runs through the auth process in Blue Button using the authorization JWT
+    """
+    # Needed to add this otherwise it would fail when looking up groups in a later method
+    Group.objects.create(name='BlueButton')
+    # Set up basic user, capability, and application
+    user = basic_user()
+    eob_capability = create_capability(name=EOB_SCOPE, urls=[['GET', '/v[3]/fhir/ExplanationOfBenefit[/]?$']])
+    coverage_capability = create_capability(name=COVERAGE_SCOPE, urls=[['GET', '/v[3]/fhir/Coverage[/]?$']])
+    patient_capability = create_capability(name=PATIENT_SCOPE, urls=[['GET', '/v[3]/fhir/Patient[/]?$']])
+    application = create_application(
+        name='test',
+        grant_type='client-credentials',
+        user=user,
+        allowed_auth_type=CLIENT_CREDENTIALS_TYPE,
+        jwks_uri='http://localhost:8000/.well-known/jwks.json',
+        client_id=TEST_APP_CLIENT_ID,
+        client_secret=TEST_APP_CLIENT_SECRET,
+    )
+    application.scope.add(eob_capability)
+    application.scope.add(coverage_capability)
+    application.scope.add(patient_capability)
+    if APP_CLIENT_ID:
+        app_client_id = APP_CLIENT_ID
+    else:
+        app_client_id = application.client_id
+
+    json_payload = get_payload(driver, app_client_id, EOB_SCOPE)
+
+    access_token_response = get_access_token_response(json_payload)
 
     assert access_token_response.status_code == 200, (
         f'Expected status code 200, got {access_token_response.status_code}'
@@ -305,6 +363,3 @@ def test_clear_integration_flow(basic_user, create_application, create_capabilit
 
     assert 'access_token' in access_token_response_json, 'Access token not found in response'
     assert 'refresh_token' in access_token_response_json, 'Refresh token not found in response'
-    assert scope == access_token_response_json.get('scope'), (
-        f'Expected scope {scope}, got {access_token_response_json.get("scope")}'
-    )
