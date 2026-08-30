d3dc708e3feaa7dc419704bc8eec46c1935dec67
Brandon Wang <brandon.wang@icf.com>
2026-08-30 17:12:16 -0500

    re-adding this validation

--- apps/fhir/bluebutton/views/search.py
+++ apps/fhir/bluebutton/views/search.py
@@ -1,7 +1,7 @@
 import waffle
 from rest_framework import permissions
 from voluptuous import (
-    ALLOW_EXTRA,
+    REMOVE_EXTRA,
     All,
     Coerce,
     Match,
@@ -192,6 +192,6 @@ class SearchViewExplanationOfBenefit(SearchView):
             params['_tag'] = request.query_params.getlist('_tag')
             params['_source'] = request.query_params.getlist('_source')
 
-        schema = Schema(query_schema, extra=ALLOW_EXTRA)
+        schema = Schema(query_schema, extra=REMOVE_EXTRA)
 
         return schema(params)
