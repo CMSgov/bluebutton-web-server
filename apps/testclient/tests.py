05f6b0b136b9ce2a52aebda4ac78c55e19c1d7af
James Demery <jamesdemery@navapbc.com>
2026-09-14 09:27:32 -0400

    BB2-4844: Ensure all integration tests pass regardless of bfd value

--- apps/testclient/tests.py
+++ apps/testclient/tests.py
@@ -1,5 +1,4 @@
 import json
-import os
 from typing import List
 
 import pytest
@@ -338,27 +337,17 @@ class BlueButtonClientApiFhirTest(TestCase):
         # Different environments have different data in them.
         # If we are testing against sandbox, we expect fewer responses.
 
-        if os.getenv('LOCAL_TESTING_TARGET', None) in ['impl']:
-            self.assertEqual(len(response_data['entry']), 12)
-        else:
-            self.assertEqual(len(response_data['entry']), 5)
+        self.assertEqual(len(response_data['entry']), 12)
 
         previous_links = [data['url'] for data in response_data['link'] if data['relation'] == 'previous']
         next_links = [data['url'] for data in response_data['link'] if data['relation'] == 'next']
         first_links = [data['url'] for data in response_data['link'] if data['relation'] == 'first']
 
-        if os.getenv('LOCAL_TESTING_TARGET', None) in ['impl']:
-            self.assertEqual(len(previous_links), 1)
-            self.assertEqual(len(next_links), 1)
-            self.assertEqual(len(first_links), 1)
-            self.assertIn('startIndex=13', previous_links[0])
-            self.assertIn('startIndex=0', first_links[0])
-        else:
-            self.assertEqual(len(previous_links), 1)
-            self.assertEqual(len(next_links), 0)
-            self.assertEqual(len(first_links), 1)
-            self.assertIn('startIndex=13', previous_links[0])
-            self.assertIn('startIndex=0', first_links[0])
+        self.assertEqual(len(previous_links), 1)
+        self.assertEqual(len(next_links), 1)
+        self.assertEqual(len(first_links), 1)
+        self.assertIn('startIndex=13', previous_links[0])
+        self.assertIn('startIndex=0', first_links[0])
 
         self.assertContains(response, 'ExplanationOfBenefit')
 
